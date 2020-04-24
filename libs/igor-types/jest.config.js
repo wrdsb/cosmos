514f7ddd707fb549b1c417920bbdf981674e6d5b
James Schumann <james@schumann.ca>
2020-04-24 09:35:39 -0400

    generate igor-types library

---
+++ libs/igor-types/jest.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  name: 'igor-types',
+  preset: '../../jest.config.js',
+  transform: {
+    '^.+\\.[tj]sx?$': 'ts-jest'
+  },
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'html'],
+  coverageDirectory: '../../coverage/libs/igor-types'
+};
