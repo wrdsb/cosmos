e5b6b41235cf3e8b6d46b42d44c9d01bffd8efff
James Schumann <james@schumann.ca>
2020-05-28 13:28:02 -0400

    absorb codex-angular repo. closes #2

--- apps/codex-console/src/app/app.component.ts
+++ apps/codex-console/src/app/app.component.ts
@@ -1,10 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+import { Menu } from "./core/navigation/menu.model";
+import { NavigationService } from "./core/navigation/navigation.service";
 
 @Component({
-  selector: 'cosmos-root',
+  selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'codex-console';
+export class AppComponent implements OnInit{
+  title = 'Codex';
+
+  footerMenu: Menu;
+  headerMenu: Menu;
+  sideMenu: Menu;
+
+  showNav = false;
+
+  constructor(private navigationService: NavigationService) {}
+
+  ngOnInit() {
+    this.getFooterMenu();
+    this.getHeaderMenu();
+    this.getSideMenu();
+  }
+
+  getFooterMenu(): void {
+    this.navigationService.getFooter().subscribe(menu => this.footerMenu = menu);
+  }
+
+  getHeaderMenu(): void {
+    this.navigationService.getHeader().subscribe(menu => this.headerMenu = menu);
+  }
+
+  getSideMenu(): void {
+    this.navigationService.getSide().subscribe(menu => this.sideMenu = menu);
+  }
 }
--- apps/codex-console/src/app/app.module.ts
+++ apps/codex-console/src/app/app.module.ts
@@ -1,16 +1,43 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { environment } from "../environments/environment";
+
+// Firebase imports
+import { AngularFireModule } from "@angular/fire";
+import { AngularFirestoreModule } from "@angular/fire/firestore";
+import { AngularFireAuthModule, AngularFireAuth } from "@angular/fire/auth";
+import { AngularFireAuthGuardModule } from "@angular/fire/auth-guard";
+
+import { AuthenticationModule } from "./authentication/authentication.module";
+
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { RouterModule } from '@angular/router';
+
+import { NavigationModule } from "./core/navigation/navigation.module";
+import { ThemeModule } from "./core/theme/theme.module";
+
+import { HomeComponent } from './home/home.component';
+import { HappyComponent } from './happy/happy.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    HappyComponent
+  ],
   imports: [
     BrowserModule,
-    RouterModule.forRoot([], { initialNavigation: 'enabled' })
+    AuthenticationModule,
+    AppRoutingModule,
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFirestoreModule,
+    AngularFireAuthModule,
+    AngularFireAuthGuardModule,
+    NavigationModule,
+    ThemeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule {}
+export class AppModule { }
