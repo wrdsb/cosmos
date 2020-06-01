14f2a312176e4a511c069f085507ed22dcf417c0
James Schumann <james@schumann.ca>
2020-06-01 11:54:39 -0400

    ng g @nrwl/angular:app flenderson-console

---
+++ apps/flenderson-console/jest.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  name: 'flenderson-console',
+  preset: '../../jest.config.js',
+  coverageDirectory: '../../coverage/apps/flenderson-console',
+  snapshotSerializers: [
+    'jest-preset-angular/build/AngularNoNgAttributesSnapshotSerializer.js',
+    'jest-preset-angular/build/AngularSnapshotSerializer.js',
+    'jest-preset-angular/build/HTMLCommentSerializer.js'
+  ]
+};
