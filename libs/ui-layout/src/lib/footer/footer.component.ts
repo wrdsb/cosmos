63934d4c64e71112310737b728d3461a34337599
James Schumann <james@schumann.ca>
2020-06-04 14:42:42 -0400

    absorb footer component from sorting-hat-console into ui-layout lib

--- libs/ui-layout/src/lib/footer/footer.component.ts
+++ libs/ui-layout/src/lib/footer/footer.component.ts
@@ -1,15 +1,61 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Menu } from '@cosmos/ui';
+import { UINavigationService } from '@cosmos/ui-navigation';
+
+import { UILayoutService } from '../ui-layout.service';
+
 @Component({
   selector: 'cosmos-footer',
   templateUrl: './footer.component.html',
   styleUrls: ['./footer.component.scss']
 })
 export class FooterComponent implements OnInit {
+  enabled: boolean;
+  enabled$ = this.layoutService.footerEnabled$;
 
-  constructor() { }
+  visible: boolean;
+  visible$ = this.layoutService.footerVisible$;
+
+  content: string;
+  content$ = this.layoutService.footerContent$;
+
+  footerMenu: Menu;
+
+  constructor(
+    private layoutService: UILayoutService,
+    private navigationService: UINavigationService
+  ) { }
 
   ngOnInit(): void {
+    this.getEnabled();
+    this.getVisible();
+    this.getContent();
+    this.getFooterMenu();
   }
 
+  getEnabled(): void {
+    this.enabled$.subscribe(enabled => 
+      this.enabled = enabled
+    );
+    console.log(`footer enabled: ${this.enabled}`);
+  }
+
+  getVisible(): void {
+    this.visible$.subscribe(visible => 
+      this.visible = visible
+    );
+    console.log(`footer visible: ${this.visible}`);
+  }
+
+  getContent(): void {
+    this.content$.subscribe(content => 
+      this.content = content
+    );
+    console.log(`footer content: ${this.content}`);
+  }
+
+  getFooterMenu(): void {
+    this.navigationService.getFooter().subscribe(menu => this.footerMenu = menu);
+  }
 }
--- libs/ui-layout/src/lib/ui-layout.module.ts
+++ libs/ui-layout/src/lib/ui-layout.module.ts
@@ -1,16 +1,41 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FooterComponent } from './footer/footer.component';
-import { HeaderComponent } from './header/header.component';
+import { RouterModule } from '@angular/router';
+
+import { UINavigationModule } from "@cosmos/ui-navigation";
+import { UIPanelsModule } from '@cosmos/ui-panels';
+
 import { LayoutComponent } from './layout/layout.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { SidebarRightComponent } from './sidebar-right/sidebar-right.component';
+import { SidebarLeftComponent } from './sidebar-left/sidebar-left.component';
+
 import { PanelLeftComponent } from './panel-left/panel-left.component';
 import { PanelRightComponent } from './panel-right/panel-right.component';
-import { SidebarLeftComponent } from './sidebar-left/sidebar-left.component';
-import { SidebarRightComponent } from './sidebar-right/sidebar-right.component';
 
 @NgModule({
-  imports: [CommonModule],
-  declarations: [FooterComponent, HeaderComponent, LayoutComponent, PanelLeftComponent, PanelRightComponent, SidebarLeftComponent, SidebarRightComponent],
-  exports: [FooterComponent, HeaderComponent, LayoutComponent, PanelLeftComponent, PanelRightComponent, SidebarLeftComponent, SidebarRightComponent]
+  declarations: [
+    LayoutComponent,
+    HeaderComponent,
+    FooterComponent,
+    SidebarRightComponent,
+    SidebarLeftComponent,
+    PanelLeftComponent,
+    PanelRightComponent
+  ],
+  imports: [
+    CommonModule,
+    RouterModule,
+    UINavigationModule,
+    UIPanelsModule
+  ],
+  exports: [
+    LayoutComponent,
+    HeaderComponent,
+    FooterComponent,
+    SidebarRightComponent,
+    SidebarLeftComponent
+  ]
 })
 export class UILayoutModule {}
