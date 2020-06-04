257b28dd31b41bfd587a7bf3de5fbce2b2470ceb
James Schumann <james@schumann.ca>
2020-06-04 16:31:21 -0400

    absorb sidebar-left component from sorting-hat-console into ui-layout lib

--- libs/ui-layout/src/lib/sidebar-left/sidebar-left.component.ts
+++ libs/ui-layout/src/lib/sidebar-left/sidebar-left.component.ts
@@ -1,15 +1,50 @@
 import { Component, OnInit } from '@angular/core';
 
+import { UILayoutService } from '../ui-layout.service';
+
 @Component({
   selector: 'cosmos-sidebar-left',
   templateUrl: './sidebar-left.component.html',
   styleUrls: ['./sidebar-left.component.scss']
 })
 export class SidebarLeftComponent implements OnInit {
+  enabled: boolean;
+  enabled$ = this.layoutService.sidebarLeftEnabled$;
 
-  constructor() { }
+  visible: boolean;
+  visible$ = this.layoutService.sidebarLeftVisible$;
+
+  content: string;
+  content$ = this.layoutService.sidebarLeftContent$;
+
+  constructor(
+    private layoutService: UILayoutService
+  ) { }
 
   ngOnInit(): void {
+    this.getEnabled();
+    this.getVisible();
+    this.getContent();
   }
 
+  getEnabled(): void {
+    this.enabled$.subscribe(enabled => 
+      this.enabled = enabled
+    );
+    console.log(`sidebar-left enabled: ${this.enabled}`);
+  }
+
+  getVisible(): void {
+    this.visible$.subscribe(visible => 
+      this.visible = visible
+    );
+    console.log(`sidebar-left visible: ${this.visible}`);
+  }
+
+  getContent(): void {
+    this.content$.subscribe(content => 
+      this.content = content
+    );
+    console.log(`sidebar-left content: ${this.content}`);
+  }
 }
