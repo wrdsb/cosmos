f038edd67617b7f9f8e9f3bdc4fe832baf2c8ace
James Schumann <james@schumann.ca>
2020-06-04 16:29:11 -0400

    absorb panel-right component from sorting-hat-console into ui-layout lib

--- libs/ui-layout/src/lib/panel-right/panel-right.component.ts
+++ libs/ui-layout/src/lib/panel-right/panel-right.component.ts
@@ -1,15 +1,50 @@
 import { Component, OnInit } from '@angular/core';
 
+import { UILayoutService } from '../ui-layout.service';
+
 @Component({
   selector: 'cosmos-panel-right',
   templateUrl: './panel-right.component.html',
   styleUrls: ['./panel-right.component.scss']
 })
 export class PanelRightComponent implements OnInit {
+  enabled: boolean;
+  enabled$ = this.layoutService.panelRightEnabled$;
 
-  constructor() { }
+  visible: boolean;
+  visible$ = this.layoutService.panelRightVisible$
+
+  content: string;
+  content$ = this.layoutService.panelRightContent$;
+
+  constructor(
+    private layoutService: UILayoutService
+  ) { }
 
   ngOnInit(): void {
+    this.getEnabled();
+    this.getVisible();
+    this.getContent();
   }
 
+  getEnabled(): void {
+    this.enabled$.subscribe(enabled => 
+      this.enabled = enabled
+    );
+    console.log(`panel-right enabled: ${this.enabled}`);
+  }
+
+  getVisible(): void {
+    this.visible$.subscribe(visible => 
+      this.visible = visible
+    );
+    console.log(`panel-right visible: ${this.visible}`);
+  }
+
+  getContent(): void {
+    this.content$.subscribe(content => 
+      this.content = content
+    );
+    console.log(`panel-right content: ${this.content}`);
+  }
 }
