329c619b167b79e480aa9566944b0cb1f2b889e2
James Schumann <james@schumann.ca>
2020-06-04 12:02:04 -0400

    ng g component sidebar-right --project=ui-layout --export

--- libs/ui-layout/src/lib/ui-layout.module.ts
+++ libs/ui-layout/src/lib/ui-layout.module.ts
@@ -6,10 +6,11 @@ import { LayoutComponent } from './layout/layout.component';
 import { PanelLeftComponent } from './panel-left/panel-left.component';
 import { PanelRightComponent } from './panel-right/panel-right.component';
 import { SidebarLeftComponent } from './sidebar-left/sidebar-left.component';
+import { SidebarRightComponent } from './sidebar-right/sidebar-right.component';
 
 @NgModule({
   imports: [CommonModule],
-  declarations: [FooterComponent, HeaderComponent, LayoutComponent, PanelLeftComponent, PanelRightComponent, SidebarLeftComponent],
-  exports: [FooterComponent, HeaderComponent, LayoutComponent, PanelLeftComponent, PanelRightComponent, SidebarLeftComponent]
+  declarations: [FooterComponent, HeaderComponent, LayoutComponent, PanelLeftComponent, PanelRightComponent, SidebarLeftComponent, SidebarRightComponent],
+  exports: [FooterComponent, HeaderComponent, LayoutComponent, PanelLeftComponent, PanelRightComponent, SidebarLeftComponent, SidebarRightComponent]
 })
 export class UiLayoutModule {}
