58d8f9f16159b1fb1b3c229421a9ab9d80400496
James Schumann <james@schumann.ca>
2020-06-04 13:43:33 -0400

    move menu model into ui lib; finish removing navigation module from sorting-hat-console

--- libs/ui/src/lib/ui.module.ts
+++ libs/ui/src/lib/ui.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { Menu, MenuLink } from "./model/menu.model";
+
 @NgModule({
-  imports: [CommonModule]
+  declarations: [
+    Menu,
+    MenuLink
+  ],
+  imports: [
+    CommonModule
+  ],
+  exports: [
+    Menu,
+    MenuLink
+  ]
 })
 export class UIModule {}
