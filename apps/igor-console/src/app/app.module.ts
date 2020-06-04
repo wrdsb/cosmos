f8feb6cd25e7dc939ec3b5e64ed0ad69b14a7393
James Schumann <james@schumann.ca>
2020-06-04 12:13:48 -0400

    import ui-layout footer into igor-console

--- apps/igor-console/src/app/app.module.ts
+++ apps/igor-console/src/app/app.module.ts
@@ -3,12 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { RouterModule } from '@angular/router';
+import { UiLayoutModule } from "@cosmos/ui-layout";
 
 @NgModule({
   declarations: [AppComponent],
   imports: [
     BrowserModule,
-    RouterModule.forRoot([], { initialNavigation: 'enabled' })
+    RouterModule.forRoot([], { initialNavigation: 'enabled' },),
+    UiLayoutModule
   ],
   providers: [],
   bootstrap: [AppComponent]
