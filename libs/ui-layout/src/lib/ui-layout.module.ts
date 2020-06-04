cebd7e8c57f479872473b2510772ef5fa3391a80
James Schumann <james@schumann.ca>
2020-06-04 12:00:44 -0400

    ng g component layout --project=ui-layout --export

--- libs/ui-layout/src/lib/ui-layout.module.ts
+++ libs/ui-layout/src/lib/ui-layout.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FooterComponent } from './footer/footer.component';
 import { HeaderComponent } from './header/header.component';
+import { LayoutComponent } from './layout/layout.component';
 
 @NgModule({
   imports: [CommonModule],
-  declarations: [FooterComponent, HeaderComponent],
-  exports: [FooterComponent, HeaderComponent]
+  declarations: [FooterComponent, HeaderComponent, LayoutComponent],
+  exports: [FooterComponent, HeaderComponent, LayoutComponent]
 })
 export class UiLayoutModule {}
