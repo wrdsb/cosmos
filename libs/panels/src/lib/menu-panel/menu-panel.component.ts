0178c11def96ea916414f63bb4bd81bf5fc167ff
James Schumann <james@schumann.ca>
2020-06-05 09:47:57 -0400

    ng g component menu-panel --project=panels --export

---
+++ libs/panels/src/lib/menu-panel/menu-panel.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'cosmos-menu-panel',
+  templateUrl: './menu-panel.component.html',
+  styleUrls: ['./menu-panel.component.scss']
+})
+export class MenuPanelComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- libs/panels/src/lib/panels.module.ts
+++ libs/panels/src/lib/panels.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FeedbackPanelComponent } from './feedback-panel/feedback-panel.component';
 import { HelpPanelComponent } from './help-panel/help-panel.component';
+import { MenuPanelComponent } from './menu-panel/menu-panel.component';
 
 @NgModule({
   imports: [CommonModule],
-  declarations: [FeedbackPanelComponent, HelpPanelComponent],
-  exports: [FeedbackPanelComponent, HelpPanelComponent]
+  declarations: [FeedbackPanelComponent, HelpPanelComponent, MenuPanelComponent],
+  exports: [FeedbackPanelComponent, HelpPanelComponent, MenuPanelComponent]
 })
 export class PanelsModule {}
