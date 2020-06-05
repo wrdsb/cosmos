79afb0064083cc40811dbfeb39edd7c5900e90c2
James Schumann <james@schumann.ca>
2020-06-05 09:49:18 -0400

    ng g component settings-panel --project=panels --export

--- libs/panels/src/lib/panels.module.ts
+++ libs/panels/src/lib/panels.module.ts
@@ -5,10 +5,11 @@ import { HelpPanelComponent } from './help-panel/help-panel.component';
 import { MenuPanelComponent } from './menu-panel/menu-panel.component';
 import { NotifictaionsPanelComponent } from './notifictaions-panel/notifictaions-panel.component';
 import { ProfilePanelComponent } from './profile-panel/profile-panel.component';
+import { SettingsPanelComponent } from './settings-panel/settings-panel.component';
 
 @NgModule({
   imports: [CommonModule],
-  declarations: [FeedbackPanelComponent, HelpPanelComponent, MenuPanelComponent, NotifictaionsPanelComponent, ProfilePanelComponent],
-  exports: [FeedbackPanelComponent, HelpPanelComponent, MenuPanelComponent, NotifictaionsPanelComponent, ProfilePanelComponent]
+  declarations: [FeedbackPanelComponent, HelpPanelComponent, MenuPanelComponent, NotifictaionsPanelComponent, ProfilePanelComponent, SettingsPanelComponent],
+  exports: [FeedbackPanelComponent, HelpPanelComponent, MenuPanelComponent, NotifictaionsPanelComponent, ProfilePanelComponent, SettingsPanelComponent]
 })
 export class PanelsModule {}
