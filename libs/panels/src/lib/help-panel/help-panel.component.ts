80843d6344086a9af0a837443b0338c38844d684
James Schumann <james@schumann.ca>
2020-06-05 09:47:39 -0400

    ng g component help-panel --project=panels --export

---
+++ libs/panels/src/lib/help-panel/help-panel.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'cosmos-help-panel',
+  templateUrl: './help-panel.component.html',
+  styleUrls: ['./help-panel.component.scss']
+})
+export class HelpPanelComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- libs/panels/src/lib/panels.module.ts
+++ libs/panels/src/lib/panels.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FeedbackPanelComponent } from './feedback-panel/feedback-panel.component';
+import { HelpPanelComponent } from './help-panel/help-panel.component';
 
 @NgModule({
   imports: [CommonModule],
-  declarations: [FeedbackPanelComponent],
-  exports: [FeedbackPanelComponent]
+  declarations: [FeedbackPanelComponent, HelpPanelComponent],
+  exports: [FeedbackPanelComponent, HelpPanelComponent]
 })
 export class PanelsModule {}
