d3d14d13ee66113a9979c84bdf252eebaa868174
James Schumann <james@schumann.ca>
2020-06-08 12:50:40 -0400

    update all HAGAR functions to use new shared lib

--- apps/hagar-functions/aad-group-member-remove/index.ts
+++ apps/hagar-functions/aad-group-member-remove/index.ts
@@ -1,8 +1,8 @@
 import { AzureFunction, Context } from "@azure/functions"
-import { createLogObject } from "../shared/createLogObject";
-import { storeLogBlob } from "../shared/storeLogBlob";
-import { createCallbackMessage } from "../shared/createCallbackMessage";
-import { createEvent } from "../shared/createEvent";
+import { createLogObject } from "@cosmos/azure-functions-shared";
+import { storeLogBlob } from "@cosmos/azure-functions-shared";
+import { createCallbackMessage } from "@cosmos/azure-functions-shared";
+import { createEvent } from "@cosmos/azure-functions-shared";
 
 const aadGroupMemberRemove: AzureFunction = async function (context: Context, triggerMessage: any): Promise<void> {
     const functionInvocationID = context.executionContext.invocationId;
