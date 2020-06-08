9214720aa8e4e7b245a8c2eee1f9264d43477a11
James Schumann <james@schumann.ca>
2020-06-08 13:31:14 -0400

    init viewfinder-functions. closes #182

---
+++ apps/viewfinder-functions/ping/index.ts
@@ -0,0 +1,67 @@
+import { AzureFunction, Context, HttpRequest } from "@azure/functions"
+import { createLogObject } from "@cosmos/azure-functions-shared";
+import { storeLogBlob } from "@cosmos/azure-functions-shared";
+import { createCallbackMessage } from "@cosmos/azure-functions-shared";
+import { createEvent } from "@cosmos/azure-functions-shared";
+
+const ping: AzureFunction = async function (context: Context, req: HttpRequest): Promise<void> {
+    const functionInvocationID = context.executionContext.invocationId;
+    const functionInvocationTime = new Date();
+    const functionInvocationTimestamp = functionInvocationTime.toJSON();  // format: 2012-04-23T18:25:43.511Z
+
+    const functionName = context.executionContext.functionName;
+    const functionEventType = 'WRDSB.Viewfinder.Ping';
+    const functionEventID = `viewfinder-functions-${functionName}-${functionInvocationID}`;
+    const functionLogID = `${functionInvocationTime.getTime()}-${functionInvocationID}`;
+
+    const logStorageAccount = process.env['storageAccount'];
+    const logStorageKey = process.env['storageKey'];
+    const logStorageContainer = 'function-ping-logs';
+
+    const eventLabel = '';
+    const eventTags = [
+        "viewfinder", 
+    ];
+
+    const request = req;
+
+    const operation = request.body.operation;
+    const payload = request.body.payload;
+
+    let result = "pong";
+
+    const logPayload = result;
+
+    const logObject = await createLogObject(functionInvocationID, functionInvocationTime, functionName, logPayload);
+    const logBlob = await storeLogBlob(logStorageAccount, logStorageKey, logStorageContainer, logObject);
+    context.log(logBlob);
+
+    const callbackMessage = await createCallbackMessage(logObject, 200);
+    context.bindings.callbackMessage = JSON.stringify(callbackMessage);
+    context.log(callbackMessage);
+
+    const invocationEvent = await createEvent(
+        functionInvocationID,
+        functionInvocationTime,
+        functionInvocationTimestamp,
+        functionName,
+        functionEventType,
+        functionEventID,
+        functionLogID,
+        logStorageAccount,
+        logStorageContainer,
+        eventLabel,
+        eventTags
+    );
+    context.bindings.flynnEvent = JSON.stringify(invocationEvent);
+    context.log(invocationEvent);
+
+    context.res = {
+        status: 200,
+        body: invocationEvent
+    };
+
+    context.done(null, logBlob);
+};
+
+export default ping;
