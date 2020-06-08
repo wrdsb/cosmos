e5c1a3f7139dc2049f641a85b6b426cbd39e2ac7
James Schumann <james@schumann.ca>
2020-06-08 12:48:49 -0400

    copy shared functions code from HAGAR into lib

--- libs/azure-functions-shared/src/index.ts
+++ libs/azure-functions-shared/src/index.ts
@@ -1,1 +1,4 @@
-export * from './lib/azure-functions-shared';
+export * from './lib/create-callback-message';
+export * from './lib/create-event';
+export * from './lib/create-log-object';
+export * from './lib/store-log-blob';
