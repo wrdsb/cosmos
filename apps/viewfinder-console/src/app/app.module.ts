101b7a8861c2eb4295e50308375611855758612b
James Schumann <james@schumann.ca>
2020-06-10 16:56:26 -0400

    start moving auth code into its own shared module

--- apps/viewfinder-console/src/app/app.module.ts
+++ apps/viewfinder-console/src/app/app.module.ts
@@ -3,16 +3,8 @@ import { CommonModule } from '@angular/common';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
-import {
-  MsalModule,
-  MsalInterceptor,
-  MSAL_CONFIG,
-  MSAL_CONFIG_ANGULAR,
-  MsalService,
-  MsalAngularConfiguration
-} from '@azure/msal-angular';
-import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
-import { Configuration } from 'msal';
+import { MsalModule } from '@azure/msal-angular';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -20,45 +12,7 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { ProfilePanelComponent } from "./profile-panel/profile-panel.component";
 
-import { GraphService } from '@cosmos/msgraph-service';
-
-
-export const protectedResourceMap: [string, string[]][] = [
-  ['https://graph.microsoft.com/v1.0/me', ['user.read']]
-];
-
-const isIE = window.navigator.userAgent.indexOf("MSIE ") > -1 || window.navigator.userAgent.indexOf("Trident/") > -1;
-
-function MSALConfigFactory(): Configuration {
-  return {
-    auth: {
-      clientId: 'd15732c7-13e4-41fc-9d8f-c7776b875e58',
-      authority: "https://login.microsoftonline.com/cd25c694-bfb8-48f4-9d0d-b9af282c4ab4",
-      validateAuthority: true,
-      redirectUri: "http://localhost:4200/",
-      postLogoutRedirectUri: "http://localhost:4200/",
-      navigateToLoginRequestUrl: true,
-    },
-    cache: {
-      cacheLocation: "localStorage",
-      storeAuthStateInCookie: isIE, // set to true for IE 11
-    },
-  };
-}
-
-function MSALAngularConfigFactory(): MsalAngularConfiguration {
-  return {
-    popUp: !isIE,
-    consentScopes: [
-      "user.read",
-      "openid",
-      "profile"
-    ],
-    unprotectedResources: ["https://www.microsoft.com/en-us/"],
-    protectedResourceMap,
-    extraQueryParameters: {}
-  };
-}
+import { AngularAADAuthModule } from "@cosmos/angular-aad-auth";
 
 @NgModule({
   declarations: [
@@ -71,26 +25,10 @@ function MSALAngularConfigFactory(): MsalAngularConfiguration {
     AppRoutingModule,
     BrowserAnimationsModule,
     HttpClientModule,
-    MsalModule
+    MsalModule,
+    AngularAADAuthModule
   ],
   schemas: [CUSTOM_ELEMENTS_SCHEMA],
-  providers: [
-    GraphService,
-    {
-      provide: HTTP_INTERCEPTORS,
-      useClass: MsalInterceptor,
-      multi: true
-    },
-    {
-      provide: MSAL_CONFIG,
-      useFactory: MSALConfigFactory
-    },
-    {
-      provide: MSAL_CONFIG_ANGULAR,
-      useFactory: MSALAngularConfigFactory
-    },
-    MsalService
-  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- libs/angular-aad-auth/src/lib/angular-aad-auth.module.ts
+++ libs/angular-aad-auth/src/lib/angular-aad-auth.module.ts
@@ -1,7 +1,78 @@
-import { NgModule } from '@angular/core';
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { GraphService } from '@cosmos/msgraph-service';
+
+import {
+  MsalModule,
+  MsalInterceptor,
+  MSAL_CONFIG,
+  MSAL_CONFIG_ANGULAR,
+  MsalService,
+  MsalAngularConfiguration
+} from '@azure/msal-angular';
+import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
+import { Configuration } from 'msal';
+
+export const protectedResourceMap: [string, string[]][] = [
+  ['https://graph.microsoft.com/v1.0/me', ['user.read']]
+];
+
+const isIE = window.navigator.userAgent.indexOf("MSIE ") > -1 || window.navigator.userAgent.indexOf("Trident/") > -1;
+
+function MSALConfigFactory(): Configuration {
+  return {
+    auth: {
+      clientId: 'd15732c7-13e4-41fc-9d8f-c7776b875e58',
+      authority: "https://login.microsoftonline.com/cd25c694-bfb8-48f4-9d0d-b9af282c4ab4",
+      validateAuthority: true,
+      redirectUri: "http://localhost:4200/",
+      postLogoutRedirectUri: "http://localhost:4200/",
+      navigateToLoginRequestUrl: true,
+    },
+    cache: {
+      cacheLocation: "localStorage",
+      storeAuthStateInCookie: isIE, // set to true for IE 11
+    },
+  };
+}
+
+function MSALAngularConfigFactory(): MsalAngularConfiguration {
+  return {
+    popUp: !isIE,
+    consentScopes: [
+      "user.read",
+      "openid",
+      "profile"
+    ],
+    unprotectedResources: ["https://www.microsoft.com/en-us/"],
+    protectedResourceMap,
+    extraQueryParameters: {}
+  };
+}
+
 @NgModule({
-  imports: [CommonModule]
+  imports: [
+    CommonModule,
+    MsalModule
+  ],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
+  providers: [
+    GraphService,
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: MsalInterceptor,
+      multi: true
+    },
+    {
+      provide: MSAL_CONFIG,
+      useFactory: MSALConfigFactory
+    },
+    {
+      provide: MSAL_CONFIG_ANGULAR,
+      useFactory: MSALAngularConfigFactory
+    },
+    MsalService
+  ],
 })
 export class AngularAADAuthModule {}
