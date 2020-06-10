c651977327de686edd9e7b74082b9971f83961d1
James Schumann <james@schumann.ca>
2020-06-10 10:51:15 -0400

    delete old SendMessage function; keep code handy in signalr-send-user

--- apps/houston-functions/signalr-send-user/index.ts
+++ apps/houston-functions/signalr-send-user/index.ts
@@ -2,6 +2,21 @@ import { AzureFunction, Context } from "@azure/functions"
 
 const signalrSendUser: AzureFunction = async function (context: Context, triggerMessage: string): Promise<void> {
     context.log('Queue trigger function processed work item', triggerMessage);
+
+    // const message = req.body;
+    // message.sender = req.headers && req.headers['x-ms-client-principal-name'] || '';
+    
+    // let recipientUserId = '';
+    // if (message.recipient) {
+        // recipientUserId = message.recipient;
+        // message.isPrivate = true;
+    // }
+    
+    // return {
+        // 'userId': recipientUserId,
+        // 'target': 'newMessage',
+        // 'arguments': [ message ]
+    // };
 };
 
 export default signalrSendUser;
