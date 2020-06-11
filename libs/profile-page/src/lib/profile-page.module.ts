8636988d02095589808f8b0d8035bfda8a73f42d
James Schumann <james@schumann.ca>
2020-06-11 15:47:33 -0400

    copy over profile panel code to profile page

--- libs/profile-page/src/lib/profile-page.module.ts
+++ libs/profile-page/src/lib/profile-page.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 
@@ -15,6 +15,7 @@ import { ProfilePageComponent } from './profile-page/profile-page.component';
       {path: '', pathMatch: 'full', component: ProfilePageComponent}
     ])
   ],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
   exports: [
     ProfilePageComponent
   ]
--- libs/profile-page/src/lib/profile-page/profile-page.component.ts
+++ libs/profile-page/src/lib/profile-page/profile-page.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import '@microsoft/mgt/dist/es6/components/mgt-person/mgt-person';
+import { User } from "@microsoft/microsoft-graph-types";
+import { GraphService } from '@cosmos/msgraph-service';
 
 @Component({
   selector: 'cosmos-profile-page',
@@ -6,10 +9,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./profile-page.component.scss']
 })
 export class ProfilePageComponent implements OnInit {
+  profile: User;
+  profile$ = this.graphService.getProfile();
 
-  constructor() { }
+  constructor(
+    private graphService: GraphService
+  ) { }
 
   ngOnInit(): void {
+    this.getProfile();
   }
 
+  profileString() {
+    return JSON.stringify(this.profile);
+  }
+
+  getProfile() {
+    this.profile$.subscribe(profile => 
+      this.profile = profile
+    );
+  }
 }
