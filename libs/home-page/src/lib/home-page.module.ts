91ef0d0193f4cc3a445e079687715246654d0219
James Schumann <james@schumann.ca>
2020-06-11 13:42:00 -0400

    ng g component home-page --project=home-page

--- libs/home-page/src/lib/home-page.module.ts
+++ libs/home-page/src/lib/home-page.module.ts
@@ -1,7 +1,9 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { HomePageComponent } from './home-page/home-page.component';
 
 @NgModule({
-  imports: [CommonModule]
+  imports: [CommonModule],
+  declarations: [HomePageComponent]
 })
 export class HomePageModule {}
