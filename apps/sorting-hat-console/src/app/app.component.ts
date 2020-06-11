165056e05c13a3b23ba3f71191e35db3090ce117
James Schumann <james@schumann.ca>
2020-06-11 17:00:03 -0400

    harmonize sorting-hat and viewfinder app.component

--- apps/sorting-hat-console/src/app/app.component.ts
+++ apps/sorting-hat-console/src/app/app.component.ts
@@ -1,13 +1,65 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { BroadcastService, MsalService } from '@azure/msal-angular';
+import { Logger, CryptoUtils } from 'msal';
 
-import { Menu } from "@cosmos/ui";
-import { UINavigationService } from "@cosmos/ui-navigation";
+import { AppSettingsService } from "@cosmos/app-settings";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  ngOnInit() {}
+export class AppComponent implements OnInit {
+  isIframe = false;
+  loggedIn = false;
+
+  constructor(
+    private broadcastService: BroadcastService,
+    private authService: MsalService,
+    private appSettingsService: AppSettingsService
+  ) {}
+
+  ngOnInit(): void {
+    this.appSettingsService.setAppName('Sorting Hat');
+
+    this.isIframe = window !== window.parent && !window.opener;
+
+    this.checkoutAccount();
+
+    this.broadcastService.subscribe('msal:loginSuccess', () => {
+      this.checkoutAccount();
+    });
+
+    this.authService.handleRedirectCallback((authError, response) => {
+      if (authError) {
+        console.error('Redirect Error: ', authError.errorMessage);
+        return;
+      }
+
+      console.log('Redirect Success: ', response);
+    });
+
+    this.authService.setLogger(new Logger((logLevel, message, piiEnabled) => {
+      console.log('MSAL Logging: ', message);
+    }, {
+      correlationId: CryptoUtils.createNewGuid(),
+      piiLoggingEnabled: false
+    }));
+  }
+
+  getAppName() {
+    return this.appSettingsService.getAppName();
+  }
+
+  checkoutAccount() {
+    this.loggedIn = !!this.authService.getAccount();
+  }
+
+  login() {
+    this.authService.loginPopup();
+  }
+
+  logout() {
+    this.authService.logout();
+  }
 }
