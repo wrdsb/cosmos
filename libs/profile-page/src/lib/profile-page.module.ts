33e9bf1f4edddab846b435280df0c7b18497a35d
James Schumann <james@schumann.ca>
2020-06-11 14:19:47 -0400

    ng g component profile-page --project=profile-page

--- libs/profile-page/src/lib/profile-page.module.ts
+++ libs/profile-page/src/lib/profile-page.module.ts
@@ -1,7 +1,9 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ProfilePageComponent } from './profile-page/profile-page.component';
 
 @NgModule({
-  imports: [CommonModule]
+  imports: [CommonModule],
+  declarations: [ProfilePageComponent]
 })
 export class ProfilePageModule {}
