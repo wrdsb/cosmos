16f869809b69f442e545fd46d3e025c9a9c94232
James Schumann <james@schumann.ca>
2020-06-15 10:03:12 -0400

    remove appName setting from app-settings service

--- libs/app-settings/src/lib/app-settings.service.ts
+++ libs/app-settings/src/lib/app-settings.service.ts
@@ -5,16 +5,5 @@ import { Observable, of, BehaviorSubject } from 'rxjs';
   providedIn: 'root'
 })
 export class AppSettingsService {
-  private appName: string;
-
   constructor() { }
-
-  setAppName(name: string): void {
-    this.appName = name;
-  }
-
-  getAppName(): string {
-    return this.appName;
-  }
-
 }
