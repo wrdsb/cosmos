a59bdf4a17cba498dc9b57310b5bbf32547d88b8
James Schumann <james@schumann.ca>
2020-06-18 15:09:09 -0400

    set up environment service

--- libs/environment/src/index.ts
+++ libs/environment/src/index.ts
@@ -1,1 +1,2 @@
 export * from './lib/environment.module';
+export * from './lib/environment.service';
--- libs/environment/src/lib/environment.service.ts
+++ libs/environment/src/lib/environment.service.ts
@@ -1,9 +1,29 @@
 import { Injectable } from '@angular/core';
+import { Environment } from "@cosmos/types";
 
 @Injectable({
   providedIn: 'root'
 })
 export class EnvironmentService {
+  readonly production: boolean;
+  readonly appName: string;
 
-  constructor() { }
+  readonly aadClientId: string;
+  readonly aadAuthority: string;
+  readonly aadValidateAuthority: boolean;
+  readonly aadRedirectUri: string;
+  readonly aadPostLogoutRedirectUri: string;
+  readonly aadNavigateToLoginRequestUrl: boolean;
+
+  constructor(environment: Environment) {
+    this.production = environment.production;
+    this.appName= environment.appName;
+
+    this.aadClientId = environment.aadClientId;
+    this.aadAuthority = environment.aadAuthority;
+    this.aadValidateAuthority = environment.aadValidateAuthority;
+    this.aadRedirectUri = environment.aadRedirectUri;
+    this.aadPostLogoutRedirectUri = environment.aadPostLogoutRedirectUri;
+    this.aadNavigateToLoginRequestUrl = environment.aadNavigateToLoginRequestUrl;
+  }
 }
