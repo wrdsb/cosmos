b11d1acd4d8bed4b3a133d640aecb842925e7a49
James Schumann <james@schumann.ca>
2020-06-23 09:06:35 -0400

    stub aad-user-get interfaces. closes #216

---
+++ libs/types/src/lib/azure-functions/hagar-functions/aad-user-get.ts
@@ -0,0 +1,38 @@
+import * as Cosmos from "../common";
+
+interface AADUserGetFunctionRequest extends Cosmos.FunctionRequest {
+    readonly payload: AADUserGetFunctionRequestPayload;
+}
+
+interface AADUserGetFunctionResponse extends Cosmos.FunctionResponse {}
+
+interface AADUserGetFunctionLogObject extends Cosmos.FunctionLogObject {}
+
+interface AADUserGetFunctionCallbackMessage extends Cosmos.FunctionCallbackMessage {}
+
+interface AADUserGetFunctionInvocationEvent extends Cosmos.FunctionInvocationEvent {}
+
+interface AADUserGetFunctionRequestPayload extends Cosmos.FunctionRequestPayload {
+    readonly groupID: string;
+}
+
+interface AADUserGetFunctionResponsePayload extends Cosmos.FunctionResponsePayload {}
+
+interface AADUserGetFunctionLogObjectPayload extends Cosmos.FunctionLogObjectPayload {}
+
+interface AADUserGetFunctionCallbackMessagePayload extends Cosmos.FunctionCallbackMessagePayload {}
+
+interface AADUserGetFunctionInvocationEventPayload extends Cosmos.FunctionInvocationEventPayload {}
+
+export {
+    AADUserGetFunctionRequest,
+    AADUserGetFunctionResponse,
+    AADUserGetFunctionLogObject,
+    AADUserGetFunctionCallbackMessage,
+    AADUserGetFunctionInvocationEvent,
+    AADUserGetFunctionRequestPayload,
+    AADUserGetFunctionResponsePayload,
+    AADUserGetFunctionLogObjectPayload,
+    AADUserGetFunctionCallbackMessagePayload,
+    AADUserGetFunctionInvocationEventPayload
+}
