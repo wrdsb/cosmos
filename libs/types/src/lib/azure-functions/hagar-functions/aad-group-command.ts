925ee6740e848f001355304a105faec3385360ca
James Schumann <james@schumann.ca>
2020-06-23 07:56:49 -0400

    stub aad-group-command interfaces. closes #232

---
+++ libs/types/src/lib/azure-functions/hagar-functions/aad-group-command.ts
@@ -0,0 +1,44 @@
+import * as Cosmos from "../common";
+
+interface AADGroupCommandFunctionRequest extends Cosmos.FunctionRequest {
+    readonly payload: AADGroupCommandFunctionRequestPayload;
+}
+
+interface AADGroupCommandFunctionResponse extends Cosmos.FunctionResponse {
+    readonly payload: AADGroupCommandFunctionResponsePayload;
+}
+
+interface AADGroupCommandFunctionLogObject extends Cosmos.FunctionLogObject {
+    readonly payload: AADGroupCommandFunctionResponsePayload;
+}
+
+interface AADGroupCommandFunctionCallbackMessage extends Cosmos.FunctionCallbackMessage {}
+
+interface AADGroupCommandFunctionInvocationEvent extends Cosmos.FunctionInvocationEvent {
+    readonly data: AADGroupCommandFunctionInvocationEventPayload;
+}
+
+interface AADGroupCommandFunctionRequestPayload extends Cosmos.FunctionRequestPayload {
+}
+
+interface AADGroupCommandFunctionResponsePayload extends Cosmos.FunctionResponsePayload {
+}
+
+interface AADGroupCommandFunctionLogObjectPayload extends Cosmos.FunctionLogObjectPayload {}
+
+interface AADGroupCommandFunctionCallbackMessagePayload extends Cosmos.FunctionCallbackMessagePayload {}
+
+interface AADGroupCommandFunctionInvocationEventPayload extends Cosmos.FunctionInvocationEventPayload {}
+
+export {
+    AADGroupCommandFunctionRequest,
+    AADGroupCommandFunctionResponse,
+    AADGroupCommandFunctionLogObject,
+    AADGroupCommandFunctionCallbackMessage,
+    AADGroupCommandFunctionInvocationEvent,
+    AADGroupCommandFunctionRequestPayload,
+    AADGroupCommandFunctionResponsePayload,
+    AADGroupCommandFunctionLogObjectPayload,
+    AADGroupCommandFunctionCallbackMessagePayload,
+    AADGroupCommandFunctionInvocationEventPayload
+}
