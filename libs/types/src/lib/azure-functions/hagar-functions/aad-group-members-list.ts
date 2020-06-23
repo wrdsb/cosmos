297d44271cd269c5adf59a4ad18cd6780b122923
James Schumann <james@schumann.ca>
2020-06-23 08:58:50 -0400

    stub aad-group-members-list interfaces. closes #222

---
+++ libs/types/src/lib/azure-functions/hagar-functions/aad-group-members-list.ts
@@ -0,0 +1,35 @@
+import * as Cosmos from "../common";
+
+interface AADGroupMembersListFunctionRequest extends Cosmos.FunctionRequest {}
+
+interface AADGroupMembersListFunctionResponse extends Cosmos.FunctionResponse {}
+
+interface AADGroupMembersListFunctionLogObject extends Cosmos.FunctionLogObject {}
+
+interface AADGroupMembersListFunctionCallbackMessage extends Cosmos.FunctionCallbackMessage {}
+
+interface AADGroupMembersListFunctionInvocationEvent extends Cosmos.FunctionInvocationEvent {}
+
+// No payload required
+interface AADGroupMembersListFunctionRequestPayload extends Cosmos.FunctionRequestPayload {}
+
+interface AADGroupMembersListFunctionResponsePayload extends Cosmos.FunctionResponsePayload {}
+
+interface AADGroupMembersListFunctionLogObjectPayload extends Cosmos.FunctionLogObjectPayload {}
+
+interface AADGroupMembersListFunctionCallbackMessagePayload extends Cosmos.FunctionCallbackMessagePayload {}
+
+interface AADGroupMembersListFunctionInvocationEventPayload extends Cosmos.FunctionInvocationEventPayload {}
+
+export {
+    AADGroupMembersListFunctionRequest,
+    AADGroupMembersListFunctionResponse,
+    AADGroupMembersListFunctionLogObject,
+    AADGroupMembersListFunctionCallbackMessage,
+    AADGroupMembersListFunctionInvocationEvent,
+    AADGroupMembersListFunctionRequestPayload,
+    AADGroupMembersListFunctionResponsePayload,
+    AADGroupMembersListFunctionLogObjectPayload,
+    AADGroupMembersListFunctionCallbackMessagePayload,
+    AADGroupMembersListFunctionInvocationEventPayload
+}
