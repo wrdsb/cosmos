3902b4668cd7acda9e8dd5e3aef14a06cfba83b6
James Schumann <james@schumann.ca>
2020-06-23 10:57:29 -0400

    export all stubbed interfaces

--- libs/types/src/lib/azure-functions/hagar-functions/index.ts
+++ libs/types/src/lib/azure-functions/hagar-functions/index.ts
@@ -1,5 +1,23 @@
+export * from './aad-group-command';
 export * from './aad-group-create';
 export * from './aad-group-delete';
 export * from './aad-group-get';
+export * from './aad-group-query';
+export * from './aad-group-store';
 export * from './aad-group-update';
+//export * from './aad-groups-delta';
 export * from './aad-groups-list';
+
+export * from './aad-group-member-add';
+export * from './aad-group-member-command';
+export * from './aad-group-member-get';
+export * from './aad-group-member-query';
+export * from './aad-group-member-remove';
+export * from './aad-group-member-store';
+export * from './aad-group-members-list';
+
+export * from './aad-user-command';
+export * from './aad-user-get';
+export * from './aad-user-store';
+//export * from './aad-users-delta';
+export * from './aad-users-list';
