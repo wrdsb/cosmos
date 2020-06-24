83fc5b543a8dd52cad837fb403b184052ee76a4c
James Schumann <james@schumann.ca>
2020-06-24 13:34:51 -0400

    add default routing file to console apps without existing routing

---
+++ apps/flenderson-console/src/app/app-routing.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { MsalGuard } from '@azure/msal-angular';
+
+const routes: Routes = [
+  {
+    path: 'profile',
+    loadChildren: () => import('@cosmos/profile-page').then(m => m.ProfilePageModule),
+    canActivate: [
+      MsalGuard
+    ]
+  },
+  {
+    path: '',
+    loadChildren: () => import('@cosmos/home-page').then(m => m.HomePageModule)
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes, { useHash: false })],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
