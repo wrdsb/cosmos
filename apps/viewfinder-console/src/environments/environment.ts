0aad703aa54365c09b6cc61afc6f8f3bae9589f8
James Schumann <james@schumann.ca>
2020-06-24 11:18:14 -0400

    assign unique local port to each console app in development environment

--- apps/viewfinder-console/src/environments/environment.ts
+++ apps/viewfinder-console/src/environments/environment.ts
@@ -7,7 +7,7 @@ export const environment: Environment = {
   aadClientId: 'd15732c7-13e4-41fc-9d8f-c7776b875e58',
   aadAuthority: "https://login.microsoftonline.com/cd25c694-bfb8-48f4-9d0d-b9af282c4ab4",
   aadValidateAuthority: true,
-  aadRedirectUri: "http://localhost:4200/",
-  aadPostLogoutRedirectUri: "http://localhost:4200/",
+  aadRedirectUri: "http://localhost:4290/",
+  aadPostLogoutRedirectUri: "http://localhost:4290/",
   aadNavigateToLoginRequestUrl: true,
 };
