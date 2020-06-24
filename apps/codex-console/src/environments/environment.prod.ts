b74189c2ff54f5eb8a9b99888bf03b46dccee084
James Schumann <james@schumann.ca>
2020-06-24 10:45:44 -0400

    add titles and AAD settings to environments

--- apps/codex-console/src/environments/environment.prod.ts
+++ apps/codex-console/src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
-export const environment = {
-  production: true
+import { Environment } from "@cosmos/types";
+
+export const environment: Environment = {
+  production: true,
+  appName: 'Codex',
+
+  aadClientId: 'd15732c7-13e4-41fc-9d8f-c7776b875e58',
+  aadAuthority: "https://login.microsoftonline.com/cd25c694-bfb8-48f4-9d0d-b9af282c4ab4",
+  aadValidateAuthority: true,
+  aadRedirectUri: "http://localhost:4200/",
+  aadPostLogoutRedirectUri: "http://localhost:4200/",
+  aadNavigateToLoginRequestUrl: true,
 };
