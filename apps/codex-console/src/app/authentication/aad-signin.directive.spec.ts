484f272b868c52ff63d94fe5f76ab1f24685ed26
James Schumann <james@schumann.ca>
2020-06-29 11:38:28 -0400

    remove old angular fire (firebase) auth code

--- apps/codex-console/src/app/authentication/aad-signin.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { AADSigninDirective } from './aad-signin.directive';
-
-describe('AadSigninDirective', () => {
-  it('should create an instance', () => {
-    const directive = new AADSigninDirective();
-    expect(directive).toBeTruthy();
-  });
-});
--- apps/houston-console/src/app/authentication/aad-signin.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { AadSigninDirective } from './aad-signin.directive';
-
-describe('AadSigninDirective', () => {
-  it('should create an instance', () => {
-    const directive = new AadSigninDirective();
-    expect(directive).toBeTruthy();
-  });
-});
