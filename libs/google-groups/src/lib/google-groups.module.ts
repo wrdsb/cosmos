865d31d68a1b7dd5926d356b4a4a68b59f84cd69
James Schumann <james@schumann.ca>
2020-06-29 14:59:36 -0400

    update google-groups module with components stolen from houston

--- libs/google-groups/src/lib/google-groups.module.ts
+++ libs/google-groups/src/lib/google-groups.module.ts
@@ -1,7 +1,25 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { GoogleGroupsRoutingModule } from './google-groups-routing.module';
+import { GroupDetailComponent } from './group-detail/group-detail.component';
+import { GroupsListComponent } from './groups-list/groups-list.component';
+import { GroupsHomeComponent } from './groups-home/groups-home.component';
+
 @NgModule({
-  imports: [CommonModule]
+  declarations: [
+    GroupDetailComponent,
+    GroupsListComponent,
+    GroupsHomeComponent
+  ],
+  imports: [
+    CommonModule,
+    GoogleGroupsRoutingModule
+  ],
+  exports: [
+    GroupDetailComponent,
+    GroupsListComponent,
+    GroupsHomeComponent
+  ]
 })
 export class GoogleGroupsModule {}
