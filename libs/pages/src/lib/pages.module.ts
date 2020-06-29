4aeeea2e6f1c708f111024a5ae3973fe23c2f67d
James Schumann <james@schumann.ca>
2020-06-29 11:08:44 -0400

    move home-page component into pages lib

--- libs/pages/src/lib/pages.module.ts
+++ libs/pages/src/lib/pages.module.ts
@@ -1,7 +1,22 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+
+import { HomePageComponent } from './home-page/home-page.component';
 
 @NgModule({
-  imports: [CommonModule]
+  declarations: [
+    HomePageComponent
+  ],
+  imports: [
+    CommonModule,
+
+    RouterModule.forChild([
+      {path: '', pathMatch: 'full', component: HomePageComponent}
+    ])
+  ],
+  exports: [
+    HomePageComponent
+  ]
 })
 export class PagesModule {}
