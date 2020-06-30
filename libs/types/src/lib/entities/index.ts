b66c8f5ae35bf906dabc11d50b55f18b506bc1d7
James Schumann <james@schumann.ca>
2020-06-30 12:17:36 -0400

    export new AAD and HAGAR entities

--- libs/types/src/lib/entities/index.ts
+++ libs/types/src/lib/entities/index.ts
@@ -1,7 +1,9 @@
 export * from './conceptual';
 
+export * from './aad';
 export * from './codex';
 export * from './google';
+export * from './hagar';
 export * from './igor';
 export * from './ipps';
 export * from './sparkrock';
