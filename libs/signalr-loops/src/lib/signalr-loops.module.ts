e7eead502240768ff3211ec68780b5ab81f8d95a
James Schumann <james@schumann.ca>
2020-06-30 09:15:11 -0400

    move over code from houston-console loops module

--- libs/signalr-loops/src/lib/signalr-loops.module.ts
+++ libs/signalr-loops/src/lib/signalr-loops.module.ts
@@ -1,9 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+
+import { LoopsRoutingModule } from './loops-routing.module';
 import { UserLoopComponent } from './user-loop/user-loop.component';
 
 @NgModule({
-  imports: [CommonModule],
-  declarations: [UserLoopComponent]
+  declarations: [
+    UserLoopComponent
+  ],
+  imports: [
+    CommonModule
+  ],
+  exports: [
+    LoopsRoutingModule
+  ]
 })
 export class SignalrLoopsModule {}
--- libs/signalr-loops/src/lib/user-loop/user-loop.component.ts
+++ libs/signalr-loops/src/lib/user-loop/user-loop.component.ts
@@ -1,15 +1,41 @@
 import { Component, OnInit } from '@angular/core';
 
+//import { HoustonSignalrService } from "src/app/shared/services/houston-signalr.service";
+import * as SignalR from "@aspnet/signalr";
+
 @Component({
   selector: 'cosmos-user-loop',
   templateUrl: './user-loop.component.html',
   styleUrls: ['./user-loop.component.scss']
 })
 export class UserLoopComponent implements OnInit {
+  messages = [];
 
+  //constructor(private signalRService: HoustonSignalrService) { }
   constructor() { }
 
-  ngOnInit(): void {
+  ngOnInit() {
+    //this.signalRService.init();
+    const connection = new SignalR.HubConnectionBuilder()
+      .configureLogging(SignalR.LogLevel.Information)
+      .withUrl("https://wrdsb-houston.azurewebsites.net/api")
+      .build();
+
+    connection.on('newMessage', this.onNewMessage.bind(this));
+    connection.onclose(() => console.log('disconnected'));
+
+    connection.start().then(function () {
+      console.log('SignalR Connected!');
+    }).catch(function (err) {
+      return console.error(err.toString());
+    });
   }
 
+  onNewMessage(message) {
+    if (!message.sender) {
+      message.sender = "anonymous";
+    }
+    console.log(message);
+    this.messages.unshift(message);
+  }
 }
