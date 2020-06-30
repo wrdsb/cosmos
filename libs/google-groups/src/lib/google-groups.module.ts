85c0a0931ca55234e5d13a4efc4ff5df2100df43
James Schumann <james@schumann.ca>
2020-06-30 10:52:50 -0400

    logical sort of imports, rather then creation order

--- libs/google-groups/src/lib/google-groups.module.ts
+++ libs/google-groups/src/lib/google-groups.module.ts
@@ -2,10 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { GoogleGroupsRoutingModule } from './google-groups-routing.module';
-import { GroupDetailComponent } from './group-detail/group-detail.component';
-import { GroupsListComponent } from './groups-list/groups-list.component';
+
 import { GroupsHomeComponent } from './groups-home/groups-home.component';
 import { GroupsSearchComponent } from './groups-search/groups-search.component';
+
+import { GroupDetailComponent } from './group-detail/group-detail.component';
+import { GroupsListComponent } from './groups-list/groups-list.component';
+
 import { MembershipsListComponent } from './memberships-list/memberships-list.component';
 import { MembershipDetailComponent } from './membership-detail/membership-detail.component';
 
