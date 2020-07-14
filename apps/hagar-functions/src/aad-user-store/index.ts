9802707f5087b02b0401b95ac7590fc3125f0b8f
James Schumann <james@schumann.ca>
2020-07-14 15:32:23 -0400

    aad-user-store working

--- apps/hagar-functions/src/aad-user-store/index.ts
+++ apps/hagar-functions/src/aad-user-store/index.ts
@@ -25,11 +25,38 @@ const aadUserStore: AzureFunction = async function (context: Context, triggerMes
     ];
 
     const triggerObject = triggerMessage as AADUserStoreFunctionRequest;
+    const operation = triggerObject.operation;
     const payload = triggerObject.payload as AADUserStoreFunctionRequestPayload;
 
+    let oldRecord = context.bindings.recordIn;
+    let newRecord;
     let result;
 
-    const logPayload = result;
+    let statusCode;
+    let statusMessage;
+
+    switch (operation) {
+        case 'delete':
+            result = doDelete(oldRecord, payload);
+            statusCode = '200';
+            statusMessage = 'Success: Marked user record deleted.';
+            break;
+        case 'patch':
+            result = doPatch(oldRecord, payload);
+            statusCode = '200';
+            statusMessage = 'Success: Patched user record.';
+            break;
+        case 'replace':
+            result = doReplace(oldRecord, payload);
+            statusCode = '200';
+            statusMessage = 'Success: Replaced user record.';
+            break;
+        default:
+            break;
+    }
+
+    context.bindings.recordOut = result.newRecord;
+    const logPayload = result.event;
     context.log(logPayload);
 
     const logObject = await createLogObject(functionInvocationID, functionInvocationTime, functionName, logPayload);
@@ -57,6 +84,189 @@ const aadUserStore: AzureFunction = async function (context: Context, triggerMes
     context.log(invocationEvent);
 
     context.done(null, logBlob);
+
+    function doDelete(oldRecord, payload)
+    {
+        let event = {};
+        let newRecord = {
+            created_at: '',
+            updated_at: '',
+            deleted_at: '',
+            deleted: false,
+        };
+
+        // check for existing record
+        if (!oldRecord) {
+            newRecord = Object.assign(newRecord, payload);
+            newRecord.created_at = functionInvocationTimestamp;
+            newRecord.updated_at = functionInvocationTimestamp;
+
+            // mark the record as deleted
+            newRecord.deleted_at = functionInvocationTimestamp;
+            newRecord.deleted = true;
+
+            event = craftUserDeleteEvent(oldRecord, newRecord);
+
+        } else {
+            newRecord = Object.assign(newRecord, oldRecord);
+
+            // mark the record as deleted
+            newRecord.deleted_at = functionInvocationTimestamp;
+            newRecord.deleted = true;
+
+            event = craftUserDeleteEvent(oldRecord, newRecord);
+        }
+
+        return {event: event, newRecord: newRecord};
+    }
+
+    function doPatch(oldRecord, payload)
+    {
+        let event = {};
+        let newRecord = {
+            created_at: '',
+            updated_at: '',
+            deleted_at: '',
+            deleted: false,
+        };
+
+        if (!oldRecord) {
+            newRecord = Object.assign(newRecord, payload);
+            newRecord.created_at = functionInvocationTimestamp;
+            newRecord.updated_at = functionInvocationTimestamp;
+    
+            // patching a record implicitly undeletes it
+            newRecord.deleted_at = '';
+            newRecord.deleted = false;
+    
+            event = craftUserCreateEvent(oldRecord, newRecord);
+
+        } else {
+            // Merge request object into current record
+            newRecord = Object.assign(newRecord, oldRecord, payload);
+            newRecord.updated_at = functionInvocationTimestamp;
+    
+            // patching a record implicitly undeletes it
+            newRecord.deleted_at = '';
+            newRecord.deleted = false;
+
+            event = craftUserUpdateEvent(oldRecord, newRecord);
+        }
+
+        return {event: event, newRecord: newRecord};
+    }
+    
+    function doReplace(oldRecord, payload)
+    {
+        let event = {};
+        let newRecord = {
+            created_at: '',
+            updated_at: '',
+            deleted_at: '',
+            deleted: false,
+        };
+
+        newRecord = Object.assign(newRecord, payload);
+
+        if (!oldRecord) {
+            newRecord.created_at = functionInvocationTimestamp;
+            newRecord.updated_at = functionInvocationTimestamp;
+
+            // replacing a record implicitly undeletes it
+            newRecord.deleted_at = '';
+            newRecord.deleted = false;
+        
+            event = craftUserCreateEvent(oldRecord, newRecord);
+
+        } else {
+            newRecord.created_at = oldRecord.created_at;
+            newRecord.updated_at = functionInvocationTimestamp;
+
+            // replacing a record implicitly undeletes it
+            newRecord.deleted_at = '';
+            newRecord.deleted = false;
+        
+            event = craftUserUpdateEvent(oldRecord, newRecord);
+        }
+
+        return {event: event, newRecord: newRecord};
+    }
+
+    function craftUserCreateEvent(old_record, new_record)
+    {
+        let event_type = 'HAGAR.User.Create';
+        let source = 'create';
+        let schema = 'create';
+        let label = `User ${new_record.id} created.`;
+        let payload = {
+            record: new_record
+        };
+
+        let event = craftEvent(new_record.id, source, schema, event_type, label, payload);
+        return event;
+    }
+    
+    function craftUserUpdateEvent(old_record, new_record)
+    {
+        let event_type = 'HAGAR.User.Update';
+        let source = 'update';
+        let schema = 'update';
+        let label = `User ${new_record.id} updated.`;
+        let payload = {
+            old_record: old_record,
+            new_record: new_record,
+        };
+
+        let event = craftEvent(new_record.id, source, schema, event_type, label, payload);
+        return event;
+    }
+
+    function craftUserDeleteEvent(old_record, new_record)
+    {
+        let event_type = 'HAGAR.User.Delete';
+        let source = 'delete';
+        let schema = 'delete';
+        let label = `User ${new_record.id} deleted.`;
+        let payload = {
+            record: old_record
+        };
+
+        let event = craftEvent(old_record.id, source, schema, event_type, label, payload);
+        return event;
+    }
+
+    function craftEvent(recordID, source, schema, event_type, label, payload) {
+        let event = {
+            id: `${event_type}-${context.executionContext.invocationId}`,
+            time: functionInvocationTimestamp,
+
+            type: event_type,
+            source: `/hagar/user/${recordID}/${source}`,
+            schemaURL: `ca.wrdsb.hagar.user.${schema}.json`,
+
+            label: label,
+            tags: [
+                "hagar", 
+                "aad_user_change",
+                "user_change"
+            ], 
+
+            data: {
+                function_name: context.executionContext.functionName,
+                invocation_id: context.executionContext.invocationId,
+                result: {
+                    payload: payload 
+                },
+            },
+
+            eventTypeVersion: "0.1",
+            specversion: "0.2",
+            contentType: "application/json"
+        };
+
+        // TODO: check message length
+        return event;
+    }
 };
 
 export default aadUserStore;
