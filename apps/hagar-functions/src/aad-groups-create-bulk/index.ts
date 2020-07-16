0fbedc02cbc477aa264eedba6abd5207cbb07419
James Schumann <james@schumann.ca>
2020-07-16 14:05:12 -0400

    add apps/hagar-functions/src/aad-groups-create-bulk

---
+++ apps/hagar-functions/src/aad-groups-create-bulk/index.ts
@@ -0,0 +1,72 @@
+import { AzureFunction, Context } from "@azure/functions";
+import { createLogObject } from "@cosmos/azure-functions-shared";
+import { storeLogBlob } from "@cosmos/azure-functions-shared";
+import { createCallbackMessage } from "@cosmos/azure-functions-shared";
+import { createEvent } from "@cosmos/azure-functions-shared";
+import { AADGroupsCreateBulkFunctionRequest, AADGroupsCreateBulkFunctionRequestPayload } from "@cosmos/types";
+
+const aadGroupsCreateBulk: AzureFunction = async function (context: Context, triggerMessage: AADGroupsCreateBulkFunctionRequest): Promise<void> {
+    const functionInvocationID = context.executionContext.invocationId;
+    const functionInvocationTime = new Date();
+    const functionInvocationTimestamp = functionInvocationTime.toJSON();  // format: 2012-04-23T18:25:43.511Z
+
+    const functionName = context.executionContext.functionName;
+    const functionEventType = 'WRDSB.HAGAR.AAD.Groups.Create.Bulk';
+    const functionEventID = `hagar-functions-${functionName}-${functionInvocationID}`;
+    const functionLogID = `${functionInvocationTime.getTime()}-${functionInvocationID}`;
+
+    const logStorageAccount = process.env['storageAccount'];
+    const logStorageKey = process.env['storageKey'];
+    const logStorageContainer = 'function-aad-groups-create-bulk-logs';
+
+    const eventLabel = '';
+    const eventTags = [
+        "hagar", 
+    ];
+
+    const triggerObject = triggerMessage as AADGroupsCreateBulkFunctionRequest;
+    const payload = triggerObject.payload as AADGroupsCreateBulkFunctionRequestPayload;
+
+    let queueMessages = [];
+
+    payload.groups.forEach((group) => {
+        queueMessages.push({
+            payload: {
+                group: group
+            }
+        });
+    });
+
+    context.bindings.createGroup = queueMessages;
+
+    let logPayload = {};
+    context.log(logPayload);
+
+    const logObject = await createLogObject(functionInvocationID, functionInvocationTime, functionName, logPayload);
+    const logBlob = await storeLogBlob(logStorageAccount, logStorageKey, logStorageContainer, logObject);
+    context.log(logBlob);
+
+    const callbackMessage = await createCallbackMessage(logObject, 200);
+    context.bindings.callbackMessage = JSON.stringify(callbackMessage);
+    context.log(callbackMessage);
+
+    const invocationEvent = await createEvent(
+        functionInvocationID,
+        functionInvocationTime,
+        functionInvocationTimestamp,
+        functionName,
+        functionEventType,
+        functionEventID,
+        functionLogID,
+        logStorageAccount,
+        logStorageContainer,
+        eventLabel,
+        eventTags
+    );
+    context.bindings.flynnEvent = JSON.stringify(invocationEvent);
+    context.log(invocationEvent);
+
+    context.done(null, logBlob);
+};
+
+export default aadGroupsCreateBulk;
