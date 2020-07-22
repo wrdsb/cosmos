15aa4fe9535b037635509ce37ed57b58801a1e5a
James Schumann <james@schumann.ca>
2020-07-22 11:03:33 -0400

    add aad-users-get-bulk

---
+++ apps/hagar-functions/src/aad-users-get-bulk/index.ts
@@ -0,0 +1,72 @@
+import { AzureFunction, Context } from "@azure/functions";
+import { createLogObject } from "@cosmos/azure-functions-shared";
+import { storeLogBlob } from "@cosmos/azure-functions-shared";
+import { createCallbackMessage } from "@cosmos/azure-functions-shared";
+import { createEvent } from "@cosmos/azure-functions-shared";
+import { AADUsersGetBulkFunctionRequest, AADUsersGetBulkFunctionRequestPayload } from "@cosmos/types";
+
+const aadUsersGetBulk: AzureFunction = async function (context: Context, triggerMessage: AADUsersGetBulkFunctionRequest): Promise<void> {
+    const functionInvocationID = context.executionContext.invocationId;
+    const functionInvocationTime = new Date();
+    const functionInvocationTimestamp = functionInvocationTime.toJSON();  // format: 2012-04-23T18:25:43.511Z
+
+    const functionName = context.executionContext.functionName;
+    const functionEventType = 'WRDSB.HAGAR.AAD.User.Get.Bulk';
+    const functionEventID = `hagar-functions-${functionName}-${functionInvocationID}`;
+    const functionLogID = `${functionInvocationTime.getTime()}-${functionInvocationID}`;
+
+    const logStorageAccount = process.env['storageAccount'];
+    const logStorageKey = process.env['storageKey'];
+    const logStorageContainer = 'function-aad-users-get-bulk-logs';
+
+    const eventLabel = '';
+    const eventTags = [
+        "hagar", 
+    ];
+
+    const triggerObject = triggerMessage as AADUsersGetBulkFunctionRequest;
+    const payload = triggerObject.payload as AADUsersGetBulkFunctionRequestPayload;
+
+    let queueMessages = [];
+
+    payload.userIDs.forEach((user) => {
+        queueMessages.push({
+            payload: {
+                userID: user
+            }
+        });
+    });
+
+    context.bindings.userGetQueue = queueMessages;
+
+    let logPayload = {};
+    context.log(logPayload);
+
+    const logObject = await createLogObject(functionInvocationID, functionInvocationTime, functionName, logPayload);
+    const logBlob = await storeLogBlob(logStorageAccount, logStorageKey, logStorageContainer, logObject);
+    context.log(logBlob);
+
+    const callbackMessage = await createCallbackMessage(logObject, 200);
+    context.bindings.callbackMessage = JSON.stringify(callbackMessage);
+    context.log(callbackMessage);
+
+    const invocationEvent = await createEvent(
+        functionInvocationID,
+        functionInvocationTime,
+        functionInvocationTimestamp,
+        functionName,
+        functionEventType,
+        functionEventID,
+        functionLogID,
+        logStorageAccount,
+        logStorageContainer,
+        eventLabel,
+        eventTags
+    );
+    context.bindings.flynnEvent = JSON.stringify(invocationEvent);
+    context.log(invocationEvent);
+
+    context.done(null, logBlob);
+};
+
+export default aadUsersGetBulk;
