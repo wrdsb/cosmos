07784bd8faa5ceb956bf483848c7fcda33df61f4
James Schumann <james@schumann.ca>
2020-07-22 11:05:34 -0400

    add aad-group-members-list-all

---
+++ apps/hagar-functions/src/aad-group-members-list-all/index.ts
@@ -0,0 +1,69 @@
+import { AzureFunction, Context } from "@azure/functions"
+import { createLogObject } from "@cosmos/azure-functions-shared";
+import { storeLogBlob } from "@cosmos/azure-functions-shared";
+import { createCallbackMessage } from "@cosmos/azure-functions-shared";
+import { createEvent } from "@cosmos/azure-functions-shared";
+import { MSGraphGroupMembershipsAPI } from "../shared/MSGraphGroupMembershipsAPI";
+import { AADGroupMembersListFunctionRequest, AADGroupMembersListFunctionRequestPayload } from "@cosmos/types";
+
+const aadGroupMembersList: AzureFunction = async function (context: Context, triggerMessage: AADGroupMembersListFunctionRequest): Promise<void> {
+    const functionInvocationID = context.executionContext.invocationId;
+    const functionInvocationTime = new Date();
+    const functionInvocationTimestamp = functionInvocationTime.toJSON();  // format: 2012-04-23T18:25:43.511Z
+
+    const functionName = context.executionContext.functionName;
+    const functionEventType = 'WRDSB.HAGAR.AAD.GroupMember.List';
+    const functionEventID = `hagar-functions-${functionName}-${functionInvocationID}`;
+    const functionLogID = `${functionInvocationTime.getTime()}-${functionInvocationID}`;
+
+    const logStorageAccount = process.env['storageAccount'];
+    const logStorageKey = process.env['storageKey'];
+    const logStorageContainer = 'function-aad-group-members-list-logs';
+
+    const eventLabel = '';
+    const eventTags = [
+        "hagar", 
+    ];
+
+    const triggerObject = triggerMessage as AADGroupMembersListFunctionRequest;
+    const payload = triggerObject.payload as AADGroupMembersListFunctionRequestPayload;
+
+    const apiToken = context.bindings.graphToken;
+    const apiClient = new MSGraphGroupMembershipsAPI(apiToken);
+
+    let result = await apiClient.list(payload.groupID);
+    let outputBlob = result;
+
+    context.bindings.outputBlob = outputBlob;
+
+    const logPayload = result;
+    context.log(logPayload);
+
+    const logObject = await createLogObject(functionInvocationID, functionInvocationTime, functionName, logPayload);
+    const logBlob = await storeLogBlob(logStorageAccount, logStorageKey, logStorageContainer, logObject);
+    context.log(logBlob);
+
+    const callbackMessage = await createCallbackMessage(logObject, 200);
+    context.bindings.callbackMessage = JSON.stringify(callbackMessage);
+    context.log(callbackMessage);
+
+    const invocationEvent = await createEvent(
+        functionInvocationID,
+        functionInvocationTime,
+        functionInvocationTimestamp,
+        functionName,
+        functionEventType,
+        functionEventID,
+        functionLogID,
+        logStorageAccount,
+        logStorageContainer,
+        eventLabel,
+        eventTags
+    );
+    context.bindings.flynnEvent = JSON.stringify(invocationEvent);
+    context.log(invocationEvent);
+
+    context.done(null, logBlob);
+};
+
+export default aadGroupMembersList;
