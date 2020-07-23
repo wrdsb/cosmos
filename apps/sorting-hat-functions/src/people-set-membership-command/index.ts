d7de78f00ac085d720178adfbe9a30c44452affb
James Schumann <james@schumann.ca>
2020-07-23 10:34:36 -0400

    stub people-set-memberships-command

---
+++ apps/sorting-hat-functions/src/people-set-membership-command/index.ts
@@ -0,0 +1,63 @@
+import { AzureFunction, Context } from "@azure/functions"
+import { createLogObject } from "@cosmos/azure-functions-shared";
+import { storeLogBlob } from "@cosmos/azure-functions-shared";
+import { createCallbackMessage } from "@cosmos/azure-functions-shared";
+import { createEvent } from "@cosmos/azure-functions-shared";
+
+const peopleSetMembershipCommand: AzureFunction = async function (context: Context, req: any): Promise<void> {
+    const functionInvocationID = context.executionContext.invocationId;
+    const functionInvocationTime = new Date();
+    const functionInvocationTimestamp = functionInvocationTime.toJSON();  // format: 2012-04-23T18:25:43.511Z
+
+    const functionName = context.executionContext.functionName;
+    const functionEventType = 'WRDSB.SortingHat.SortingHat.PeopleSetMembership.Command';
+    const functionEventID = `sorting-hat-functions-${functionName}-${functionInvocationID}`;
+    const functionLogID = `${functionInvocationTime.getTime()}-${functionInvocationID}`;
+
+    const logStorageAccount = process.env['storageAccount'];
+    const logStorageKey = process.env['storageKey'];
+    const logStorageContainer = 'function-people-set-membership-command-logs';
+
+    const eventLabel = '';
+    const eventTags = [
+        "sorting-hat", 
+    ];
+
+    const request = req;
+
+    const operation = request.operation;
+    const payload = request.payload;
+
+    let result = "";
+
+    const logPayload = result;
+    context.log(logPayload);
+
+    const logObject = await createLogObject(functionInvocationID, functionInvocationTime, functionName, logPayload);
+    const logBlob = await storeLogBlob(logStorageAccount, logStorageKey, logStorageContainer, logObject);
+    context.log(logBlob);
+
+    const callbackMessage = await createCallbackMessage(logObject, 200);
+    context.bindings.callbackMessage = JSON.stringify(callbackMessage);
+    context.log(callbackMessage);
+
+    const invocationEvent = await createEvent(
+        functionInvocationID,
+        functionInvocationTime,
+        functionInvocationTimestamp,
+        functionName,
+        functionEventType,
+        functionEventID,
+        functionLogID,
+        logStorageAccount,
+        logStorageContainer,
+        eventLabel,
+        eventTags
+    );
+    context.bindings.flynnEvent = JSON.stringify(invocationEvent);
+    context.log(invocationEvent);
+
+    context.done(null, logBlob);
+};
+
+export default peopleSetMembershipCommand;
