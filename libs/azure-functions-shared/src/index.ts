593408685c0ce7b5c64daf4eb6ece6d146d51e29
James Schumann <james@schumann.ca>
2020-07-23 12:43:03 -0400

    update lib with shared code from sorting-hat

--- libs/azure-functions-shared/src/index.ts
+++ libs/azure-functions-shared/src/index.ts
@@ -2,3 +2,13 @@ export * from './lib/create-callback-message';
 export * from './lib/create-event';
 export * from './lib/create-log-object';
 export * from './lib/store-log-blob';
+
+export * from './lib/findCreatesAndUpdates';
+export * from './lib/findDeletes';
+export * from './lib/getCosmosItems';
+export * from './lib/initCalculation';
+export * from './lib/initOperations';
+export * from './lib/processCreates';
+export * from './lib/processDeletes';
+export * from './lib/processDifferences';
+export * from './lib/processUpdates';
