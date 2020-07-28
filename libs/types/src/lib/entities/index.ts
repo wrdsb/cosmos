1f2db6e48d3a7af637eb6ef9ac481534bcf80d84
James Schumann <james@schumann.ca>
2020-07-28 09:17:10 -0400

    export sorting-hat entities

--- libs/types/src/lib/entities/index.ts
+++ libs/types/src/lib/entities/index.ts
@@ -6,6 +6,7 @@ export * from './google';
 export * from './hagar';
 export * from './igor';
 export * from './ipps';
+export * from './sorting-hat';
 export * from './sparkrock';
 export * from './team-viewer';
 export * from './trillium';
