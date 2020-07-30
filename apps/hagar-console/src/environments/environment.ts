b65632bad159c1ea136c1d667165b720aeae0673
James Schumann <james@schumann.ca>
2020-07-30 10:33:29 -0400

    header and footer typography; background gradient

--- apps/hagar-console/src/environments/environment.ts
+++ apps/hagar-console/src/environments/environment.ts
@@ -2,7 +2,7 @@ import { Environment } from "@cosmos/types";
 
 export const environment: Environment = {
   production: false,
-  appName: 'HAGAR Beta',
+  appName: 'H.A.G.A.R. (Beta)',
 
   aadClientId: 'd15732c7-13e4-41fc-9d8f-c7776b875e58',
   aadAuthority: "https://login.microsoftonline.com/cd25c694-bfb8-48f4-9d0d-b9af282c4ab4",
