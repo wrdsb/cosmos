5064c4652cf53359d245d18a65d0cb5eb29c372c
James Schumann <james@schumann.ca>
2020-08-04 11:03:55 -0400

    nx g component members-list --project=ui-aad-groups

---
+++ libs/ui-aad-groups/src/lib/members-list/members-list.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'cosmos-members-list',
+  templateUrl: './members-list.component.html',
+  styleUrls: ['./members-list.component.scss']
+})
+export class MembersListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- libs/ui-aad-groups/src/lib/ui-aad-groups.module.ts
+++ libs/ui-aad-groups/src/lib/ui-aad-groups.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { GroupsListComponent } from './groups-list/groups-list.component';
+import { MembersListComponent } from './members-list/members-list.component';
 
 @NgModule({
   imports: [CommonModule],
-  declarations: [GroupsListComponent]
+  declarations: [GroupsListComponent, MembersListComponent]
 })
 export class UiAadGroupsModule {}
