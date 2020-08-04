36b0b7d54b9e4d98f9109bdbe41fde1d9acfe4dd
James Schumann <james@schumann.ca>
2020-08-04 11:02:41 -0400

    nx g component users-list --project=ui-aad-users

--- libs/ui-aad-users/src/lib/ui-aad-users.module.ts
+++ libs/ui-aad-users/src/lib/ui-aad-users.module.ts
@@ -1,7 +1,9 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { UsersListComponent } from './users-list/users-list.component';
 
 @NgModule({
-  imports: [CommonModule]
+  imports: [CommonModule],
+  declarations: [UsersListComponent]
 })
 export class UiAadUsersModule {}
