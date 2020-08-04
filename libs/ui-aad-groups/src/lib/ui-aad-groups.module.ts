98b21d7a60d85c5b87ab9aa0050c8c0184cc6b51
James Schumann <james@schumann.ca>
2020-08-04 11:02:20 -0400

    nx g component groups-list --project=ui-aad-groups

--- libs/ui-aad-groups/src/lib/ui-aad-groups.module.ts
+++ libs/ui-aad-groups/src/lib/ui-aad-groups.module.ts
@@ -1,7 +1,9 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { GroupsListComponent } from './groups-list/groups-list.component';
 
 @NgModule({
-  imports: [CommonModule]
+  imports: [CommonModule],
+  declarations: [GroupsListComponent]
 })
 export class UiAadGroupsModule {}
