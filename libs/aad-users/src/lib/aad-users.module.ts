464c8c6575b4bf365e3d504f7e3ff377fa21c6d7
James Schumann <james@schumann.ca>
2020-08-04 16:01:28 -0400

    move lib/ui-aad-users into lib/aad-users

--- libs/aad-users/src/lib/aad-users.module.ts
+++ libs/aad-users/src/lib/aad-users.module.ts
@@ -1,7 +1,23 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { UsersListComponent } from './users-list/users-list.component';
+import { UsersListBriefComponent } from './users-list-brief/users-list-brief.component';
+import { UsersListFullComponent } from './users-list-full/users-list-full.component';
+
 @NgModule({
-  imports: [CommonModule]
+  declarations: [
+    UsersListComponent,
+    UsersListFullComponent,
+    UsersListBriefComponent
+  ],
+  imports: [
+    CommonModule
+  ],
+  exports: [
+    UsersListComponent,
+    UsersListFullComponent,
+    UsersListBriefComponent
+  ]
 })
 export class AadUsersModule {}
--- libs/ui-aad-users/README.md
+++
@@ -1,7 +0,0 @@
-# ui-aad-users
-
-This library was generated with [Nx](https://nx.dev).
-
-## Running unit tests
-
-Run `nx test ui-aad-users` to execute the unit tests.
