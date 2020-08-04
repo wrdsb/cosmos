5bd18cbca024b2eb7df764f5eb0fe61b91e9436e
James Schumann <james@schumann.ca>
2020-08-04 11:00:08 -0400

    app menu wip

--- libs/panels/src/lib/panels.module.ts
+++ libs/panels/src/lib/panels.module.ts
@@ -10,6 +10,7 @@ import { ProfilePanelComponent } from './profile-panel/profile-panel.component';
 import { SettingsPanelComponent } from './settings-panel/settings-panel.component';
 import { FooterPanelComponent } from './footer-panel/footer-panel.component';
 import { HeaderPanelComponent } from './header-panel/header-panel.component';
+import { AppsMenuPanelComponent } from './apps-menu-panel/apps-menu-panel.component';
 
 @NgModule({
   declarations: [
@@ -20,7 +21,8 @@ import { HeaderPanelComponent } from './header-panel/header-panel.component';
     ProfilePanelComponent,
     SettingsPanelComponent,
     FooterPanelComponent,
-    HeaderPanelComponent
+    HeaderPanelComponent,
+    AppsMenuPanelComponent
   ],
   imports: [
     CommonModule,
@@ -35,7 +37,8 @@ import { HeaderPanelComponent } from './header-panel/header-panel.component';
     ProfilePanelComponent,
     SettingsPanelComponent,
     FooterPanelComponent,
-    HeaderPanelComponent
+    HeaderPanelComponent,
+    AppsMenuPanelComponent
   ]
 })
 export class PanelsModule {}
