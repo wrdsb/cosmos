933f4bf3a12d424318490173ba46198643607492
James Schumann <james@schumann.ca>
2020-08-11 15:48:28 -0400

    add some custom http interceptors, mostly for testing

--- libs/http-interceptors/src/index.ts
+++ libs/http-interceptors/src/index.ts
@@ -1,1 +1,14 @@
-export * from './lib/http-interceptors.module';
+/* "Barrel" of Http Interceptors */
+import { HTTP_INTERCEPTORS } from '@angular/common/http';
+
+import { MsalInterceptor } from '@azure/msal-angular';
+
+import { NoopInterceptor } from './lib/noop-interceptor';
+import { LoggingInterceptor } from './lib/logging-interceptor';
+
+/** Http interceptor providers in outside-in order */
+export const httpInterceptorProviders = [
+  { provide: HTTP_INTERCEPTORS, useClass: MsalInterceptor, multi: true },
+  { provide: HTTP_INTERCEPTORS, useClass: NoopInterceptor, multi: true },
+  { provide: HTTP_INTERCEPTORS, useClass: LoggingInterceptor, multi: true },
+];
