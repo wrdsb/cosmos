4f5d068c639ac5d3ce51ae2989202a2a1a56b24e
James Schumann <james@schumann.ca>
2020-08-11 15:50:47 -0400

    connect hagar service to ping function

--- libs/hagar-service/src/lib/hagar.service.ts
+++ libs/hagar-service/src/lib/hagar.service.ts
@@ -1,24 +1,46 @@
 import { Injectable } from '@angular/core';
-import { Observable, of } from "rxjs";
+import { Observable, of } from 'rxjs';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { UserAuthService } from '@cosmos/user-auth';
 
-import { AADGroup } from "@cosmos/types";
-import { AADUser } from "@cosmos/types";
+import { AADGroup } from '@cosmos/types';
+import { AADUser } from '@cosmos/types';
 
-import { GROUPS } from "./mocks/aad-groups";
-import { USERS } from "./mocks/aad-users";
+import { GROUPS } from './mocks/aad-groups';
+import { USERS } from './mocks/aad-users';
 
 @Injectable({
   providedIn: 'root'
 })
 export class HagarService {
+  private pingURL = 'https://wrdsb-hagar.azurewebsites.net/api/ping';
+  private groupsURL = '';
+  private usersURL = '';
 
-  constructor() { }
+  httpOptions = {
+    headers: new HttpHeaders({
+      'Content-Type': 'application/json',
+    })
+  };
+
+  constructor(private http: HttpClient, private msalService: UserAuthService) {}
+
+  getPing(): Observable<string> {
+    console.log('ping hagar');
+
+    this.httpOptions = {
+      headers: new HttpHeaders({
+        'Content-Type': 'application/json',
+      })
+    };
+    return this.http.get<string>(this.pingURL, this.httpOptions);
+  }
 
   getGroups(): Observable<AADGroup[]> {
     return of(GROUPS);
   }
 
-  getUsers(): Observable<AADUser[]>  {
+  getUsers(): Observable<AADUser[]> {
     return of(USERS);
   }
 }
