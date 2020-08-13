1b82d6b01ccf225fd76cfa0e38aff224baf03366
James Schumann <james@schumann.ca>
2020-08-13 09:55:16 -0400

    update createBlob for newer version of SDK

--- libs/azure-functions-shared/src/lib/createBlob.ts
+++ libs/azure-functions-shared/src/lib/createBlob.ts
@@ -1,23 +1,18 @@
-import { Aborter, BlobURL, BlockBlobURL, ContainerURL, ServiceURL, StorageURL, SharedKeyCredential } from "@azure/storage-blob";
+import { BlobServiceClient, StorageSharedKeyCredential } from "@azure/storage-blob";
 
 async function createBlob(storageAccount: string, storageKey: string, storageContainer: string, blobName: string, blobBody: string)
 {
-    const sharedKeyCredential = new SharedKeyCredential(storageAccount, storageKey);
-    const pipeline = StorageURL.newPipeline(sharedKeyCredential);
-    const serviceURL = new ServiceURL(
+    const sharedKeyCredential = new StorageSharedKeyCredential(storageAccount, storageKey);
+    const blobServiceClient = new BlobServiceClient(
         `https://${storageAccount}.blob.core.windows.net`,
-        pipeline
-    );
-    const containerURL = ContainerURL.fromServiceURL(serviceURL, storageContainer);
-    const blobURL = BlobURL.fromContainerURL(containerURL, blobName);
-    const blockBlobURL = BlockBlobURL.fromBlobURL(blobURL);
-
-    const uploadBlobResponse = await blockBlobURL.upload(
-        Aborter.none,
-        blobBody,
-        blobBody.length
+        sharedKeyCredential
     );
 
+    const containerClient = blobServiceClient.getContainerClient(storageContainer);
+    const blockBlobClient = containerClient.getBlockBlobClient(blobName);
+    const uploadBlobResponse = await blockBlobClient.upload(blobBody, blobBody.length);
+  
+    console.log(`Upload block blob ${blobName} successfully`, uploadBlobResponse.requestId);
     return uploadBlobResponse;
 }
 
