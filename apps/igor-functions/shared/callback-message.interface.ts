a4846badc527f01c1715c2f2a6fb780c2446f031
James Schumann <james@schumann.ca>
2020-08-18 11:37:19 -0400

    trying to get igor-functions deployable

--- apps/igor-functions/shared/callback-message.interface.ts
+++
@@ -1,8 +0,0 @@
-interface CallbackMessage {
-    id: string;
-    callback_type: string;
-    function_name: string;
-    invocation_id: string;
-    timestamp: string;
-    status: number;
-}
--- apps/igor-functions/shared/log-object.interface.ts
+++
@@ -1,7 +0,0 @@
-interface LogObject {
-    id: string;
-    function_name: string;
-    invocation_id: string;
-    timestamp: string;
-    payload: object;
-}
