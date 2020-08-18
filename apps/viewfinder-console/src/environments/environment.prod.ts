254ff4eb0329cc369baa66beb1204c3ff0007afd
James Schumann <james@schumann.ca>
2020-08-18 09:13:02 -0400

    update production env with "beta" and accurate app URL

--- apps/viewfinder-console/src/environments/environment.prod.ts
+++ apps/viewfinder-console/src/environments/environment.prod.ts
@@ -2,12 +2,12 @@ import { Environment } from "@cosmos/types";
 
 export const environment: Environment = {
   production: true,
-  appName: 'Viewfinder',
+  appName: 'Viewfinder (Beta)',
 
   aadClientId: 'd15732c7-13e4-41fc-9d8f-c7776b875e58',
   aadAuthority: "https://login.microsoftonline.com/cd25c694-bfb8-48f4-9d0d-b9af282c4ab4",
   aadValidateAuthority: true,
-  aadRedirectUri: "http://localhost:4200/",
-  aadPostLogoutRedirectUri: "http://localhost:4200/",
+  aadRedirectUri: "https://wrdsbviewfinder.z9.web.core.windows.net/",
+  aadPostLogoutRedirectUri: "https://wrdsbviewfinder.z9.web.core.windows.net/",
   aadNavigateToLoginRequestUrl: true,
 };
