eedabd367daac4c6f71b1d794561977481bfafc4
James Schumann <james@schumann.ca>
2020-08-18 12:34:50 -0400

    add chassis service

--- apps/viewfinder-console/src/app/app.component.ts
+++ apps/viewfinder-console/src/app/app.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { BroadcastService, MsalService } from '@azure/msal-angular';
 import { Logger, CryptoUtils } from 'msal';
 import { EnvironmentService } from '@cosmos/environment';
+import { Menu } from "@cosmos/types";
+import { ChassisService } from '@cosmos/chassis';
 
 @Component({
   selector: 'app-root',
@@ -14,11 +16,26 @@ export class AppComponent implements OnInit {
 
   constructor(
     public environmentService: EnvironmentService,
+    public chassisService: ChassisService,
     private broadcastService: BroadcastService,
     private authService: MsalService
   ) {}
 
   ngOnInit(): void {
+    this.chassisService.setHeaderContent(
+      {
+        links: [
+        ]
+      }
+    );
+
+    this.chassisService.setFooterContent(
+      {
+        links: [
+        ]
+      }
+    );
+
     this.isIframe = window !== window.parent && !window.opener;
 
     this.checkoutAccount();
