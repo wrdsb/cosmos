6fc4f2ed23795a5c4c0bd8666a69ec8f55b0b063
James Schumann <james@schumann.ca>
2020-08-19 10:43:31 -0400

    add website-store

---
+++ apps/codex-functions/src/website-store/index.ts
@@ -0,0 +1,272 @@
+import { AzureFunction, Context } from "@azure/functions"
+import { createLogObject } from "@cosmos/azure-functions-shared";
+import { storeLogBlob } from "@cosmos/azure-functions-shared";
+import { createCallbackMessage } from "@cosmos/azure-functions-shared";
+import { createEvent } from "@cosmos/azure-functions-shared";
+import { CodexWebsiteStoreFunctionRequest, CodexWebsiteStoreFunctionRequestPayload } from "@cosmos/types";
+
+const codexWebsiteStore: AzureFunction = async function (context: Context, triggerMessage: CodexWebsiteStoreFunctionRequest): Promise<void> {
+    const functionInvocationID = context.executionContext.invocationId;
+    const functionInvocationTime = new Date();
+    const functionInvocationTimestamp = functionInvocationTime.toJSON();  // format: 2012-04-23T18:25:43.511Z
+
+    const functionName = context.executionContext.functionName;
+    const functionEventType = 'WRDSB.Codex.Website.Store';
+    const functionEventID = `codex-functions-${functionName}-${functionInvocationID}`;
+    const functionLogID = `${functionInvocationTime.getTime()}-${functionInvocationID}`;
+
+    const logStorageAccount = process.env['storageAccount'];
+    const logStorageKey = process.env['storageKey'];
+    const logStorageContainer = 'function-codex-website-store-logs';
+
+    const eventLabel = '';
+    const eventTags = [
+        "codex", 
+    ];
+
+    const triggerObject = triggerMessage as CodexWebsiteStoreFunctionRequest;
+    const operation = triggerObject.operation;
+    const payload = triggerObject.payload as CodexWebsiteStoreFunctionRequestPayload;
+
+    let oldRecord = context.bindings.recordIn;
+    let newRecord;
+    let result;
+
+    let statusCode;
+    let statusMessage;
+
+    switch (operation) {
+        case 'delete':
+            result = doDelete(oldRecord, payload);
+            statusCode = '200';
+            statusMessage = 'Success: Marked website record deleted.';
+            break;
+        case 'patch':
+            result = doPatch(oldRecord, payload);
+            statusCode = '200';
+            statusMessage = 'Success: Patched website record.';
+            break;
+        case 'replace':
+            result = doReplace(oldRecord, payload);
+            statusCode = '200';
+            statusMessage = 'Success: Replaced website record.';
+            break;
+        default:
+            break;
+    }
+
+    context.bindings.recordOut = result.newRecord;
+    const logPayload = result.event;
+    context.log(logPayload);
+
+    const logObject = await createLogObject(functionInvocationID, functionInvocationTime, functionName, logPayload);
+    const logBlob = await storeLogBlob(logStorageAccount, logStorageKey, logStorageContainer, logObject);
+    context.log(logBlob);
+
+    const callbackMessage = await createCallbackMessage(logObject, 200);
+    context.bindings.callbackMessage = JSON.stringify(callbackMessage);
+    context.log(callbackMessage);
+
+    const invocationEvent = await createEvent(
+        functionInvocationID,
+        functionInvocationTime,
+        functionInvocationTimestamp,
+        functionName,
+        functionEventType,
+        functionEventID,
+        functionLogID,
+        logStorageAccount,
+        logStorageContainer,
+        eventLabel,
+        eventTags
+    );
+    context.bindings.flynnEvent = JSON.stringify(invocationEvent);
+    context.log(invocationEvent);
+
+    context.done(null, logBlob);
+
+    function doDelete(oldRecord, payload)
+    {
+        let event = {};
+        let newRecord = {
+            created_at: '',
+            updated_at: '',
+            deleted_at: '',
+            deleted: false
+        };
+
+        // check for existing record
+        if (!oldRecord) {
+            newRecord = Object.assign(newRecord, payload);
+            newRecord.created_at = functionInvocationTimestamp;
+            newRecord.updated_at = functionInvocationTimestamp;
+
+            // mark the record as deleted
+            newRecord.deleted_at = functionInvocationTimestamp;
+            newRecord.deleted = true;
+
+            event = craftWebsiteDeleteEvent(oldRecord, newRecord);
+
+        } else {
+            newRecord = Object.assign(newRecord, oldRecord);
+
+            // mark the record as deleted
+            newRecord.deleted_at = functionInvocationTimestamp;
+            newRecord.deleted = true;
+
+            event = craftWebsiteDeleteEvent(oldRecord, newRecord);
+        }
+
+        return {event: event, newRecord: newRecord};
+    }
+
+    function doPatch(oldRecord, payload)
+    {
+        let event = {};
+        let newRecord = {
+            created_at: '',
+            updated_at: '',
+            deleted_at: '',
+            deleted: false
+        };
+
+        if (!oldRecord) {
+            newRecord = Object.assign(newRecord, payload);
+            newRecord.created_at = functionInvocationTimestamp;
+            newRecord.updated_at = functionInvocationTimestamp;
+    
+            // patching a record implicitly undeletes it
+            newRecord.deleted_at = '';
+            newRecord.deleted = false;
+    
+            event = craftWebsiteCreateEvent(oldRecord, newRecord);
+
+        } else {
+            // Merge request object into current record
+            newRecord = Object.assign(newRecord, oldRecord, payload);
+            newRecord.updated_at = functionInvocationTimestamp;
+    
+            // patching a record implicitly undeletes it
+            newRecord.deleted_at = '';
+            newRecord.deleted = false;
+
+            event = craftWebsiteUpdateEvent(oldRecord, newRecord);
+        }
+
+        return {event: event, newRecord: newRecord};
+    }
+    
+    function doReplace(oldRecord, payload)
+    {
+        let event = {};
+        let newRecord = {
+            created_at: '',
+            updated_at: '',
+            deleted_at: '',
+            deleted: false
+        };
+
+        newRecord = Object.assign(newRecord, payload);
+
+        if (!oldRecord) {
+            newRecord.created_at = functionInvocationTimestamp;
+            newRecord.updated_at = functionInvocationTimestamp;
+
+            // replacing a record implicitly undeletes it
+            newRecord.deleted_at = '';
+            newRecord.deleted = false;
+        
+            event = craftWebsiteCreateEvent(oldRecord, newRecord);
+
+        } else {
+            newRecord.created_at = oldRecord.created_at;
+            newRecord.updated_at = functionInvocationTimestamp;
+
+            // replacing a record implicitly undeletes it
+            newRecord.deleted_at = '';
+            newRecord.deleted = false;
+        
+            event = craftWebsiteUpdateEvent(oldRecord, newRecord);
+        }
+
+        return {event: event, newRecord: newRecord};
+    }
+
+    function craftWebsiteCreateEvent(old_record, new_record)
+    {
+        let event_type = 'Codex.Website.Create';
+        let source = 'create';
+        let schema = 'create';
+        let label = `Codex ${new_record.id} created.`;
+        let payload = {
+            record: new_record
+        };
+
+        let event = craftEvent(new_record.id, source, schema, event_type, label, payload);
+        return event;
+    }
+    
+    function craftWebsiteUpdateEvent(old_record, new_record)
+    {
+        let event_type = 'Codex.Website.Update';
+        let source = 'update';
+        let schema = 'update';
+        let label = `Website ${new_record.id} updated.`;
+        let payload = {
+            old_record: old_record,
+            new_record: new_record,
+        };
+
+        let event = craftEvent(new_record.id, source, schema, event_type, label, payload);
+        return event;
+    }
+
+    function craftWebsiteDeleteEvent(old_record, new_record)
+    {
+        let event_type = 'Codex.Website.Delete';
+        let source = 'delete';
+        let schema = 'delete';
+        let label = `Website ${new_record.id} deleted.`;
+        let payload = {
+            record: old_record
+        };
+
+        let event = craftEvent(old_record.id, source, schema, event_type, label, payload);
+        return event;
+    }
+
+    function craftEvent(recordID, source, schema, event_type, label, payload) {
+        let event = {
+            id: `${event_type}-${context.executionContext.invocationId}`,
+            time: functionInvocationTimestamp,
+
+            type: event_type,
+            source: `/codex/website/${recordID}/${source}`,
+            schemaURL: `ca.wrdsb.codex.website.${schema}.json`,
+
+            label: label,
+            tags: [
+                "codex", 
+                "codex_website_change",
+                "website_change"
+            ], 
+
+            data: {
+                function_name: context.executionContext.functionName,
+                invocation_id: context.executionContext.invocationId,
+                result: {
+                    payload: payload 
+                },
+            },
+
+            eventTypeVersion: "0.1",
+            specversion: "0.2",
+            contentType: "application/json"
+        };
+
+        // TODO: check message length
+        return event;
+    }
+};
+
+export default codexWebsiteStore;
