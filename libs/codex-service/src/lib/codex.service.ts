0575837a38d269a0ae1de289058ff7718f659522
James Schumann <james@schumann.ca>
2020-08-20 12:44:47 -0400

    bring codex service inline with igor and hagar

--- libs/codex-service/src/lib/codex.service.ts
+++ libs/codex-service/src/lib/codex.service.ts
@@ -1,9 +1,69 @@
 import { Injectable } from '@angular/core';
+import { Observable, of } from 'rxjs';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { UserAuthService } from '@cosmos/user-auth';
+
+import { PingFunctionResponse } from "@cosmos/types";
+
+import { GoogleGroup } from "@cosmos/types";
+
+//import { AADGroup } from '@cosmos/types';
+//import { AADUser } from '@cosmos/types';
+
+//import { GROUPS } from './mocks/aad-groups';
+//import { USERS } from './mocks/aad-users';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CodexService {
+  private pingURL = 'https://wrdsb-codex.azurewebsites.net/api/ping';
+  private searchURL = 'https://wrdsb-codex.azurewebsites.net/api/igor-groups-groups-search';
+  
+  httpOptions = {
+    headers: new HttpHeaders({
+      'Content-Type': 'application/json',
+    })
+  };
 
-  constructor() { }
+  constructor(
+    private http: HttpClient,
+    private msalService: UserAuthService
+  ) {}
+
+  getPing(): Observable<PingFunctionResponse> {
+    console.log('ping codex');
+
+    this.httpOptions = {
+      headers: new HttpHeaders({
+        'Content-Type': 'application/json',
+      })
+    };
+    return this.http.get<PingFunctionResponse>(this.pingURL, this.httpOptions);
+  }
+
+  search() {
+    console.log('search codex');
+
+    this.httpOptions = {
+      headers: new HttpHeaders({
+        'Content-Type': 'application/json',
+      })
+    };
+
+    let body = {
+      payload: {
+        //count: true,
+        //select: '*',
+        //filter: '',
+        //facets: '',
+        //orderby: '',
+        //search: '',
+        //skip: '',
+        //top: ''
+      }
+    };
+
+    return this.http.post<GoogleGroup[]>(this.searchURL, body, this.httpOptions);
+  }
 }
