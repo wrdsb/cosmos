56ee7faf4310b537ee03bb54ca8fa4a47a6aaa01
James Schumann <james@schumann.ca>
2020-08-20 12:46:12 -0400

    remove temporary ping

--- libs/aad-groups/src/lib/groups-list-full/groups-list-full.component.ts
+++ libs/aad-groups/src/lib/groups-list-full/groups-list-full.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import { AADGroup } from '@cosmos/types';
-
 import { HagarService } from '@cosmos/hagar-service';
 
 @Component({
@@ -22,18 +21,12 @@ export class GroupsListFullComponent implements OnInit {
     //'securityEnabled',
   ];
 
-  ping: string;
   groups: AADGroup[];
 
   constructor(private hagarService: HagarService) {}
 
   ngOnInit(): void {
-    this.getPing();
     this.getGroups();
-  }
-
-  getPing() {
-    this.hagarService.getPing().subscribe(ping => (this.ping = ping));
   }
 
   getGroups(): void {
