aad55a8e1fee13e2b0caca1bee8fb175a6926789
James Schumann <james@schumann.ca>
2020-08-25 15:51:47 -0400

    add types for ping request state

--- libs/types/src/lib/types.ts
+++ libs/types/src/lib/types.ts
@@ -1,3 +1,5 @@
+import { Observable } from "rxjs";
+
 export interface Environment {
   production: boolean;
   appName: string;
@@ -8,4 +10,17 @@ export interface Environment {
   aadRedirectUri: string;
   aadPostLogoutRedirectUri: string;
   aadNavigateToLoginRequestUrl: boolean;
+}
+
+export interface PingRequestState {
+  status: Status;
+  response?: string;
+  error?: string;
+}
+
+export enum Status {
+  UNKNOWN = 'UNKNOWN',
+  LOADING = 'LOADING',
+  SUCCESS = 'SUCCESS',
+  ERROR = 'ERROR'
 }
