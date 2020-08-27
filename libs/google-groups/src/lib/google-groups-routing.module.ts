df772900122b070d33a451bde069b45dbf650992
James Schumann <james@schumann.ca>
2020-08-26 20:55:41 -0400

    add guards to google-groups module

--- libs/google-groups/src/lib/google-groups-routing.module.ts
+++ libs/google-groups/src/lib/google-groups-routing.module.ts
@@ -1,14 +1,43 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { MsalGuard } from '@azure/msal-angular';
+import { RolesGuard } from "@cosmos/guards";
+
 import { GroupsHomeComponent } from './groups-home/groups-home.component';
 import { GroupDetailComponent } from './group-detail/group-detail.component';
 import { GroupsListComponent } from './groups-list/groups-list.component';
 
-
 const routes: Routes = [
-  { path: 'group', component: GroupDetailComponent },
-  { path: 'list', component: GroupsListComponent},
-  { path: '', component: GroupsHomeComponent }
+  {
+    path: 'group',
+    component: GroupDetailComponent,
+    data: {
+      roles: ['cosmos-superuser', 'cosmos-user-its']
+    },
+    canActivate: [
+      MsalGuard, RolesGuard
+    ]
+  },
+  { 
+    path: 'list',
+    component: GroupsListComponent,
+    data: {
+      roles: ['cosmos-superuser', 'cosmos-user-its']
+    },
+    canActivate: [
+      MsalGuard, RolesGuard
+    ]
+  },
+  { 
+    path: '',
+    component: GroupsHomeComponent,
+    data: {
+      roles: ['cosmos-superuser', 'cosmos-user-its']
+    },
+    canActivate: [
+      MsalGuard, RolesGuard
+    ]
+  }
 ];
 
 @NgModule({
