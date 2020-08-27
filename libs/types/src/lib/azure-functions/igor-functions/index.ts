65bfb31b4ab497ebcc84c7571c73240864afb20f
James Schumann <james@schumann.ca>
2020-08-26 20:57:33 -0400

    how 'bout we export those types as well. duh.

---
+++ libs/types/src/lib/azure-functions/igor-functions/index.ts
@@ -0,0 +1,6 @@
+export * from './google-group-memberships-abc-calculate';
+export * from './google-group-memberships-abc-calculate-all';
+export * from './google-group-memberships-all-staff-calculate';
+export * from './google-group-memberships-roles-calculate';
+export * from './google-group-memberships-one-offs-calculate';
+export * from './google-group-memberships-unions-calculate';
