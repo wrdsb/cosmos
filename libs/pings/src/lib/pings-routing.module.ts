105ceb169bbaf93aa138a5320e30f1deda4b47cf
James Schumann <james@schumann.ca>
2020-08-26 20:56:04 -0400

    add guards to pings module

--- libs/pings/src/lib/pings-routing.module.ts
+++ libs/pings/src/lib/pings-routing.module.ts
@@ -1,5 +1,7 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { MsalGuard } from '@azure/msal-angular';
+import { RolesGuard } from "@cosmos/guards";
 
 import { AllPingsComponent } from "./all-pings/all-pings.component";
 
@@ -8,10 +10,46 @@ import { HagarPingComponent } from './hagar-ping/hagar-ping.component';
 import { IgorPingComponent } from './igor-ping/igor-ping.component';
 
 const routes: Routes = [
-  { path: 'codex', component: CodexPingComponent },
-  { path: 'hagar', component: HagarPingComponent },
-  { path: 'igor', component: IgorPingComponent },
-  { path: '', component: AllPingsComponent }
+  {
+    path: 'codex',
+    component: CodexPingComponent,
+    data: {
+      roles: ['cosmos-superuser', 'cosmos-user-its']
+    },
+    canActivate: [
+      MsalGuard, RolesGuard
+    ]
+  },
+  {
+    path: 'hagar',
+    component: HagarPingComponent,
+    data: {
+      roles: ['cosmos-superuser', 'cosmos-user-its']
+    },
+    canActivate: [
+      MsalGuard, RolesGuard
+    ]
+  },
+  {
+    path: 'igor',
+    component: IgorPingComponent,
+    data: {
+      roles: ['cosmos-superuser', 'cosmos-user-its']
+    },
+    canActivate: [
+      MsalGuard, RolesGuard
+    ]
+  },
+  {
+    path: '',
+    component: AllPingsComponent,
+    data: {
+      roles: ['cosmos-superuser', 'cosmos-user-its']
+    },
+    canActivate: [
+      MsalGuard, RolesGuard
+    ]
+  }
 ];
 
 @NgModule({
