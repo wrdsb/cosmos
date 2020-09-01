6e28b1f3f7da19c6ba0df0c31dff2abd5bfb9e81
James Schumann <james@schumann.ca>
2020-09-01 16:43:37 -0400

    flesh out igor group-store

--- apps/igor-functions/src/group-store/index.ts
+++ apps/igor-functions/src/group-store/index.ts
@@ -1,6 +1,7 @@
 import { AzureFunction, Context } from "@azure/functions";
+import { FunctionInvocation, GoogleGroupStoreFunctionRequest, GoogleGroupStoreFunctionRequestPayload } from "@cosmos/types";
 
-const groupStore: AzureFunction = async function (context: Context, triggerMessage): Promise<void> {
+const groupStore: AzureFunction = async function (context: Context, triggerMessage: GoogleGroupStoreFunctionRequest): Promise<void> {
     const functionInvocation = {
         functionInvocationID: context.executionContext.invocationId,
         functionInvocationTimestamp: new Date().toJSON(),
@@ -9,10 +10,227 @@ const groupStore: AzureFunction = async function (context: Context, triggerMessa
         functionDataType: 'Group',
         functionDataOperation: 'Store',
         eventLabel: ''
+    } as FunctionInvocation;
+
+    const triggerObject = triggerMessage as GoogleGroupStoreFunctionRequest;
+    const operation = triggerObject.operation;
+    const payload = triggerObject.payload as GoogleGroupStoreFunctionRequestPayload;
+
+    const oldRecord = context.bindings.recordIn;
+    let newRecord = {
+        created_at: '',
+        updated_at: '',
+        deleted_at: '',
+        deleted: false,
+        businessOwner: '',
+        membership_automation_active: false,
+        automate_members: false,
+        automate_mangers: false,
+        automate_owners: false,
+        members_people_sets: [],
+        mangers_people_sets: [],
+        owners_people_sets: [],
+        configurationAutomationActive: false,
+        configurationTemplates: false
     };
+    let result;
+
+    let statusCode;
+    let statusMessage;
+
+    switch (operation) {
+        case 'delete':
+            result = doDelete(oldRecord, newRecord, payload);
+            statusCode = '200';
+            statusMessage = 'Success: Marked group record deleted.';
+            break;
+        case 'patch':
+            result = doPatch(oldRecord, newRecord, payload);
+            statusCode = '200';
+            statusMessage = 'Success: Patched group record.';
+            break;
+        case 'replace':
+            result = doReplace(oldRecord, newRecord, payload);
+            statusCode = '200';
+            statusMessage = 'Success: Replaced group record.';
+            break;
+        default:
+            break;
+    }
+
+    context.bindings.recordOut = result.newRecord;
+
+    const logPayload = result.event;
+    functionInvocation.logPayload = logPayload;
+    context.log(logPayload);
 
     context.log(functionInvocation);
     context.done(null, functionInvocation);
+
+    function doDelete(oldRecord, newRecord, payload)
+    {
+        let event = {};
+
+        // check for existing record
+        if (!oldRecord) {
+            newRecord = Object.assign(newRecord, payload);
+            newRecord.created_at = functionInvocation.functionInvocationTimestamp;
+            newRecord.updated_at = functionInvocation.functionInvocationTimestamp;
+
+            // mark the record as deleted
+            newRecord.deleted_at = functionInvocation.functionInvocationTimestamp;
+            newRecord.deleted = true;
+
+            event = craftGroupDeleteEvent(oldRecord, newRecord);
+
+        } else {
+            newRecord = Object.assign(newRecord, oldRecord);
+
+            // mark the record as deleted
+            newRecord.deleted_at = functionInvocation.functionInvocationTimestamp;
+            newRecord.deleted = true;
+
+            event = craftGroupDeleteEvent(oldRecord, newRecord);
+        }
+
+        return {event: event, newRecord: newRecord};
+    }
+
+    function doPatch(oldRecord, newRecord, payload)
+    {
+        let event = {};
+
+        if (!oldRecord) {
+            newRecord = Object.assign(newRecord, payload);
+            newRecord.created_at = functionInvocation.functionInvocationTimestamp;
+            newRecord.updated_at = functionInvocation.functionInvocationTimestamp;
+    
+            // patching a record implicitly undeletes it
+            newRecord.deleted_at = '';
+            newRecord.deleted = false;
+    
+            event = craftGroupCreateEvent(oldRecord, newRecord);
+
+        } else {
+            // Merge request object into current record
+            newRecord = Object.assign(newRecord, oldRecord, payload);
+            newRecord.updated_at = functionInvocation.functionInvocationTimestamp;
+    
+            // patching a record implicitly undeletes it
+            newRecord.deleted_at = '';
+            newRecord.deleted = false;
+
+            event = craftGroupUpdateEvent(oldRecord, newRecord);
+        }
+
+        return {event: event, newRecord: newRecord};
+    }
+    
+    function doReplace(oldRecord, newRecord, payload)
+    {
+        let event = {};
+
+        newRecord = Object.assign(newRecord, payload);
+
+        if (!oldRecord) {
+            newRecord.created_at = functionInvocation.functionInvocationTimestamp;
+            newRecord.updated_at = functionInvocation.functionInvocationTimestamp;
+
+            // replacing a record implicitly undeletes it
+            newRecord.deleted_at = '';
+            newRecord.deleted = false;
+        
+            event = craftGroupCreateEvent(oldRecord, newRecord);
+
+        } else {
+            newRecord.created_at = oldRecord.created_at;
+            newRecord.updated_at = functionInvocation.functionInvocationTimestamp;
+
+            // replacing a record implicitly undeletes it
+            newRecord.deleted_at = '';
+            newRecord.deleted = false;
+        
+            event = craftGroupUpdateEvent(oldRecord, newRecord);
+        }
+
+        return {event: event, newRecord: newRecord};
+    }
+
+    function craftGroupCreateEvent(old_record, new_record)
+    {
+        let event_type = 'IGOR.Group.Create';
+        let source = 'create';
+        let schema = 'create';
+        let label = `Group ${new_record.id} created.`;
+        let payload = {
+            record: new_record
+        };
+
+        let event = craftEvent(new_record.id, source, schema, event_type, label, payload);
+        return event;
+    }
+    
+    function craftGroupUpdateEvent(old_record, new_record)
+    {
+        let event_type = 'IGOR.Group.Update';
+        let source = 'update';
+        let schema = 'update';
+        let label = `Group ${new_record.id} updated.`;
+        let payload = {
+            old_record: old_record,
+            new_record: new_record,
+        };
+
+        let event = craftEvent(new_record.id, source, schema, event_type, label, payload);
+        return event;
+    }
+
+    function craftGroupDeleteEvent(old_record, new_record)
+    {
+        let event_type = 'IGOR.Group.Delete';
+        let source = 'delete';
+        let schema = 'delete';
+        let label = `Group ${new_record.id} deleted.`;
+        let payload = {
+            record: old_record
+        };
+
+        let event = craftEvent(old_record.id, source, schema, event_type, label, payload);
+        return event;
+    }
+
+    function craftEvent(recordID, source, schema, event_type, label, payload) {
+        let event = {
+            id: `${event_type}-${context.executionContext.invocationId}`,
+            time: functionInvocation.functionInvocationTimestamp,
+
+            type: event_type,
+            source: `/igor/group/${recordID}/${source}`,
+            schemaURL: `ca.wrdsb.igor.group.${schema}.json`,
+
+            label: label,
+            tags: [
+                "igor", 
+                "google_group_change",
+                "group_change"
+            ], 
+
+            data: {
+                function_name: context.executionContext.functionName,
+                invocation_id: context.executionContext.invocationId,
+                result: {
+                    payload: payload 
+                },
+            },
+
+            eventTypeVersion: "0.1",
+            specversion: "0.2",
+            contentType: "application/json"
+        };
+
+        // TODO: check message length
+        return event;
+    }
 };
 
 export default groupStore;
