b2b955c434b1dbaf1ee4ba9513ab06c0257007e7
James Schumann <james@schumann.ca>
2020-09-02 14:34:54 -0400

    update URL for igor group query in CORS config

--- libs/user-auth/src/lib/user-auth.module.ts
+++ libs/user-auth/src/lib/user-auth.module.ts
@@ -22,7 +22,7 @@ export const protectedResourceMap: [string, string[]][] = [
   ['https://wrdsb-codex.azurewebsites.net/api/igor-groups-groups-search', [ 'https://wrdsb-codex.azurewebsites.net/user_impersonation' ]],
 
   ['https://wrdsb-igor3.azurewebsites.net/api/ping', [ 'https://wrdsb-igor3.azurewebsites.net/user_impersonation' ]],
-  ['https://wrdsb-igor3.azurewebsites.net/api/groups-query', [ 'https://wrdsb-igor3.azurewebsites.net/user_impersonation' ]],
+  ['https://wrdsb-igor3.azurewebsites.net/api/group-query', [ 'https://wrdsb-igor3.azurewebsites.net/user_impersonation' ]],
 
   ['https://wrdsb-hagar.azurewebsites.net/api/ping', [ 'https://wrdsb-hagar.azurewebsites.net/user_impersonation' ]],
   ['https://wrdsb-hagar.azurewebsites.net/api/aad-group-query', [ 'https://wrdsb-hagar.azurewebsites.net/user_impersonation' ]],
