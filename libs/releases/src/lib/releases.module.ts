a36d7b672eadb49036c109319e86be4b55f7c4f8
James Schumann <james@schumann.ca>
2020-09-04 09:36:30 -0400

    ng g component release-info --project=releases

--- libs/releases/src/lib/releases.module.ts
+++ libs/releases/src/lib/releases.module.ts
@@ -1,7 +1,9 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ReleaseInfoComponent } from './release-info/release-info.component';
 
 @NgModule({
-  imports: [CommonModule]
+  imports: [CommonModule],
+  declarations: [ReleaseInfoComponent]
 })
 export class ReleasesModule {}
