26ca377bf9770b0105c9d3eb9fdda36b29e226af
James Schumann <james@schumann.ca>
2020-09-09 09:53:40 -0400

    export new types for skinner functions

--- libs/types/src/lib/azure-functions/index.ts
+++ libs/types/src/lib/azure-functions/index.ts
@@ -4,4 +4,5 @@ export * from './common';
 export * from './codex-functions';
 export * from './hagar-functions';
 export * from './igor-functions';
+export * from './skinner-functions';
 export * from './sorting-hat-functions';
---
+++ libs/types/src/lib/azure-functions/skinner-functions/index.ts
@@ -0,0 +1,13 @@
+import { fromEventPattern } from 'rxjs';
+
+export * from './trillium-assignment-store';
+export * from './trillium-class-store';
+export * from './trillium-enrolment-store';
+export * from './trillium-school-store';
+export * from './trillium-staff-store';
+export * from './trillium-student-store';
+export * from './trillium-teacher-store';
+
+export * from './view-gclassroom-process';
+export * from './view-skinnerassignments-process';
+export * from './view-skinnerstaff-process';
