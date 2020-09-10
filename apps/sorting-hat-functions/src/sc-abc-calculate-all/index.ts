8ddee6c70e9430d6ac3a24720675c4d2925e0ba5
James Schumann <james@schumann.ca>
2020-09-10 17:24:01 -0400

    create sc-abc-calculate-all

---
+++ apps/sorting-hat-functions/src/sc-abc-calculate-all/index.ts
@@ -0,0 +1,95 @@
+import { AzureFunction, Context } from "@azure/functions";
+import { createLogObject } from "@cosmos/azure-functions-shared";
+import { storeLogBlob } from "@cosmos/azure-functions-shared";
+import { createCallbackMessage } from "@cosmos/azure-functions-shared";
+import { createEvent } from "@cosmos/azure-functions-shared";
+
+const GroupMembershipsABCCalculateAll: AzureFunction = async function (context: Context, triggerMessage: any): Promise<void> {
+    const functionInvocationID = context.executionContext.invocationId;
+    const functionInvocationTime = new Date();
+    const functionInvocationTimestamp = functionInvocationTime.toJSON();  // format: 2012-04-23T18:25:43.511Z
+
+    const functionName = context.executionContext.functionName;
+    const functionEventType = 'WRDSB.SortingHat.SC.ABC.Calculate.All';
+    const functionEventID = `sorting-hat-functions-${functionName}-${functionInvocationID}`;
+    const functionLogID = `${functionInvocationTime.getTime()}-${functionInvocationID}`;
+
+    const logStorageAccount = process.env['storageAccount'];
+    const logStorageKey = process.env['storageKey'];
+    const logStorageContainer = 'function-sc-abc-calculate-all-logs';
+
+    const eventLabel = '';
+    const eventTags = [
+        "sorting-hat", 
+    ];
+
+    const triggerObject = triggerMessage;
+    const payload = triggerObject.payload;
+
+    context.log(payload);
+
+    const classes = context.bindings.classesNow;
+    
+    let queueMessages = await prepareMessages(classes);
+
+    context.bindings.outputQueue = queueMessages;
+
+    const logPayload = {
+        queueMessages: queueMessages
+    };
+    context.log(logPayload);
+
+    const logObject = await createLogObject(functionInvocationID, functionInvocationTime, functionName, logPayload);
+    const logBlob = await storeLogBlob(logStorageAccount, logStorageKey, logStorageContainer, logObject);
+    context.log(logBlob);
+
+    const callbackMessage = await createCallbackMessage(logObject, 200);
+    //context.bindings.callbackMessage = JSON.stringify(callbackMessage);
+    context.log(callbackMessage);
+
+    const invocationEvent = await createEvent(
+        functionInvocationID,
+        functionInvocationTime,
+        functionInvocationTimestamp,
+        functionName,
+        functionEventType,
+        functionEventID,
+        functionLogID,
+        logStorageAccount,
+        logStorageContainer,
+        eventLabel,
+        eventTags
+    );
+    //context.bindings.flynnEvent = JSON.stringify(invocationEvent);
+    context.log(invocationEvent);
+
+    context.done(null, logBlob);
+
+    
+    async function prepareMessages(classes) {
+        let messages = [];
+        let schoolCodes = new Set();
+
+        classes.forEach(function(classObject) {
+            let schoolCode = (classObject.school_code) ? classObject.school_code.toLowerCase() : 0;
+
+            if (isNaN(schoolCode)) {
+                schoolCodes.add(schoolCode);
+            }
+        });
+
+        schoolCodes.forEach(schoolCode => {
+            let message = {
+                payload: {
+                    schoolCode: schoolCode
+                }
+            };
+
+            messages.push(message);
+        });
+
+        return messages;
+    }
+}
+
+export default GroupMembershipsABCCalculateAll;
