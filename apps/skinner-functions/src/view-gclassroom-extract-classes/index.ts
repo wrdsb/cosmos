38627140546c74bd65f591a81494ad97e09995be
James Schumann <james@schumann.ca>
2020-09-10 09:49:01 -0400

    rework view-gclassroom-extract-classes

--- apps/skinner-functions/src/view-gclassroom-extract-classes/index.ts
+++ apps/skinner-functions/src/view-gclassroom-extract-classes/index.ts
@@ -1,35 +1,45 @@
-import { AzureFunction, Context } from "@azure/functions"
+import { AzureFunction, Context } from "@azure/functions";
+import { FunctionInvocation, ViewGclassroomExtractClassesFunctionRequest, ViewGclassroomExtractClassesFunctionRequestPayload, ViewGclassroomRecord, TrilliumClass } from "@cosmos/types";
 
-const viewGClassroomExtractClasses: AzureFunction = async function (context: Context, triggerMessage: string): Promise<void> {
-    const execution_timestamp = (new Date()).toJSON();  // format: 2012-04-23T18:25:43.511Z
+const viewGclassroomExtractClasses: AzureFunction = async function (context: Context, triggerMessage: ViewGclassroomExtractClassesFunctionRequest): Promise<void> {
+    const functionInvocation = {
+        functionInvocationID: context.executionContext.invocationId,
+        functionInvocationTimestamp: new Date().toJSON(),
+        functionApp: 'Skinner',
+        functionName: context.executionContext.functionName,
+        functionDataType: 'ViewGclassroom',
+        functionDataOperation: 'ExtractClasses',
+        eventLabel: ''
+    } as FunctionInvocation;
 
-    const panamaBlob = context.bindings.panamaBlob;
+    const triggerObject = triggerMessage as ViewGclassroomExtractClassesFunctionRequest;
+    const payload = triggerObject.payload as ViewGclassroomExtractClassesFunctionRequestPayload;
 
-    const rows = panamaBlob;
+    const objects = context.bindings.viewRaw;
 
     let classesObject = {};
     let classesArray = [];
 
-    rows.forEach(function(row) {
-        let school_code        = row.SCHOOL_CODE;
-        let class_code         = row.CLASS_CODE;
+    objects.forEach(function(record: ViewGclassroomRecord) {
+        let schoolCode   = record.schoolCode ? record.schoolCode : "";
+        let classCode    = record.classCode ? record.classCode : "";
+        let staffNumber  = record.staffNumber ? record.staffNumber : "";  
 
-        let teacher_ein        = row.TEACHER_EIN ? row.TEACHER_EIN : '0';
-        let teacher_email      = row.TEACHER_EMAIL ? row.TEACHER_EMAIL : '0';
+        // we don't care if a teacher is assigned yet
+        if (schoolCode !== "" && classCode !== "") {
+            let classObjectID = sanitizeID(`${schoolCode}-${classCode}`);
 
-        let classObjectID     = sanitizeID(`${school_code}-${class_code}`);
-
-        // Extract the 'class' object from the row
-        let classObject = {
-            id:             classObjectID,
-            school_code:    school_code,
-            class_code:     class_code,
-            teacher_ein:    teacher_ein,
-            teacher_email:  teacher_email
-        };
-
-        // Add/overwrite individual objects from this row to their collection objects
-        classesObject[classObject.id]        = classObject;
+            // Extract the 'class' object from the row
+            let classObject = {
+                id: classObjectID,
+                schoolCode: schoolCode,
+                classCode: classCode,
+                staffNumber: staffNumber
+            } as TrilliumClass;
+    
+            // Add/overwrite individual objects from this row to their collection objects
+            classesObject[classObjectID] = classObject;
+        }
     });
 
     // Add each class from classesObject to classesArray
@@ -41,31 +51,17 @@ const viewGClassroomExtractClasses: AzureFunction = async function (context: Con
     context.bindings.classesNowArray = JSON.stringify(classesArray);
     context.bindings.classesNowObject = JSON.stringify(classesObject);
 
-    let callbackMessage = {
-        id: 'skinner-functions-' + context.executionContext.functionName +'-'+ context.executionContext.invocationId,
-        eventType: 'Skinner.View.GClassroom.Extract.Classes',
-        eventTime: execution_timestamp,
-        //subject: ,
-        data: {
-            event_type: 'function_invocation',
-            app: 'wrdsb-skinner',
-            function_name: context.executionContext.functionName,
-            invocation_id: context.executionContext.invocationId,
-            data: {},
-            timestamp: execution_timestamp
-        },
-        dataVersion: '1'
-    };
-
-    context.bindings.callbackMessage = JSON.stringify(callbackMessage.data);
-
     const sis_classes_reconcile_job = {
         "job_type": "Skinner.Class.Differences.Reconcile"
     };
     context.bindings.triggerJobs = [JSON.stringify(sis_classes_reconcile_job)];
 
-    context.log(JSON.stringify(callbackMessage));
-    context.done(null, callbackMessage);
+    const logPayload = "";
+    functionInvocation.logPayload = logPayload;
+    context.log(logPayload);
+
+    context.log(functionInvocation);
+    context.done(null, functionInvocation);
 
     function sanitizeID(id)
     {
@@ -81,4 +77,4 @@ const viewGClassroomExtractClasses: AzureFunction = async function (context: Con
     }
 };
 
-export default viewGClassroomExtractClasses;
+export default viewGclassroomExtractClasses;
