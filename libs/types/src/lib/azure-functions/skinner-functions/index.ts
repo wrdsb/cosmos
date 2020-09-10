2a33c51bbf1c2a374f2d42da8cd325900e8a3b8a
James Schumann <james@schumann.ca>
2020-09-10 10:59:28 -0400

    export new function types

--- libs/types/src/lib/azure-functions/skinner-functions/index.ts
+++ libs/types/src/lib/azure-functions/skinner-functions/index.ts
@@ -12,5 +12,10 @@ export * from './view-gclassroom-process';
 export * from './view-skinnerassignments-process';
 export * from './view-skinnerstaff-process';
 
+export * from './view-gclassroom-extract-classes';
+export * from './view-gclassroom-extract-enrolments';
+export * from './view-gclassroom-extract-students';
+export * from './view-gclassroom-extract-teachers';
+
 export * from './view-skinnerassignments-extract-assignments';
 export * from './view-skinnerstaff-extract-staff';
