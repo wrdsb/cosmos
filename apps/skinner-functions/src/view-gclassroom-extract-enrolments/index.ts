2094399725146aab215137f4a743ca5b8e792ca7
James Schumann <james@schumann.ca>
2020-09-10 09:48:48 -0400

    rework view-gclassroom-extract-enrolments

--- apps/skinner-functions/src/view-gclassroom-extract-enrolments/index.ts
+++ apps/skinner-functions/src/view-gclassroom-extract-enrolments/index.ts
@@ -1,87 +1,83 @@
-import { AzureFunction, Context } from "@azure/functions"
+import { AzureFunction, Context } from "@azure/functions";
+import { FunctionInvocation, ViewGclassroomExtractEnrolmentsFunctionRequest, ViewGclassroomExtractEnrolmentsFunctionRequestPayload, ViewGclassroomRecord, TrilliumEnrolment } from "@cosmos/types";
 
-const viewGClassroomExtractEnrolments: AzureFunction = async function (context: Context, triggerMessage: string): Promise<void> {
-    const execution_timestamp = (new Date()).toJSON();  // format: 2012-04-23T18:25:43.511Z
+const viewGclassroomExtractEnrolments: AzureFunction = async function (context: Context, triggerMessage: ViewGclassroomExtractEnrolmentsFunctionRequest): Promise<void> {
+    const functionInvocation = {
+        functionInvocationID: context.executionContext.invocationId,
+        functionInvocationTimestamp: new Date().toJSON(),
+        functionApp: 'Skinner',
+        functionName: context.executionContext.functionName,
+        functionDataType: 'ViewGclassroom',
+        functionDataOperation: 'ExtractEnrolments',
+        eventLabel: ''
+    } as FunctionInvocation;
 
-    const panamaBlob = context.bindings.panamaBlob;
+    const triggerObject = triggerMessage as ViewGclassroomExtractEnrolmentsFunctionRequest;
+    const payload = triggerObject.payload as ViewGclassroomExtractEnrolmentsFunctionRequestPayload;
 
-    const rows = panamaBlob;
+    const objects = context.bindings.viewRaw;
 
     let enrolmentsSortedObject = {};
     let enrolmentsSortedArrays = {};
 
-    rows.forEach(function(row) {
-        let school_code        = row.SCHOOL_CODE;
-        let class_code         = row.CLASS_CODE;
+    objects.forEach(function(record: ViewGclassroomRecord) {
+        let schoolCode        = record.schoolCode ? record.schoolCode : "";
+        let classCode         = record.classCode ? record.classCode : "";
 
-        let student_number     = row.STUDENT_NO;
-        let student_first_name = row.STUDENT_FIRST_NAME;
-        let student_last_name  = row.STUDENT_LAST_NAME;
-        let student_email      = row.STUDENT_EMAIL;
+        let studentNumber     = record.studentNumber ? record.studentNumber : "";
+        let studentFirstName  = record.studentFirstName ? record.studentFirstName : "";
+        let studentLastName   = record.studentLastName ? record.studentLastName : "";
+        let studentEmail      = record.studentEmail ? record.studentEmail : "";
 
-        let teacher_ein        = row.TEACHER_EIN ? row.TEACHER_EIN : '0';
-        let teacher_email      = row.TEACHER_EMAIL ? row.TEACHER_EMAIL : '0';
+        let staffNumber       = record.staffNumber ? record.staffNumber : "";
 
-        let classObjectID     = sanitizeID(`${school_code}-${class_code}`);
-        let enrolmentObjectID = sanitizeID(`${classObjectID}-${student_number}`);
-
-        // Extract the 'enrolment' object from the row
-        let enrolmentObject = {
-            id:                 enrolmentObjectID,
-            school_code:        school_code,
-            class_code:         class_code,
-            student_number:     student_number,
-            student_first_name: student_first_name,
-            student_last_name:  student_last_name,
-            student_email:      student_email,
-            teacher_ein:        teacher_ein,
-            teacher_email:      teacher_email
-        };
-
-        if (!enrolmentsSortedObject[enrolmentObject.school_code]) {
-            enrolmentsSortedObject[enrolmentObject.school_code] = {};
+        if (schoolCode !== "" && classCode !== "" && studentNumber !== "") {
+            let classObjectID     = sanitizeID(`${schoolCode}-${classCode}`);
+            let enrolmentObjectID = sanitizeID(`${classObjectID}-${studentNumber}`);
+    
+            // Extract the 'enrolment' object from the row
+            let enrolmentObject = {
+                id:                enrolmentObjectID,
+                schoolCode:        schoolCode,
+                classCode:         classCode,
+                studentNumber:     studentNumber,
+                studentFirstName:  studentFirstName,
+                studentLastName:   studentLastName,
+                studentEmail:      studentEmail,
+                staffNumber:       staffNumber
+            } as TrilliumEnrolment;
+    
+            if (!enrolmentsSortedObject[enrolmentObject.schoolCode]) {
+                enrolmentsSortedObject[enrolmentObject.schoolCode] = {};
+            }
+            if (!enrolmentsSortedArrays[enrolmentObject.schoolCode]) {
+                enrolmentsSortedArrays[enrolmentObject.schoolCode] = [];
+            }
+    
+            enrolmentsSortedObject[enrolmentObject.schoolCode][enrolmentObjectID] = enrolmentObject;
+            enrolmentsSortedArrays[enrolmentObject.schoolCode].push(enrolmentObject);
         }
-        if (!enrolmentsSortedArrays[enrolmentObject.school_code]) {
-            enrolmentsSortedArrays[enrolmentObject.school_code] = [];
-        }
-
-        enrolmentsSortedObject[enrolmentObject.school_code][enrolmentObject.id] = enrolmentObject;
-        enrolmentsSortedArrays[enrolmentObject.school_code].push(enrolmentObject);
     });
 
     // Write out arrays and objects to blobs
     context.bindings.enrolmentsNowSortedArrays = JSON.stringify(enrolmentsSortedArrays);
     context.bindings.enrolmentsNowSortedObject = JSON.stringify(enrolmentsSortedObject);
 
-    let callbackMessage = {
-        id: 'skinner-functions-' + context.executionContext.functionName +'-'+ context.executionContext.invocationId,
-        eventType: 'Skinner.View.GClassroom.Extract.Enrolments',
-        eventTime: execution_timestamp,
-        //subject: ,
-        data: {
-            event_type: 'function_invocation',
-            app: 'wrdsb-skinner',
-            function_name: context.executionContext.functionName,
-            invocation_id: context.executionContext.invocationId,
-            data: {},
-            timestamp: execution_timestamp
-        },
-        dataVersion: '1'
-    };
-
-    context.bindings.callbackMessage = JSON.stringify(callbackMessage.data);
-
-    let sis_enrolments_reconcile_jobs = [];
-    Object.getOwnPropertyNames(enrolmentsSortedObject).forEach(school_code => {
-        sis_enrolments_reconcile_jobs.push({
+    let sisEnrolmentsReconcileJobs = [];
+    Object.getOwnPropertyNames(enrolmentsSortedObject).forEach(schoolCode => {
+        sisEnrolmentsReconcileJobs.push({
             job_type: "Skinner.Enrolment.Differences.Reconcile.Alpha",
-            alpha: school_code
+            alpha: schoolCode
         });
     }); 
-    context.bindings.triggerJobs = JSON.stringify(sis_enrolments_reconcile_jobs);
+    context.bindings.triggerJobs = JSON.stringify(sisEnrolmentsReconcileJobs);
 
-    context.log(JSON.stringify(callbackMessage));
-    context.done(null, callbackMessage);
+    const logPayload = "";
+    functionInvocation.logPayload = logPayload;
+    context.log(logPayload);
+
+    context.log(functionInvocation);
+    context.done(null, functionInvocation);
 
     function sanitizeID(id)
     {
@@ -97,4 +93,4 @@ const viewGClassroomExtractEnrolments: AzureFunction = async function (context: 
     }
 };
 
-export default viewGClassroomExtractEnrolments;
+export default viewGclassroomExtractEnrolments;
