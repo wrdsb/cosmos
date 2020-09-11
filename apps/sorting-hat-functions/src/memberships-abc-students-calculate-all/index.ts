88fcc17493ea101e6e2c6417307a822be70bd65d
James Schumann <james@schumann.ca>
2020-09-11 13:58:55 -0400

    create memberships-abc-students-calculate-all

---
+++ apps/sorting-hat-functions/src/memberships-abc-students-calculate-all/index.ts
@@ -0,0 +1,55 @@
+import { AzureFunction, Context } from "@azure/functions";
+
+const MembershipsABCStudentsCalculateAll: AzureFunction = async function (context: Context, triggerMessage: any): Promise<void> {
+    const triggerObject = triggerMessage;
+    const payload = triggerObject.payload;
+
+    context.log(payload);
+
+    const rows = context.bindings.studentsNow;
+    
+    let queueMessages = await prepareMessages(rows);
+
+    context.bindings.outputQueue = queueMessages;
+
+    const logPayload = {
+        queueMessages: queueMessages
+    };
+    context.log(logPayload);
+
+    context.done(null, logPayload);
+    
+    async function prepareMessages(rows) {
+        let messages = [];
+        let schoolCodes = new Set();
+
+        rows.forEach(function(row) {
+            let schoolCode = (row.school_code) ? row.school_code.toLowerCase() : 0;
+
+            if (isNaN(schoolCode)) {
+                schoolCodes.add(schoolCode);
+            }
+        });
+
+        schoolCodes.forEach(schoolCode => {
+            let message = {
+                payload: {
+                    schoolCode: schoolCode
+                }
+            };
+
+            messages.push(message);
+        });
+
+        let oyapMessage = {
+            payload: {
+                schoolCode: 'oyap'
+            }
+        };
+        messages.push(oyapMessage);
+
+        return messages;
+    }
+}
+
+export default MembershipsABCStudentsCalculateAll;
