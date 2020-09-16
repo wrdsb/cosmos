9eb7174b7715a5804203e341fc878a64fbce2838
James Schumann <james@schumann.ca>
2020-09-16 14:51:52 -0400

    add mat button

--- libs/user-auth/src/lib/user-auth.module.ts
+++ libs/user-auth/src/lib/user-auth.module.ts
@@ -1,5 +1,6 @@
 import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { MatButtonModule } from "@angular/material/button";
 
 import { EnvironmentService } from "@cosmos/environment";
 import { GraphService } from '@cosmos/msgraph-service';
@@ -69,6 +70,7 @@ function MSALAngularConfigFactory(): MsalAngularConfiguration {
   ],
   imports: [
     CommonModule,
+    MatButtonModule,
     MsalModule
   ],
   schemas: [CUSTOM_ELEMENTS_SCHEMA],
