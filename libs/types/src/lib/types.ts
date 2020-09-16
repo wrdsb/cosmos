8bf8d4843d8567270392b88308c8cfc9fc5df64c
James Schumann <james@schumann.ca>
2020-09-16 14:53:49 -0400

    add interface for tracking groups list request status

--- libs/types/src/lib/types.ts
+++ libs/types/src/lib/types.ts
@@ -18,6 +18,12 @@ export interface PingRequestState {
   error?: string;
 }
 
+export interface ListGroupsRequestState {
+  status: Status;
+  response?: string;
+  error?: string;
+}
+
 export enum Status {
   UNKNOWN = 'UNKNOWN',
   LOADING = 'LOADING',
