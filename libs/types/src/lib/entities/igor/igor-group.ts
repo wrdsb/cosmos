80cb78ef2f26af34733189a9ddf71967bd8742c9
James Schumann <james@schumann.ca>
2020-09-22 09:36:16 -0400

    Remove redundant attribute declarations from igor-group type

--- libs/types/src/lib/entities/igor/igor-group.ts
+++ libs/types/src/lib/entities/igor/igor-group.ts
@@ -2,17 +2,4 @@ import { GoogleGroup } from "../google";
 import { PeopleSetDefinition } from "../sorting-hat";
 
 export interface IGORGroup extends GoogleGroup {
-    business_owner?: string;
-
-    membership_automation_active?: boolean;
-    automate_managers?: boolean;
-    automate_members?: boolean;
-    automate_owners?: boolean;
-
-    managers_people_sets?: PeopleSetDefinition[];
-    members_people_sets?: PeopleSetDefinition[];
-    owners_people_sets?: PeopleSetDefinition[];
-
-    configuration_automation_active?: boolean;
-    configuration_templates?: string;
 }
