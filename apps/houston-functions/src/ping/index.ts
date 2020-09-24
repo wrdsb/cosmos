658a48562416e42b0120c31fa2efdebc0f9bcadc
James Schumann <james@schumann.ca>
2020-09-24 15:29:21 -0400

    bring ping function up to new standards

--- apps/houston-functions/src/ping/index.ts
+++ apps/houston-functions/src/ping/index.ts
@@ -1,21 +1,83 @@
-import { AzureFunction, Context, HttpRequest } from "@azure/functions"
+import { AzureFunction, Context, HttpRequest } from "@azure/functions";
+import jwt_decode from 'jwt-decode';
+import { FunctionInvocation } from "@cosmos/types";
 
-const httpTrigger: AzureFunction = async function (context: Context, req: HttpRequest): Promise<void> {
-    context.log('HTTP trigger function processed a request.');
-    const name = (req.query.name || (req.body && req.body.name));
+const ping: AzureFunction = async function (context: Context, req: HttpRequest): Promise<void> {
+    const functionInvocation = {
+        functionInvocationID: context.executionContext.invocationId,
+        functionInvocationTimestamp: new Date().toJSON(),
+        functionApp: 'Houston',
+        functionName: context.executionContext.functionName,
+        functionDataType: 'Ping',
+        functionDataOperation: 'Ping',
+        eventLabel: ''
+    } as FunctionInvocation;
 
-    if (name) {
-        context.res = {
-            // status: 200, /* Defaults to 200 */
-            body: "Hello " + (req.query.name || req.body.name)
-        };
+    const request = req;
+    let authenticated = false;
+    let authorized = false;
+    let idToken = '';
+    let userName = '';
+    let userEmail = '';
+    let userRoles = [];
+
+    if (request.headers['x-ms-token-aad-id-token']) {
+        authenticated = true;
+        idToken = request.headers['x-ms-token-aad-id-token'];
+        let decodedToken = jwt_decode(idToken);
+        userName = decodedToken.name,
+        userEmail = decodedToken.email;
+        userRoles = decodedToken.roles as string[];
+        authorized = userRoles.includes('cosmos-user-its') ? true : false;
+    }
+
+    let response = {
+        payload: {
+            status: 200,
+            message: "",
+            chatter: "",
+            timestamp: functionInvocation.functionInvocationTimestamp,
+            authenticated: authenticated,
+            authorized: authorized,
+            userName: userName,
+            userEmail: userEmail,
+            userRoles: userRoles
+        }
+    };
+
+    if (!authenticated) {
+        response.payload.status = 401;
+        response.payload.message = "Unauthorized: Cannot verify your identity.";
+        response.payload.chatter = "Unauthorized: Cannot verify your identity.";
+    }
+    else if (authenticated && !authorized) {
+        response.payload.status = 403;
+        response.payload.message = "Forbidden: You are not permitted to ping Houston";
+        response.payload.chatter = "Forbidden: You are not permitted to ping Houston";
+    }
+    else if (authenticated && authorized) {
+        response.payload.status = 200;
+        response.payload.message = "Houston is up";
+        response.payload.chatter = "Houston here. What can I do for you?";
     }
     else {
-        context.res = {
-            status: 400,
-            body: "Please pass a name on the query string or in the request body"
-        };
+        response.payload.status = 400;
+        response.payload.message = "Bad Request: We're not sure what happend, but we're pretty sure it's you, not us.";
+        response.payload.chatter = "Bad Request: We're not sure what happend, but we're pretty sure it's you, not us.";
     }
+
+    context.res = {
+        status: response.payload.status,
+        body: response
+    }
+
+    const logPayload = response;
+    context.log(logPayload);
+
+    functionInvocation.logPayload = logPayload;
+    context.log(functionInvocation);
+
+    context.done(null, functionInvocation);
 };
 
-export default httpTrigger;
+export default ping;
