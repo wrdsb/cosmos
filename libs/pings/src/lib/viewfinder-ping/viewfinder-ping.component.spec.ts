9cdbb646084d74cbc18717846eeefc2810b2c503
James Schumann <james@schumann.ca>
2020-09-30 13:21:07 -0400

    create viewfinder-ping component

---
+++ libs/pings/src/lib/viewfinder-ping/viewfinder-ping.component.spec.ts
@@ -0,0 +1,25 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { ViewfinderPingComponent } from './viewfinder-ping.component';
+
+describe('ViewfinderPingComponent', () => {
+  let component: ViewfinderPingComponent;
+  let fixture: ComponentFixture<ViewfinderPingComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ ViewfinderPingComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(ViewfinderPingComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ libs/pings/src/lib/viewfinder-ping/viewfinder-ping.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit } from '@angular/core';
+
+import { ViewfinderService } from "@cosmos/viewfinder-service";
+import { PingFunctionResponse, PingRequestState, Status } from "@cosmos/types";
+
+@Component({
+  selector: 'cosmos-viewfinder-ping',
+  templateUrl: './viewfinder-ping.component.html',
+  styleUrls: ['./viewfinder-ping.component.scss']
+})
+export class ViewfinderPingComponent implements OnInit {
+  readonly Status = Status;
+  readonly pingState$;
+  readonly pingRequestState$;
+
+  constructor(private viewfinderService: ViewfinderService) {
+    this.pingState$ = this.viewfinderService.pingState$;
+    this.pingRequestState$ = this.viewfinderService.pingRequestState$;
+  }
+
+  ngOnInit(): void {
+    //this.doPing();
+  }
+
+  doPing() {
+    this.viewfinderService.doPing();
+  }
+}
