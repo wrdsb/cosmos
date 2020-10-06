ff89445553cd5ac2dff3ab3805a53f112cf5f6ae
James Schumann <james@schumann.ca>
2020-10-06 10:47:49 -0400

    groups-search component working

--- libs/google-groups/src/lib/groups-search/groups-search.component.ts
+++ libs/google-groups/src/lib/groups-search/groups-search.component.ts
@@ -1,4 +1,19 @@
 import { Component, OnInit } from '@angular/core';
+import { MatDialog, MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { MatButtonModule } from "@angular/material/button";
+import { FormControl } from '@angular/forms';
+import { Observable, BehaviorSubject } from 'rxjs';
+
+import { faCircle as FalseIcon } from "@fortawesome/free-regular-svg-icons";
+import { faAdjust as MaybeIcon } from "@fortawesome/free-solid-svg-icons";
+import { faCircle as TrueIcon } from "@fortawesome/free-solid-svg-icons";
+import { faAngleUp, faAngleDown } from "@fortawesome/free-solid-svg-icons";
+import { faFastBackward, faBackward, faForward, faFastForward } from "@fortawesome/free-solid-svg-icons";
+
+import { GoogleGroup, SearchFunctionRequestPayload, SearchFunctionResponse } from "@cosmos/types";
+import { GoogleGroupsService } from '../google-groups.service';
+import { map, debounceTime, distinctUntilChanged, switchMap, tap } from 'rxjs/operators';
+import { GroupMetaDialogComponent } from "../group-meta-dialog/group-meta-dialog.component";
 
 @Component({
   selector: 'cosmos-groups-search',
@@ -6,10 +21,132 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./groups-search.component.scss']
 })
 export class GroupsSearchComponent implements OnInit {
+  FalseIcon = FalseIcon;
+  MaybeIcon = MaybeIcon;
+  TrueIcon = TrueIcon;
 
-  constructor() { }
+  angleUpIcon = faAngleUp;
+  angleDownIcon = faAngleDown;
 
-  ngOnInit(): void {
+  beginningIcon = faFastBackward;
+  backwardIcon = faBackward;
+  forwardIcon = faForward;
+  endIcon = faFastForward;
+
+  displayedColumns$ = new BehaviorSubject<string[]>([
+    'name',
+    'email',
+    'adminCreated',
+    'membership_automation_active'
+  ]);
+
+  searchFormControl = new FormControl();
+
+  currentPage = new BehaviorSubject<number>(1);
+  currentSearch = new BehaviorSubject<string>('*');
+  pageSize = new BehaviorSubject<number>(20);
+  maxPage = new BehaviorSubject<number>(1);
+  
+  //public searchGroupsRequestState$: Observable<ListGroupsRequestState>;
+  //public searchGroupsResponse$: Observable<GroupQueryFunctionResponse>;
+  
+  public searchResponse$: Observable<SearchFunctionResponse>;
+  public totalRecords$: Observable<number>;
+  public maxPage$: Observable<number>;
+  public groupsPage$: Observable<GoogleGroup[]>;
+
+  public groupSelected$: Observable<boolean>;
+  public selectedGroup$: Observable<GoogleGroup>;
+
+  groupMetaDialogRef: MatDialogRef<GroupMetaDialogComponent>;
+
+  constructor(
+    private groupsService: GoogleGroupsService,
+    public dialog: MatDialog
+  ) {
+    this.searchResponse$ = this.groupsService.searchResponse$;
+    this.totalRecords$ = this.searchResponse$.pipe(map(response => response.payload.count));
+    this.groupsPage$ = this.searchResponse$.pipe(map(response => response.payload.documents));
+    this.maxPage$ = this.totalRecords$.pipe(map(totalRecords => Math.ceil(totalRecords / this.pageSize.value)));
+    this.maxPage$.subscribe(this.maxPage);
+
+    this.groupSelected$ = this.groupsService.groupSelected$;
+    this.selectedGroup$ = this.groupsService.selectedGroup$;
   }
 
+  ngOnInit() {
+    this.searchGroups();
+  }
+
+  ngAfterViewInit() {
+    this.searchFormControl.valueChanges
+      .pipe(
+        debounceTime(400),
+        distinctUntilChanged()
+      )
+      .subscribe(searchTerm => {
+        console.log(searchTerm);
+        this.currentSearch.next(searchTerm);
+        this.searchGroups();
+      });
+  }
+
+  pageBeginning(): void {
+    this.currentPage.next(1);
+    this.searchGroups();
+  }
+
+  pageUp(): void {
+    const nextPage = this.currentPage.value - 1;
+    if (this.nextPageValid(nextPage)) {
+      this.currentPage.next(nextPage);
+      this.searchGroups();
+    }
+  }
+
+  pageDown(): void {
+    const nextPage = this.currentPage.value + 1;
+    if (this.nextPageValid(nextPage)) {
+      this.currentPage.next(nextPage);
+      this.searchGroups();
+    }
+  }
+
+  pageEnd(): void {
+    this.currentPage.next(this.maxPage.value);
+    this.searchGroups();
+  }
+
+  nextPageValid(nextPage): boolean {
+    return ( nextPage >= 1 && nextPage <= this.maxPage.value ) ? true : false;
+  }
+
+  selectGroup(groupEmail: string): void {
+    console.log(`Show details for ${groupEmail}`);
+    let nextGroup = this.groupsPage$.find(group => group.email === groupEmail);
+    this.groupsService.selectGroup(nextGroup);
+    this.groupMetaDialogRef = this.dialog.open(GroupMetaDialogComponent, {
+      width: '800px',
+      data: nextGroup
+    });
+  }
+
+  deselectGroup(): void {
+    console.log(`Deselect selected group`);
+    this.groupsService.deselectGroup();
+  }
+
+  searchGroups(): void {
+    let searchRequestPayload = {
+      search: this.currentSearch.value,
+      skip: (this.currentPage.value - 1) * this.pageSize.value
+    } as SearchFunctionRequestPayload;
+    
+    this.groupsService.searchGroups(searchRequestPayload);
+  }
+}
+
+interface searchGroupsRequest {
+  search?: string;
+  page?: number;
 }
