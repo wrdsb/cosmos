033416d74b6ae0482ef43fa1210b4adeec9b2803
James Schumann <james@schumann.ca>
2020-10-07 09:35:11 -0400

    generate calendar-search component

---
+++ libs/google-calendar/src/lib/calendar-search/calendar-search.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'cosmos-calendar-search',
+  templateUrl: './calendar-search.component.html',
+  styleUrls: ['./calendar-search.component.scss']
+})
+export class CalendarSearchComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
