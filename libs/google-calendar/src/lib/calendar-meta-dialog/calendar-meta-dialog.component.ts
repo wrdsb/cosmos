7ba4542ac1a2910ae3ca82855e4cc524adf1cea3
James Schumann <james@schumann.ca>
2020-10-20 09:08:24 -0400

    create meta-dialog component for google calendars

--- libs/google-calendar/src/lib/calendar-meta-dialog/calendar-meta-dialog.component.ts
+++ libs/google-calendar/src/lib/calendar-meta-dialog/calendar-meta-dialog.component.ts
@@ -1,15 +1,20 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from "rxjs";
+
+import { GoogleCalendar } from "@cosmos/types";
+import { GoogleCalendarService } from '../google-calendar.service';
 
 @Component({
   selector: 'cosmos-calendar-meta-dialog',
   templateUrl: './calendar-meta-dialog.component.html',
   styleUrls: ['./calendar-meta-dialog.component.scss']
 })
-export class CalendarMetaDialogComponent implements OnInit {
+export class CalendarMetaDialogComponent {
+  public selectedCalendar$: Observable<GoogleCalendar>;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(
+    private calendarService: GoogleCalendarService
+  ) {
+    this.selectedCalendar$ = this.calendarService.selectedCalendar$;
   }
-
 }
