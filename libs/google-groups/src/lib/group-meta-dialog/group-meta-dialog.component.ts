59d405db3d7e8cb5fcaa795dd0be816b9f59cb1a
James Schumann <james@schumann.ca>
2020-10-20 09:08:37 -0400

    remove unused imports

--- libs/google-groups/src/lib/group-meta-dialog/group-meta-dialog.component.ts
+++ libs/google-groups/src/lib/group-meta-dialog/group-meta-dialog.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit, Inject } from '@angular/core';
 import { Observable } from "rxjs";
-import { MatDialog, MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 
 import { GoogleGroup } from "@cosmos/types";
 import { GoogleGroupsService } from '../google-groups.service';
