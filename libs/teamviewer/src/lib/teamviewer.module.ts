05d571f98cf14ceb678532ea04594e2d17b9a3c7
James Schumann <james@schumann.ca>
2020-10-21 12:16:07 -0400

    stubbed teamviewer home

--- libs/teamviewer/src/lib/teamviewer.module.ts
+++ libs/teamviewer/src/lib/teamviewer.module.ts
@@ -1,9 +1,25 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+
+import { HTTP_INTERCEPTORS } from '@angular/common/http';
+import { MsalInterceptor } from '@azure/msal-angular';
+
+import { TeamviewerRoutingModule } from "./teamviewer-routing.module";
 import { HomeComponent } from './home/home.component';
 
 @NgModule({
-  imports: [CommonModule],
-  declarations: [HomeComponent]
+  declarations: [
+    HomeComponent
+  ],
+  imports: [
+    CommonModule,
+    TeamviewerRoutingModule
+  ],
+  exports: [
+    HomeComponent
+  ],
+  providers: [
+    { provide: HTTP_INTERCEPTORS, useClass: MsalInterceptor, multi: true }
+  ]
 })
 export class TeamviewerModule {}
