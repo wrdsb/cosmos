97a2bec67652f83dfdcd669d35fdae68dd18eca1
James Schumann <james@schumann.ca>
2020-10-21 12:15:31 -0400

    trillium home stubbed

--- libs/trillium/src/lib/trillium.module.ts
+++ libs/trillium/src/lib/trillium.module.ts
@@ -1,9 +1,25 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+
+import { HTTP_INTERCEPTORS } from '@angular/common/http';
+import { MsalInterceptor } from '@azure/msal-angular';
+
+import { TrilliumRoutingModule } from "./trillium-routing.module";
 import { HomeComponent } from './home/home.component';
 
 @NgModule({
-  imports: [CommonModule],
-  declarations: [HomeComponent]
+  declarations: [
+    HomeComponent
+  ],
+  imports: [
+    CommonModule,
+    TrilliumRoutingModule
+  ],
+  exports: [
+    HomeComponent
+  ],
+  providers: [
+    { provide: HTTP_INTERCEPTORS, useClass: MsalInterceptor, multi: true }
+  ]
 })
 export class TrilliumModule {}
