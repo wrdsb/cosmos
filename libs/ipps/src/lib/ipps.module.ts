cffb6c9345cf5477076cfb299bdbe2b3553c1bd3
James Schumann <james@schumann.ca>
2020-10-21 12:16:49 -0400

    stubbed ipps home

--- libs/ipps/src/lib/ipps.module.ts
+++ libs/ipps/src/lib/ipps.module.ts
@@ -1,9 +1,25 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+
+import { HTTP_INTERCEPTORS } from '@angular/common/http';
+import { MsalInterceptor } from '@azure/msal-angular';
+
+import { IppsRoutingModule } from "./ipps-routing.module";
 import { HomeComponent } from './home/home.component';
 
 @NgModule({
-  imports: [CommonModule],
-  declarations: [HomeComponent]
+  declarations: [
+    HomeComponent
+  ],
+  imports: [
+    CommonModule,
+    IppsRoutingModule
+  ],
+  exports: [
+    HomeComponent
+  ],
+  providers: [
+    { provide: HTTP_INTERCEPTORS, useClass: MsalInterceptor, multi: true }
+  ]
 })
 export class IppsModule {}
