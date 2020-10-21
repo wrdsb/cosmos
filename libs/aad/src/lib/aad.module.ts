303e3ae77e7ed0ef949ca6f3ae57430b7f589aaf
James Schumann <james@schumann.ca>
2020-10-21 11:06:49 -0400

    ng g component home --project=aad

--- libs/aad/src/lib/aad.module.ts
+++ libs/aad/src/lib/aad.module.ts
@@ -1,7 +1,9 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
-  imports: [CommonModule]
+  imports: [CommonModule],
+  declarations: [HomeComponent]
 })
 export class AadModule {}
