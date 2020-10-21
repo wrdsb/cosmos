58d6f4695c28664effdfd715f0471446f9fbf08f
James Schumann <james@schumann.ca>
2020-10-21 12:16:36 -0400

    stubbed people home

---
+++ libs/people/src/lib/people-routing.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { MsalGuard } from '@azure/msal-angular';
+import { RolesGuard } from "@cosmos/guards";
+
+import { HomeComponent } from "./home/home.component";
+
+const routes: Routes = [
+  { 
+    path: '',
+    component: HomeComponent,
+    data: {
+      roles: ['cosmos-superuser', 'cosmos-user-its']
+    },
+    canActivate: [
+      MsalGuard, RolesGuard
+    ]
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule]
+})
+
+export class PeopleRoutingModule { }
