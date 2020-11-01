e00d93dd5384cf424e76c72405a40f6cb5053941
James Schumann <james@schumann.ca>
2020-11-01 16:13:48 -0500

    add new fields to trillium data

--- libs/types/src/lib/entities/trillium/trillium-class.ts
+++ libs/types/src/lib/entities/trillium/trillium-class.ts
@@ -2,6 +2,7 @@ interface TrilliumClass {
   id?: string;
   school_code: string;
   class_code: string;
+  class_grades: string[];
   teacher_ein?: string;
   teacher_email?: string;
   staff_number: string;
--- libs/types/src/lib/entities/trillium/view-gclassroom-record.ts
+++ libs/types/src/lib/entities/trillium/view-gclassroom-record.ts
@@ -3,10 +3,12 @@ interface ViewGclassroomRecord {
   class_code: string;
 
   student_number: string;
+  student_grade: string;
   student_first_name: string;
   student_last_name: string;
   student_email: string;
   student_oyap: string;
+  student_shsm_sector: string;
 
   staff_number: string;
 }
