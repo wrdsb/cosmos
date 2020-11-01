d18cf22aafd3be097c9ea981431869073a10728c
James Schumann <james@schumann.ca>
2020-11-01 16:15:02 -0500

    process new fields

--- apps/skinner-functions/src/view-gclassroom-process/index.ts
+++ apps/skinner-functions/src/view-gclassroom-process/index.ts
@@ -20,29 +20,33 @@ const viewGClassroomProcess: AzureFunction = async function (context: Context, t
     let rowsArray = [];
 
     rows.forEach(function(row) {
-        let school_code        = row.SCHOOL_CODE ? row.SCHOOL_CODE.trim() : "";
-        let class_code         = row.CLASS_CODE ? row.CLASS_CODE.trim() : "";
+        let school_code         = row.SCHOOL_CODE ? row.SCHOOL_CODE.trim() : "";
+        let class_code          = row.CLASS_CODE ? row.CLASS_CODE.trim() : "";
 
-        let student_number     = row.STUDENT_NO ? row.STUDENT_NO.trim() : "";
+        let student_number      = row.STUDENT_NO ? row.STUDENT_NO.trim() : "";
+        let student_grade       = row.GRADE ? row.GRADE.trim() : "";
         let student_first_name  = row.STUDENT_FIRST_NAME ? row.STUDENT_FIRST_NAME.trim() : "";
         let student_last_name   = row.STUDENT_LAST_NAME ? row.STUDENT_LAST_NAME.trim() : "";
-        let student_email      = row.STUDENT_EMAIL ? row.STUDENT_EMAIL.trim() : "";
-        let student_oyap       = row.OYAP ? row.OYAP.trim() : "";
+        let student_email       = row.STUDENT_EMAIL ? row.STUDENT_EMAIL.trim() : "";
+        let student_oyap        = row.OYAP ? row.OYAP.trim() : "";
+        let student_shsm_sector = row.SHSM_SECTOR ? row.SHSM_SECTOR.trim() : "";
 
-        let staff_number       = row.STAFF_NO ? row.STAFF_NO.trim() : "";
+        let staff_number        = row.TEACHER_EIN ? row.TEACHER_EIN.trim() : "";
 
         // Build the row object from the data
         let rowObject = {
-            school_code:        school_code,
-            class_code:         class_code,
+            school_code:         school_code,
+            class_code:          class_code,
 
-            student_number:     student_number,
+            student_number:      student_number,
+            student_grade:       student_grade,
             student_first_name:  student_first_name,
             student_last_name:   student_last_name,
-            student_email:      student_email,
-            student_oyap:       student_oyap,
+            student_email:       student_email,
+            student_oyap:        student_oyap,
+            student_shsm_sector: student_shsm_sector,
 
-            staff_number:       staff_number
+            staff_number:        staff_number
         } as ViewGclassroomRecord;
         
         rowsArray.push(rowObject);
