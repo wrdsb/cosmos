82153bb8ce816eaa8fa81827c5813284792a8019
James Schumann <james@schumann.ca>
2020-11-01 21:28:09 -0500

    shsm groups membership calculations working

--- apps/igor-functions/src/group-memberships-shsm-calculate-all/index.ts
+++ apps/igor-functions/src/group-memberships-shsm-calculate-all/index.ts
@@ -1,103 +1,58 @@
 import { AzureFunction, Context } from "@azure/functions";
-import { createLogObject } from "@cosmos/azure-functions-shared";
-import { storeLogBlob } from "@cosmos/azure-functions-shared";
-import { createCallbackMessage } from "@cosmos/azure-functions-shared";
-import { createEvent } from "@cosmos/azure-functions-shared";
-import { GroupMembershipsStudentsCalculateAllFunctionRequest, GroupMembershipsStudentsCalculateAllFunctionRequestPayload } from "@cosmos/types";
+import { FunctionInvocation, GroupMembershipsStudentsCalculateAllFunctionRequest, GroupMembershipsStudentsCalculateAllFunctionRequestPayload } from "@cosmos/types";
 
-const GroupMembershipsStudentsCalculateAll: AzureFunction = async function (context: Context, triggerMessage: GroupMembershipsStudentsCalculateAllFunctionRequest): Promise<void> {
-    const functionInvocationID = context.executionContext.invocationId;
-    const functionInvocationTime = new Date();
-    const functionInvocationTimestamp = functionInvocationTime.toJSON();  // format: 2012-04-23T18:25:43.511Z
-
-    const functionName = context.executionContext.functionName;
-    const functionEventType = 'WRDSB.IGOR.Google.Group.Memberships.Students.Calculate.All';
-    const functionEventID = `igor-functions-${functionName}-${functionInvocationID}`;
-    const functionLogID = `${functionInvocationTime.getTime()}-${functionInvocationID}`;
-
-    const logStorageAccount = process.env['storageAccount'];
-    const logStorageKey = process.env['storageKey'];
-    const logStorageContainer = 'function-group-memberships-shsm-calculate-all-logs';
-
-    const eventLabel = '';
-    const eventTags = [
-        "igor", 
-    ];
+const GroupMembershipsSHSMCalculateAll: AzureFunction = async function (context: Context, triggerMessage: GroupMembershipsStudentsCalculateAllFunctionRequest): Promise<void> {
+    const functionInvocation = {
+        functionInvocationID: context.executionContext.invocationId,
+        functionInvocationTimestamp: new Date().toJSON(),
+        functionApp: 'IGOR',
+        functionName: context.executionContext.functionName,
+        functionDataType: 'GroupMemberships',
+        functionDataOperation: 'CalculateSHAMAll',
+        eventLabel: ''
+    } as FunctionInvocation;
 
     const triggerObject = triggerMessage as GroupMembershipsStudentsCalculateAllFunctionRequest;
     const payload = triggerObject.payload as GroupMembershipsStudentsCalculateAllFunctionRequestPayload;
 
     context.log(payload);
 
-    const rows = context.bindings.studentsNow;
-    
-    let queueMessages = await prepareMessages(rows);
+    const sectors = [
+        'agriculture',
+        'arts',
+        'business',
+        'construction',
+        'environment',
+        'health',
+        'hospitality',
+        'communication',
+        'manufacturing',
+        'non-profit',
+        'sports',
+        'transportation'
+    ];
+    let queueMessages = [];
 
+    sectors.forEach(sector => {
+        let message = {
+            payload: {
+                sector: sector
+            }
+        };
+
+        queueMessages.push(message);
+    });
+    
     context.bindings.outputQueue = queueMessages;
 
     const logPayload = {
         queueMessages: queueMessages
     };
+    functionInvocation.logPayload = logPayload;
     context.log(logPayload);
 
-    const logObject = await createLogObject(functionInvocationID, functionInvocationTime, functionName, logPayload);
-    const logBlob = await storeLogBlob(logStorageAccount, logStorageKey, logStorageContainer, logObject);
-    context.log(logBlob);
-
-    const callbackMessage = await createCallbackMessage(logObject, 200);
-    context.bindings.callbackMessage = JSON.stringify(callbackMessage);
-    context.log(callbackMessage);
-
-    const invocationEvent = await createEvent(
-        functionInvocationID,
-        functionInvocationTime,
-        functionInvocationTimestamp,
-        functionName,
-        functionEventType,
-        functionEventID,
-        functionLogID,
-        logStorageAccount,
-        logStorageContainer,
-        eventLabel,
-        eventTags
-    );
-    context.bindings.flynnEvent = JSON.stringify(invocationEvent);
-    context.log(invocationEvent);
-
-    context.done(null, logBlob);
-
-    
-    async function prepareMessages(rows) {
-        let messages = [];
-        let schoolCodes = new Set();
-
-        rows.forEach(function(row) {
-            let schoolCode = (row.school_code) ? row.school_code.toLowerCase() : 0;
-
-            if (isNaN(schoolCode)) {
-                schoolCodes.add(schoolCode);
-            }
-        });
-
-        schoolCodes.forEach(schoolCode => {
-            let message = {
-                payload: {
-                    schoolCode: schoolCode
-                }
-            };
-
-            messages.push(message);
-        });
-
-        let oyapMessage = {
-            payload: {
-                schoolCode: 'oyap'
-            }
-        };
-        messages.push(oyapMessage);
-
-        return messages;
-    }
+    context.log(functionInvocation);
+    context.done(null, functionInvocation);
 }
 
-export default GroupMembershipsStudentsCalculateAll;
+export default GroupMembershipsSHSMCalculateAll;
--- apps/igor-functions/src/group-memberships-shsm-calculate/index.ts
+++ apps/igor-functions/src/group-memberships-shsm-calculate/index.ts
@@ -1,28 +1,16 @@
 import { AzureFunction, Context } from "@azure/functions";
-import { createLogObject } from "@cosmos/azure-functions-shared";
-import { storeLogBlob } from "@cosmos/azure-functions-shared";
-import { createCallbackMessage } from "@cosmos/azure-functions-shared";
-import { createEvent } from "@cosmos/azure-functions-shared";
-import { GoogleGroupsMembershipsSHSMCalculateFunctionRequest, GoogleGroupsMembershipsSHSMCalculateFunctionRequestPayload } from "@cosmos/types";
+import { FunctionInvocation, GoogleGroupsMembershipsSHSMCalculateFunctionRequest, GoogleGroupsMembershipsSHSMCalculateFunctionRequestPayload } from "@cosmos/types";
 
 const GroupMembershipsSHSMCalculate: AzureFunction = async function (context: Context, triggerMessage: GoogleGroupsMembershipsSHSMCalculateFunctionRequest): Promise<void> {
-    const functionInvocationID = context.executionContext.invocationId;
-    const functionInvocationTime = new Date();
-    const functionInvocationTimestamp = functionInvocationTime.toJSON();  // format: 2012-04-23T18:25:43.511Z
-
-    const functionName = context.executionContext.functionName;
-    const functionEventType = 'WRDSB.IGOR.Google.Group.Memberships.SHSM.Calculate';
-    const functionEventID = `igor-functions-${functionName}-${functionInvocationID}`;
-    const functionLogID = `${functionInvocationTime.getTime()}-${functionInvocationID}`;
-
-    const logStorageAccount = process.env['storageAccount'];
-    const logStorageKey = process.env['storageKey'];
-    const logStorageContainer = 'function-group-memberships-shsm-calculate-logs';
-
-    const eventLabel = '';
-    const eventTags = [
-        "igor", 
-    ];
+    const functionInvocation = {
+        functionInvocationID: context.executionContext.invocationId,
+        functionInvocationTimestamp: new Date().toJSON(),
+        functionApp: 'IGOR',
+        functionName: context.executionContext.functionName,
+        functionDataType: 'GroupMemberships',
+        functionDataOperation: 'CalculateSHSM',
+        eventLabel: ''
+    } as FunctionInvocation;
 
     const triggerObject = triggerMessage as GoogleGroupsMembershipsSHSMCalculateFunctionRequest;
     const payload = triggerObject.payload as GoogleGroupsMembershipsSHSMCalculateFunctionRequestPayload;
@@ -42,49 +30,70 @@ const GroupMembershipsSHSMCalculate: AzureFunction = async function (context: Co
         requestedSector: requestedSector,
         memberCounts: memberCounts
     };
+    functionInvocation.logPayload = logPayload;
     context.log(logPayload);
 
-    const logObject = await createLogObject(functionInvocationID, functionInvocationTime, functionName, logPayload);
-    const logBlob = await storeLogBlob(logStorageAccount, logStorageKey, logStorageContainer, logObject);
-    context.log(logBlob);
-
-    const callbackMessage = await createCallbackMessage(logObject, 200);
-    context.bindings.callbackMessage = JSON.stringify(callbackMessage);
-    context.log(callbackMessage);
-
-    const invocationEvent = await createEvent(
-        functionInvocationID,
-        functionInvocationTime,
-        functionInvocationTimestamp,
-        functionName,
-        functionEventType,
-        functionEventID,
-        functionLogID,
-        logStorageAccount,
-        logStorageContainer,
-        eventLabel,
-        eventTags
-    );
-    context.bindings.flynnEvent = JSON.stringify(invocationEvent);
-    context.log(invocationEvent);
-
-    context.done(null, logBlob);
-
+    context.log(functionInvocation);
+    context.done(null, functionInvocation);
 
     async function calculateMembers(rows) {
         let members = {};
 
         rows.forEach(function(row) {
             let email = (row.student_email) ? row.student_email : false;
-            let sector = (row.shsm_sector) ? row.shsm_sector.toLowerCase() : false;
+            let sector_raw = (row.student_shsm_sector) ? row.student_shsm_sector.toLowerCase() : false;
+            let group_slug = null;
 
-            if (email && (requestedSector === sector)) {
+            if (sector_raw) {
+                switch (sector_raw) {
+                    case 'agriculture':
+                        group_slug = 'agriculture';
+                        break;
+                    case 'arts and culture':
+                        group_slug = 'arts';
+                        break;
+                    case 'business':
+                        group_slug = 'business';
+                        break;
+                    case 'construction':
+                        group_slug = 'construction';
+                        break;
+                    case 'environment':
+                        group_slug = 'environment';
+                        break;
+                    case 'health and wellness':
+                        group_slug = 'health';
+                        break;
+                    case 'hospitality and tourism':
+                        group_slug = 'hospitality';
+                        break;
+                    case 'information and communications technology':
+                        group_slug = 'communication';
+                        break;
+                    case 'manufacturing':
+                        group_slug = 'manufacturing';
+                        break;
+                    case 'non-profit':
+                        group_slug = 'non-profit';
+                        break;
+                    case 'sports':
+                        group_slug = 'sports';
+                        break;
+                    case 'transportation':
+                        group_slug = 'transportation';
+                        break;
+                    default:
+                        break;
+                }
+            }
+
+            if (email && (requestedSector === group_slug)) {
                 members[email] = {
                     email:          email,
                     role:           "MEMBER",
                     status:         "ACTIVE",
                     type:           "USER",
-                    groupKey:       `shsm-students-${sector}@wrdsb.ca`
+                    groupKey:       `shsm-students-${group_slug}@wrdsb.ca`
                 };
             }
         });
