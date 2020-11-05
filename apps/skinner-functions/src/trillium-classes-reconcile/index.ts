be75d472140ca33bad3c8483e35f06861f8af810
James Schumann <james@schumann.ca>
2020-11-05 09:11:13 -0500

    create trillium-classes-reconcile

---
+++ apps/skinner-functions/src/trillium-classes-reconcile/index.ts
@@ -0,0 +1,280 @@
+import { AzureFunction, Context } from "@azure/functions"
+import { CosmosClient } from "@azure/cosmos";
+import { FunctionInvocation, TrilliumClassesReconcileFunctionRequest, TrilliumClassesReconcileFunctionRequestPayload, ViewGclassroomRecord, TrilliumClass } from "@cosmos/types";
+
+const trilliumClassesReconcile: AzureFunction = async function (context: Context, triggerMessage: TrilliumClassesReconcileFunctionRequest): Promise<void> {
+    const functionInvocation = {
+        functionInvocationID: context.executionContext.invocationId,
+        functionInvocationTimestamp: new Date().toJSON(),
+        functionApp: 'Skinner',
+        functionName: context.executionContext.functionName,
+        functionDataType: 'TrilliumClass',
+        functionDataOperation: 'Reconcile',
+        eventLabel: ''
+    } as FunctionInvocation;
+
+    const cosmosEndpoint = process.env['cosmosEndpoint'];
+    const cosmosKey = process.env['cosmosKey'];
+    const cosmosDatabase = process.env['cosmosDatabase'];
+    const cosmosContainer = 'classes';
+    const cosmosClient = new CosmosClient({endpoint: cosmosEndpoint, key: cosmosKey});
+
+    const triggerObject = triggerMessage as TrilliumClassesReconcileFunctionRequest;
+    const payload = triggerObject.payload as TrilliumClassesReconcileFunctionRequestPayload;
+
+    // give our bindings more human-readable names
+    const recordsNow = context.bindings.recordsNow;
+
+    // fetch current records from Cosmos
+    const recordsPrevious = await getCosmosItems(cosmosClient, cosmosDatabase, cosmosContainer).catch(err => {
+        context.log(err);
+    });
+
+    context.log('Reconcile classes: ' + Object.getOwnPropertyNames(recordsPrevious).length);
+
+    // object to store our total diff as we build it
+    let calculation = {
+        records_previous: recordsPrevious,
+        records_now: recordsNow,
+        differences: {
+            created_records: [],
+            updated_records: [],
+            deleted_records: []
+        }
+    };
+
+    calculation = await findCreatesAndUpdates(calculation);
+    calculation = await findDeletes(calculation);
+
+    let creates = await processCreates(calculation.differences.created_records);
+    let updates = await processUpdates(calculation.differences.updated_records);
+    let deletes = await processDeletes(calculation.differences.deleted_records);
+
+    let totalDifferences = creates.length + updates.length + deletes.length;
+
+    context.bindings.queueStore = creates.concat(updates, deletes);
+
+    const logPayload = {
+        totalDifferences: totalDifferences,
+        differences: calculation.differences
+    };
+    functionInvocation.logPayload = logPayload;
+    context.log(logPayload);
+
+    context.log(functionInvocation);
+    context.done(null, functionInvocation);
+
+
+    async function findCreatesAndUpdates(calculation) {
+        context.log('findCreatesAndUpdates');
+
+        let records_previous = calculation.records_previous;
+        let records_now = calculation.records_now;
+
+        if (!records_now) {
+            return calculation;
+        }
+
+        // loop through all records in records_now, looking for updates and creates
+        Object.getOwnPropertyNames(records_now).forEach(function (record_id) {
+            const new_record = {
+                id:                  records_now[record_id].id,
+                school_code:         records_now[record_id].school_code,
+                class_code:          records_now[record_id].class_code,
+                class_grades:        records_now[record_id].class_grades,
+                staff_number:        records_now[record_id].staff_number,
+                teacher_ein:         records_now[record_id].staff_number,
+                teacher_email:       records_now[record_id].teacher_email,
+                teacher_name:        records_now[record_id].teacher_name
+
+                // these fields are not present in the data from trillium, so we don't map them
+                //created_at
+                //updated_at
+                //deleted_at
+                //deleted
+            };
+    
+            if (!records_previous || !records_previous[record_id]) {
+                calculation.differences.created_records.push(new_record);
+            } else {
+                // get the corresponding record in records_previous
+                const old_record = {
+                    id:                  records_previous[record_id].id,
+                    school_code:         records_previous[record_id].school_code,
+                    class_code:          records_previous[record_id].class_code,
+                    class_grades:        records_previous[record_id].class_grades,
+                    staff_number:        records_previous[record_id].staff_number,
+                    teacher_ein:         records_previous[record_id].teacher_ein,
+                    teacher_email:       records_previous[record_id].teacher_email,
+                    teacher_name:        records_previous[record_id].teacher_name
+    
+                    // these fields are not present in the data from trillium, so we don't map them
+                    //created_at
+                    //updated_at
+                    //deleted_at
+                    //deleted
+                }; 
+    
+                // Compare old and new records
+                let records_equal = true;
+                
+                records_equal = (records_equal && new_record.id === old_record.id) ? true : false;
+                records_equal = (records_equal && new_record.school_code === old_record.school_code) ? true : false;
+                records_equal = (records_equal && new_record.class_code === old_record.class_code) ? true : false;
+                records_equal = (records_equal && new_record.staff_number === old_record.staff_number) ? true : false;
+                records_equal = (records_equal && new_record.teacher_ein === old_record.teacher_ein) ? true : false;
+                records_equal = (records_equal && new_record.teacher_email === old_record.teacher_email) ? true : false;
+                records_equal = (records_equal && new_record.teacher_name === old_record.teacher_name) ? true : false;
+
+                records_equal = (records_equal && arrayCompare(new_record.class_grades, old_record.class_grades)) ? true : false;
+    
+                // if record changed, record the change
+                if (!records_equal) {
+                    calculation.differences.updated_records.push({
+                        previous: old_record,
+                        now: new_record
+                    });
+                }
+            }
+        });
+        return calculation;
+    }
+
+    function arrayCompare(firstArray, secondArray) {
+        if (firstArray.length !== secondArray.length){
+            return false;
+        };
+        for (let i = 0; i < firstArray.length; i++){
+            if(!secondArray.includes(firstArray[i])){
+                return false;
+            };
+        };
+        return true;
+    }
+
+    async function findDeletes(calculation) {
+        context.log('findDeletes');
+
+        let records_previous = calculation.records_previous;
+        let records_now = calculation.records_now;
+
+        if (!records_previous) {
+            return calculation;
+        }
+
+        // loop through all records in records_previous, looking for deletes
+        Object.getOwnPropertyNames(records_previous).forEach(function (record_id) {
+            if (!records_now || !records_now[record_id]) {
+                calculation.differences.deleted_records.push(records_previous[record_id]);
+            }
+        });
+
+        return calculation;
+    }
+
+    async function processCreates(created_records) {
+        context.log('processCreates');
+
+        // array for the results being returned
+        let messages = [];
+
+        created_records.forEach(function (record) {
+            let message = {
+                operation: "replace",
+                payload: record
+            };
+            messages.push(JSON.stringify(message));
+        });
+
+        return messages;
+    }
+
+    async function processUpdates(updated_records) {
+        context.log('processUpdates');
+
+        // array for the results being returned
+        let messages = [];
+
+        updated_records.forEach(function (record) {
+            let message = {
+                operation: "replace",
+                payload: record.now
+            };
+            messages.push(JSON.stringify(message));
+        });
+
+        return messages;
+    }
+
+    async function processDeletes(deleted_records) {
+        context.log('processDeletes');
+
+        // array for the results being returned
+        let messages = [];
+
+        deleted_records.forEach(function (record) {
+            let message = {
+                operation: "delete",
+                payload: record
+            };
+            messages.push(JSON.stringify(message));
+        });
+
+        return messages;
+    }
+
+    async function getCosmosItems(cosmosClient, cosmosDatabase, cosmosContainer) {
+        context.log('getCosmosItems');
+
+        let records_previous = {};
+
+        const querySpec = {
+            query: `SELECT * FROM c WHERE c.deleted = false`
+        }
+
+        const queryOptions  = {
+            maxItemCount: -1,
+            enableCrossPartitionQuery: true
+        }
+
+        try {
+            const { resources } = await cosmosClient.database(cosmosDatabase).container(cosmosContainer).items.query(querySpec).fetchAll();
+
+            for (const item of resources) {
+                if (!item.deleted) {
+                    let classObject = {
+                        id: item.id,
+                        school_code: item.school_code,
+                        class_code: item.class_code,
+                        class_grades: item.class_grades,
+                        staff_number: item.staff_number,
+                        teacher_email: item.teacher_email,
+                        teacher_name: item.teacher_name
+    
+                        // these fields are not present in the data from trillium
+                        //created_at: item.created_at,
+                        //updated_at: item.updated_at,
+                        //deleted_at: item.deleted_at,
+                        //deleted: item.deleted
+                    } as TrilliumClass;
+        
+                    records_previous[item.id] = classObject;
+                }
+            }
+    
+            return records_previous;
+        } catch (error) {
+            context.log(error);
+    
+            context.res = {
+                status: 500,
+                body: error
+            };
+    
+            context.done(error);
+        }
+    }
+
+};
+
+export default trilliumClassesReconcile;
