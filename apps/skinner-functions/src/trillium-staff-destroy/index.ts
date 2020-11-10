17700119ba03bd5afe89360fd5b06363ad978cb4
James Schumann <james@schumann.ca>
2020-11-10 09:15:00 -0500

    create trillium-staff-destroy

---
+++ apps/skinner-functions/src/trillium-staff-destroy/index.ts
@@ -0,0 +1,203 @@
+import { AzureFunction, Context } from "@azure/functions"
+import { FunctionInvocation, TrilliumStaffDestroyFunctionRequest, TrilliumStaffDestroyFunctionRequestPayload, TrilliumStaff } from "@cosmos/types";
+
+const trilliumStaffStore: AzureFunction = async function (context: Context, triggerMessage: TrilliumStaffDestroyFunctionRequest): Promise<void> {
+    const functionInvocation = {
+        functionInvocationID: context.executionContext.invocationId,
+        functionInvocationTimestamp: new Date().toJSON(),
+        functionApp: 'Skinner',
+        functionName: context.executionContext.functionName,
+        functionDataType: 'TrilliumStaff',
+        functionDataOperation: 'Destroy',
+        eventLabel: ''
+    } as FunctionInvocation;
+
+    const triggerObject = triggerMessage as TrilliumStaffDestroyFunctionRequest;
+    const operation = triggerObject.operation;
+    const payload = triggerObject.payload as TrilliumStaffDestroyFunctionRequestPayload;
+
+    let result;
+
+    let statusCode;
+    let statusMessage;
+
+    switch (operation) {
+        case 'destroy':
+            result = doDestroy(payload);
+            statusCode = '200';
+            statusMessage = 'Success: Staff record destroyed.';
+            break;
+        default:
+            break;
+    }
+
+    context.bindings.recordOut = result.newRecord;
+
+    const logPayload = result.event;
+    functionInvocation.logPayload = logPayload;
+    context.log(logPayload);
+
+    context.log(functionInvocation);
+    context.done(null, functionInvocation);
+
+
+    function doDelete(oldRecord, newRecord, payload) {
+        let event = {};
+
+        // check for existing record
+        if (!oldRecord) {
+            newRecord = Object.assign(newRecord, payload);
+            newRecord.created_at = functionInvocation.functionInvocationTimestamp;
+            newRecord.updated_at = functionInvocation.functionInvocationTimestamp;
+
+            // mark the record as deleted
+            newRecord.deleted_at = functionInvocation.functionInvocationTimestamp;
+            newRecord.deleted = true;
+
+            event = craftStaffDeleteEvent(oldRecord, newRecord);
+
+        } else {
+            newRecord = Object.assign(newRecord, oldRecord);
+
+            // mark the record as deleted
+            newRecord.deleted_at = functionInvocation.functionInvocationTimestamp;
+            newRecord.deleted = true;
+
+            event = craftStaffDeleteEvent(oldRecord, newRecord);
+        }
+
+        return {event: event, newRecord: newRecord};
+    }
+
+    function doPatch(oldRecord, newRecord, payload) {
+        let event = {};
+
+        if (!oldRecord) {
+            newRecord = Object.assign(newRecord, payload);
+            newRecord.created_at = functionInvocation.functionInvocationTimestamp;
+            newRecord.updated_at = functionInvocation.functionInvocationTimestamp;
+    
+            // patching a record implicitly undeletes it
+            newRecord.deleted_at = '';
+            newRecord.deleted = false;
+    
+            event = craftStaffCreateEvent(oldRecord, newRecord);
+
+        } else {
+            // Merge request object into current record
+            newRecord = Object.assign(newRecord, oldRecord, payload);
+            newRecord.updated_at = functionInvocation.functionInvocationTimestamp;
+    
+            // patching a record implicitly undeletes it
+            newRecord.deleted_at = '';
+            newRecord.deleted = false;
+
+            event = craftStaffUpdateEvent(oldRecord, newRecord);
+        }
+
+        return {event: event, newRecord: newRecord};
+    }
+    
+    function doReplace(oldRecord, newRecord, payload) {
+        let event = {};
+
+        newRecord = Object.assign(newRecord, payload);
+
+        if (!oldRecord) {
+            newRecord.created_at = functionInvocation.functionInvocationTimestamp;
+            newRecord.updated_at = functionInvocation.functionInvocationTimestamp;
+
+            // replacing a record implicitly undeletes it
+            newRecord.deleted_at = '';
+            newRecord.deleted = false;
+        
+            event = craftStaffCreateEvent(oldRecord, newRecord);
+
+        } else {
+            newRecord.created_at = oldRecord.created_at;
+            newRecord.updated_at = functionInvocation.functionInvocationTimestamp;
+
+            // replacing a record implicitly undeletes it
+            newRecord.deleted_at = '';
+            newRecord.deleted = false;
+        
+            event = craftStaffUpdateEvent(oldRecord, newRecord);
+        }
+
+        return {event: event, newRecord: newRecord};
+    }
+
+    function craftStaffCreateEvent(old_record, new_record) {
+        let event_type = 'Skinner.Staff.Create';
+        let source = 'create';
+        let schema = 'create';
+        let label = `Staff ${new_record.id} created.`;
+        let payload = {
+            record: new_record
+        };
+
+        let event = craftEvent(new_record.id, source, schema, event_type, label, payload);
+        return event;
+    }
+    
+    function craftStaffUpdateEvent(old_record, new_record) {
+        let event_type = 'Skinner.Staff.Update';
+        let source = 'update';
+        let schema = 'update';
+        let label = `Staff ${new_record.id} updated.`;
+        let payload = {
+            old_record: old_record,
+            new_record: new_record,
+        };
+
+        let event = craftEvent(new_record.id, source, schema, event_type, label, payload);
+        return event;
+    }
+
+    function craftStaffDeleteEvent(old_record, new_record) {
+        let event_type = 'Skinner.Staff.Delete';
+        let source = 'delete';
+        let schema = 'delete';
+        let label = `Staff ${new_record.id} deleted.`;
+        let payload = {
+            record: old_record
+        };
+
+        let event = craftEvent(old_record.id, source, schema, event_type, label, payload);
+        return event;
+    }
+
+    function craftEvent(recordID, source, schema, event_type, label, payload) {
+        let event = {
+            id: `${event_type}-${context.executionContext.invocationId}`,
+            time: functionInvocation.functionInvocationTimestamp,
+
+            type: event_type,
+            source: `/skinner/staff/${recordID}/${source}`,
+            schemaURL: `ca.wrdsb.skinner.staff.${schema}.json`,
+
+            label: label,
+            tags: [
+                "skinner", 
+                "staff_change"
+            ], 
+
+            data: {
+                function_name: context.executionContext.functionName,
+                invocation_id: context.executionContext.invocationId,
+                result: {
+                    payload: payload 
+                },
+            },
+
+            eventTypeVersion: "0.1",
+            specversion: "0.2",
+            contentType: "application/json"
+        };
+
+        // TODO: check message length
+        return event;
+    }
+};
+
+export default trilliumStaffStore;
