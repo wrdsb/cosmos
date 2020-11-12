1aa503ef1931bd7e27454138b21bed5b228453c6
James Schumann <james@schumann.ca>
2020-11-12 13:07:51 -0500

    create ats-asset-class-types-reconcile

---
+++ apps/quartermaster-functions/src/ats-asset-class-types-reconcile/index.ts
@@ -0,0 +1,266 @@
+import { AzureFunction, Context } from "@azure/functions"
+import { CosmosClient } from "@azure/cosmos";
+import { createHash } from "crypto";
+import { FunctionInvocation, ATSAssetClassTypesReconcileFunctionRequest, ATSAssetClassTypesReconcileFunctionRequestPayload, ATSAssetClassType } from "@cosmos/types";
+
+const atsAssetClassTypesReconcile: AzureFunction = async function (context: Context, triggerMessage: ATSAssetClassTypesReconcileFunctionRequest): Promise<void> {
+    const functionInvocation = {
+        functionInvocationID: context.executionContext.invocationId,
+        functionInvocationTimestamp: new Date().toJSON(),
+        functionApp: 'Quartermaster',
+        functionName: context.executionContext.functionName,
+        functionDataType: 'ATSAssetClassType',
+        functionDataOperation: 'Reconcile',
+        eventLabel: ''
+    } as FunctionInvocation;
+
+    const cosmosEndpoint = process.env['cosmosEndpoint'];
+    const cosmosKey = process.env['cosmosKey'];
+    const cosmosDatabase = process.env['cosmosDatabase'];
+    const cosmosContainer = 'ats-asset-class-types';
+    const cosmosClient = new CosmosClient({endpoint: cosmosEndpoint, key: cosmosKey});
+
+    const triggerObject = triggerMessage as ATSAssetClassTypesReconcileFunctionRequest;
+    const payload = triggerObject.payload as ATSAssetClassTypesReconcileFunctionRequestPayload;
+
+    // give our bindings more human-readable names
+    const recordsNow = context.bindings.recordsNow;
+
+    // fetch current records from Cosmos
+    const recordsPrevious = await getCosmosItems(cosmosClient, cosmosDatabase, cosmosContainer).catch(err => {
+        context.log(err);
+    });
+
+    context.log('Reconcile ATSAssetClassTypes: ' + Object.getOwnPropertyNames(recordsPrevious).length);
+
+    // object to store our total diff as we build it
+    let calculation = {
+        records_previous: recordsPrevious,
+        records_now: recordsNow,
+        differences: {
+            created_records: [],
+            updated_records: [],
+            deleted_records: []
+        }
+    };
+
+    calculation = await findCreatesAndUpdates(calculation);
+    calculation = await findDeletes(calculation);
+
+    let creates = await processCreates(calculation.differences.created_records);
+    let updates = await processUpdates(calculation.differences.updated_records);
+    let deletes = await processDeletes(calculation.differences.deleted_records);
+
+    let totalDifferences = creates.length + updates.length + deletes.length;
+
+    context.bindings.queueStore = creates.concat(updates, deletes);
+
+    const logPayload = {
+        totalDifferences: totalDifferences,
+        differences: calculation.differences
+    };
+    functionInvocation.logPayload = logPayload;
+    context.log(logPayload);
+
+    context.log(functionInvocation);
+    context.done(null, functionInvocation);
+
+
+    async function findCreatesAndUpdates(calculation) {
+        context.log('findCreatesAndUpdates');
+
+        let records_previous = calculation.records_previous;
+        let records_now = calculation.records_now;
+
+        if (!records_now) {
+            return calculation;
+        }
+
+        // loop through all records in records_now, looking for updates and creates
+        Object.getOwnPropertyNames(records_now).forEach(function (record_id) {
+            const new_record = {
+                id:                   records_now[record_id].id,
+                changeDetectionHash:  records_now[record_id].changeDetectionHash,
+                asset_class_code:     records_now[record_id].asset_class_code,
+                asset_type_code:      records_now[record_id].asset_type_code,
+                engraved_id_prefix:   records_now[record_id].engraved_id_prefix
+        
+                // these fields are not present in the data from ats, so we don't map them
+                //created_at
+                //updated_at
+                //deleted_at
+                //deleted
+            };
+    
+            if (!records_previous || !records_previous[record_id]) {
+                calculation.differences.created_records.push(new_record);
+            } else {
+                // get the corresponding record in records_previous
+                const old_record = {
+                    id:                   records_previous[record_id].id,
+                    changeDetectionHash:  records_previous[record_id].changeDetectionHash,
+                    asset_class_code:     records_previous[record_id].asset_class_code,
+                    asset_type_code:      records_previous[record_id].asset_type_code,
+                    engraved_id_prefix:   records_previous[record_id].engraved_id_prefix
+        
+                    // these fields are not present in the data from ats, so we don't map them
+                    //created_at
+                    //updated_at
+                    //deleted_at
+                    //deleted
+                }; 
+
+                // Re-calculate the change detection hashes locally,
+                // because different functions may have different change detection standards
+                const newRecordChangeDetectionHash = makeHash(new_record);
+                const oldRecordChangeDetectionHash = makeHash(old_record);
+
+                // Compare old and new records
+                const records_equal = (newRecordChangeDetectionHash === oldRecordChangeDetectionHash) ? true : false;
+   
+                // if record changed, record the change
+                if (!records_equal) {
+                    calculation.differences.updated_records.push({
+                        previous: old_record,
+                        now: new_record
+                    });
+                }
+            }
+        });
+        return calculation;
+    }
+
+    async function findDeletes(calculation) {
+        context.log('findDeletes');
+
+        let records_previous = calculation.records_previous;
+        let records_now = calculation.records_now;
+
+        if (!records_previous) {
+            return calculation;
+        }
+
+        // loop through all records in records_previous, looking for deletes
+        Object.getOwnPropertyNames(records_previous).forEach(function (record_id) {
+            if (!records_now || !records_now[record_id]) {
+                calculation.differences.deleted_records.push(records_previous[record_id]);
+            }
+        });
+
+        return calculation;
+    }
+
+    async function processCreates(created_records) {
+        context.log('processCreates');
+
+        // array for the results being returned
+        let messages = [];
+
+        created_records.forEach(function (record) {
+            let message = {
+                operation: "replace",
+                payload: record
+            };
+            messages.push(JSON.stringify(message));
+        });
+
+        return messages;
+    }
+
+    async function processUpdates(updated_records) {
+        context.log('processUpdates');
+
+        // array for the results being returned
+        let messages = [];
+
+        updated_records.forEach(function (record) {
+            let message = {
+                operation: "replace",
+                payload: record.now
+            };
+            messages.push(JSON.stringify(message));
+        });
+
+        return messages;
+    }
+
+    async function processDeletes(deleted_records) {
+        context.log('processDeletes');
+
+        // array for the results being returned
+        let messages = [];
+
+        deleted_records.forEach(function (record) {
+            let message = {
+                operation: "delete",
+                payload: record
+            };
+            messages.push(JSON.stringify(message));
+        });
+
+        return messages;
+    }
+
+    async function getCosmosItems(cosmosClient, cosmosDatabase, cosmosContainer) {
+        context.log('getCosmosItems');
+
+        let records_previous = {};
+
+        const querySpec = {
+            query: `SELECT * FROM c WHERE c.deleted = false`
+        }
+
+        const queryOptions  = {
+            maxItemCount: -1,
+            enableCrossPartitionQuery: true
+        }
+
+        try {
+            const { resources } = await cosmosClient.database(cosmosDatabase).container(cosmosContainer).items.query(querySpec).fetchAll();
+
+            for (const item of resources) {
+                if (!item.deleted) {
+                    let dbObject = {
+                        id: item.id,
+                        changeDetectionHash: item.changeDetectionHash,
+                
+                        asset_class_code: item.asset_class_code,
+                        asset_type_code: item.asset_type_code,
+                        engraved_id_prefix: item.engraved_id_prefix
+                
+                        // these fields are not present in the data from ats
+                        //created_at: item.created_at,
+                        //updated_at: item.updated_at,
+                        //deleted_at: item.deleted_at,
+                        //deleted: item.deleted
+                    } as ATSAssetClassType;
+        
+                    records_previous[item.id] = dbObject;
+                }
+            }
+    
+            return records_previous;
+        } catch (error) {
+            context.log(error);
+    
+            context.res = {
+                status: 500,
+                body: error
+            };
+    
+            context.done(error);
+        }
+    }
+
+    function makeHash(atsAssetClassType: ATSAssetClassType): string {
+        const objectForHash = JSON.stringify({
+            asset_class_code: atsAssetClassType.asset_class_code,
+            asset_type_code: atsAssetClassType.asset_type_code,
+            engraved_id_prefix: atsAssetClassType.engraved_id_prefix
+        });
+        const objectHash = createHash('md5').update(objectForHash).digest('hex');
+        return objectHash;
+    }
+};
+
+export default atsAssetClassesReconcile;
