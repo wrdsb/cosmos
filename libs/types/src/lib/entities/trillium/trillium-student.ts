556732ccecaf512953c978e08a003e4467bc63f3
James Schumann <james@schumann.ca>
2020-11-16 11:52:32 -0500

    add fields for student_grade and student_shsm_sector

--- libs/types/src/lib/entities/trillium/trillium-student.ts
+++ libs/types/src/lib/entities/trillium/trillium-student.ts
@@ -1,11 +1,13 @@
 interface TrilliumStudent {
   id?: string;
   student_number: string;
+  student_grade: string;
   student_email: string;
   student_first_name: string;
   student_last_name: string;
   school_code: string;
   student_oyap: string;
+  student_shsm_sector: string;
   created_at?: string;
   updated_at?: string;
   deleted_at?: string;
