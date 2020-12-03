e5ec1338ad41e8f37b1ceaa2d1f4ef9792d90fb8
James Schumann <james@schumann.ca>
2020-12-03 14:10:17 -0500

    add flenderson ping. closes #290

---
+++ apps/flenderson-functions/src/ping/index.ts
@@ -0,0 +1,83 @@
+import { AzureFunction, Context, HttpRequest } from "@azure/functions";
+import jwt_decode from 'jwt-decode';
+import { FunctionInvocation } from "@cosmos/types";
+
+const ping: AzureFunction = async function (context: Context, req: HttpRequest): Promise<void> {
+    const functionInvocation = {
+        functionInvocationID: context.executionContext.invocationId,
+        functionInvocationTimestamp: new Date().toJSON(),
+        functionApp: 'Flenderson',
+        functionName: context.executionContext.functionName,
+        functionDataType: 'Ping',
+        functionDataOperation: 'Ping',
+        eventLabel: ''
+    } as FunctionInvocation;
+
+    const request = req;
+    let authenticated = false;
+    let authorized = false;
+    let idToken = '';
+    let userName = '';
+    let userEmail = '';
+    let userRoles = [];
+
+    if (request.headers['x-ms-token-aad-id-token']) {
+        authenticated = true;
+        idToken = request.headers['x-ms-token-aad-id-token'];
+        let decodedToken = jwt_decode(idToken);
+        userName = decodedToken.name,
+        userEmail = decodedToken.unique_name;
+        userRoles = decodedToken.roles as string[];
+        authorized = userRoles.includes('cosmos-user-its') ? true : false;
+    }
+
+    let response = {
+        header: {
+            status: 200,
+            message: "",
+            chatter: "",
+            timestamp: functionInvocation.functionInvocationTimestamp,
+            authenticated: authenticated,
+            authorized: authorized,
+            userName: userName,
+            userEmail: userEmail,
+            userRoles: userRoles
+        },
+        payload: []
+    };
+
+    if (!authenticated) {
+        response.header.status = 401;
+        response.header.message = "Unauthorized: Cannot verify your identity.";
+        response.header.chatter = "Unauthorized: Cannot verify your identity.";
+    }
+    else if (authenticated && !authorized) {
+        response.header.status = 403;
+        response.header.message = "Forbidden: You are not permitted to ping Flenderson";
+        response.header.chatter = "Forbidden: You are not permitted to ping Flenderson";
+    }
+    else if (authenticated && authorized) {
+        response.header.status = 200;
+        response.header.message = "Flenderson is up";
+        response.header.chatter = "Flenderson here. What can I do for you?";
+    }
+    else {
+        response.header.status = 400;
+        response.header.message = "Bad Request: We're not sure what happend, but we're pretty sure it's you, not us.";
+        response.header.chatter = "Bad Request: We're not sure what happend, but we're pretty sure it's you, not us.";
+    }
+
+    context.res = {
+        status: response.header.status,
+        body: response
+    }
+
+    const logPayload = response;
+    functionInvocation.logPayload = logPayload;
+
+    context.bindings.invocationPostProcessor = functionInvocation;
+    context.log(functionInvocation);
+    context.done(null, functionInvocation);
+};
+
+export default ping;
