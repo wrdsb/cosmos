c19c8bed8f8252e1e42b80e2006dbc5654cb8487
James Schumann <james@schumann.ca>
2021-01-13 10:19:00 -0500

    device-loan-materialize-all is working

---
+++ apps/quartermaster-functions/src/device-loan-materialize-all/index.ts
@@ -0,0 +1,94 @@
+import { AzureFunction, Context } from "@azure/functions";
+import { CosmosClient } from "@azure/cosmos";
+import { FunctionInvocation, DeviceLoanMaterializeFunctionRequest } from "@cosmos/types";
+
+const deviceLoanMaterializeAll: AzureFunction = async function (context: Context, triggerMessage: any): Promise<void> {
+    const functionInvocation = {
+        functionInvocationID: context.executionContext.invocationId,
+        functionInvocationTimestamp: new Date().toJSON(),
+        functionApp: 'Quartermaster',
+        functionName: context.executionContext.functionName,
+        functionDataType: 'DeviceLoan',
+        functionDataOperation: 'MaterializeAll',
+        eventLabel: ''
+    } as FunctionInvocation;
+
+    const cosmosEndpoint = process.env['cosmosEndpoint'];
+    const cosmosKey = process.env['cosmosKey'];
+    const cosmosDatabase = process.env['cosmosDatabase'];
+    const cosmosContainerLoans = 'device-loan-submissions';
+    const cosmosContainerReturns = 'device-return-submissions';
+    const cosmosClient = new CosmosClient({endpoint: cosmosEndpoint, key: cosmosKey});
+
+    const triggerObject = triggerMessage;
+
+    context.log('Queue all Materialize Device Loans');
+
+    let queueMessages = [];
+
+    // fetch current records from Cosmos
+    let loansAssetIDs = await getCosmosItems(cosmosClient, cosmosDatabase, cosmosContainerLoans);
+
+    // fetch current records from Cosmos
+    let returnsAssetIDs = await getCosmosItems(cosmosClient, cosmosDatabase, cosmosContainerReturns);
+
+    let allAssetIDs = new Set(loansAssetIDs)
+
+    for (let assetID of returnsAssetIDs) {
+        allAssetIDs.add(assetID)
+    }
+
+    for (let assetID of allAssetIDs) {
+        queueMessages.push({
+            payload: {
+                assetID: assetID
+            }
+        } as DeviceLoanMaterializeFunctionRequest);
+    }
+
+    context.bindings.deviceLoanMaterialize = queueMessages;
+
+    const logPayload = {count: queueMessages.length};
+    functionInvocation.logPayload = logPayload;
+
+    context.bindings.invocationPostProcessor = functionInvocation;
+    context.log(functionInvocation);
+    context.done(null, functionInvocation);
+
+
+    async function getCosmosItems(cosmosClient, cosmosDatabase, cosmosContainer) {
+        context.log(`get ${cosmosContainer} records`);
+
+        let assetIDs = new Set();
+
+        const querySpec = {
+            query: `SELECT c.correctedAssetID FROM c GROUP BY c.correctedAssetID`
+        }
+
+        const queryOptions  = {
+            maxItemCount: -1,
+            enableCrossPartitionQuery: true
+        }
+
+        try {
+            const { resources } = await cosmosClient.database(cosmosDatabase).container(cosmosContainer).items.query(querySpec).fetchAll();
+
+            for (const item of resources) {
+                assetIDs.add(item.correctedAssetID);
+            }
+    
+            return assetIDs;
+        } catch (error) {
+            context.log(`caught cosmos error: ${error}`);
+    
+            context.res = {
+                status: 500,
+                body: error
+            };
+    
+            context.done(error);
+        }
+    }
+};
+
+export default deviceLoanMaterializeAll;
