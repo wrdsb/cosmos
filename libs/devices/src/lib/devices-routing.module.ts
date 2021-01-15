c2e639f8b63af6b5c7f5745e6574c6a873274b57
James Schumann <james@schumann.ca>
2021-01-15 13:49:50 -0500

    add routing for devices dashboard and devices search

--- libs/devices/src/lib/devices-routing.module.ts
+++ libs/devices/src/lib/devices-routing.module.ts
@@ -3,12 +3,34 @@ import { Routes, RouterModule } from '@angular/router';
 import { MsalGuard } from '@azure/msal-angular';
 import { RolesGuard } from "@cosmos/guards";
 
-import { HomeComponent } from "./home/home.component";
+import { DevicesHomeComponent } from "./home/devices-home.component";
+import { DeviceLoansDashboardComponent } from "./device-loans-dashboard/device-loans-dashboard.component";
+import { DeviceLoansSearchComponent } from "./device-loans-search/device-loans-search.component";
 
 const routes: Routes = [
   { 
+    path: 'dashboard',
+    component: DeviceLoansDashboardComponent,
+    data: {
+      roles: ['cosmos-superuser', 'cosmos-user-its']
+    },
+    canActivate: [
+      MsalGuard, RolesGuard
+    ]
+  },
+  { 
+    path: 'search',
+    component: DeviceLoansSearchComponent,
+    data: {
+      roles: ['cosmos-superuser', 'cosmos-user-its']
+    },
+    canActivate: [
+      MsalGuard, RolesGuard
+    ]
+  },
+  { 
     path: '',
-    component: HomeComponent,
+    component: DevicesHomeComponent,
     data: {
       roles: ['cosmos-superuser', 'cosmos-user-its']
     },
