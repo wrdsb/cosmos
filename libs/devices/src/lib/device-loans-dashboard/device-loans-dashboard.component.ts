b51aa751024c852cf710505a08552413fdb8d74d
James Schumann <james@schumann.ca>
2021-01-18 10:14:35 -0500

    stub device loans dashboard

--- libs/devices/src/lib/device-loans-dashboard/device-loans-dashboard.component.ts
+++ libs/devices/src/lib/device-loans-dashboard/device-loans-dashboard.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable, BehaviorSubject } from 'rxjs';
+
+import { DeviceLoan, SearchFunctionRequestPayload, SearchFunctionResponse } from "@cosmos/types";
+import { DeviceLoansService } from '../device-loans.service';
 
 @Component({
   selector: 'cosmos-device-loans-dashboard',
@@ -6,10 +10,51 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./device-loans-dashboard.component.scss']
 })
 export class DeviceLoansDashboardComponent implements OnInit {
+  displayedColumns$ = new BehaviorSubject<string[]>([
+    'school',
+    'ipads',
+    'chromebooks',
+    'otherDevices',
+    'total',
+    'recordActions'
+  ]);
 
-  constructor() { }
+  loansPerSchool = new BehaviorSubject<any[]>([]);
+  public loansPerSchool$: Observable<any[]>;
 
-  ngOnInit(): void {
+  constructor(
+    private deviceLoansService: DeviceLoansService,
+    //public dialog: MatDialog
+  ) {
+    this.loansPerSchool$ = this.loansPerSchool;
   }
 
+  ngOnInit(): void {
+    this.searchLoans();
+  }
+
+  searchLoans(): void {
+    this.loansPerSchool.next(
+      [
+        {
+          school: "BCI",
+          ipads: 3,
+          chromebooks: 6,
+          total: 9
+        },
+        {
+          school: "CHC",
+          ipads: 3,
+          chromebooks: 6,
+          total: 9
+        },
+        {
+          school: "ECI",
+          ipads: 3,
+          chromebooks: 6,
+          total: 9
+        }
+      ]
+    );
+  }
 }
