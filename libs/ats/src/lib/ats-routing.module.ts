d1553f955f49d90df16b061e544b6a4759c90096
James Schumann <james@schumann.ca>
2021-01-19 13:30:36 -0500

    update ats routing module

---
+++ libs/ats/src/lib/ats-routing.module.ts
@@ -0,0 +1,37 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { MsalGuard } from '@azure/msal-angular';
+import { RolesGuard } from "@cosmos/guards";
+
+import { ATSHomeComponent } from "./home/home.component";
+import { AssetsSearchComponent } from "./assets-search/assets-search.component";
+
+const routes: Routes = [
+  { 
+    path: 'assets/search',
+    component: AssetsSearchComponent,
+    data: {
+      roles: ['cosmos-superuser', 'cosmos-user-its']
+    },
+    canActivate: [
+      MsalGuard, RolesGuard
+    ]
+  },
+  { 
+    path: '',
+    component: ATSHomeComponent,
+    data: {
+      roles: ['cosmos-superuser', 'cosmos-user-its']
+    },
+    canActivate: [
+      MsalGuard, RolesGuard
+    ]
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule]
+})
+
+export class DevicesRoutingModule { }
