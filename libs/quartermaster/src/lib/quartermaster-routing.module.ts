2a4c13fc076e12f69379297520444a99f064303b
James Schumann <james@schumann.ca>
2021-01-19 13:30:56 -0500

    add quartermaster routing module

---
+++ libs/quartermaster/src/lib/quartermaster-routing.module.ts
@@ -0,0 +1,37 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { MsalGuard } from '@azure/msal-angular';
+import { RolesGuard } from "@cosmos/guards";
+
+import { QuartermasterHomeComponent } from "./home/home.component";
+import { AssetsSearchComponent } from './assets-search/assets-search.component';
+
+const routes: Routes = [
+  { 
+    path: 'assets/search',
+    component: AssetsSearchComponent,
+    data: {
+      roles: ['cosmos-superuser', 'cosmos-user-its']
+    },
+    canActivate: [
+      MsalGuard, RolesGuard
+    ]
+  },
+  { 
+    path: '',
+    component: QuartermasterHomeComponent,
+    data: {
+      roles: ['cosmos-superuser', 'cosmos-user-its']
+    },
+    canActivate: [
+      MsalGuard, RolesGuard
+    ]
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule]
+})
+
+export class DevicesRoutingModule { }
