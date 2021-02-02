88fdc561a0e525504fbce2b3ca93e7e5ad71ab47
James Schumann <james@schumann.ca>
2021-02-02 15:36:50 -0500

    finish conversion to new build system

--- apps/igor-functions/webpack.config.js
+++ apps/igor-functions/webpack.config.js
@@ -55,7 +55,9 @@ module.exports = (config, context) => {
         resolve: {
             extensions: ['.tsx', '.ts', '.js'],
             plugins: [
-                new TsconfigPathsPlugin()
+                new TsconfigPathsPlugin({
+                    configFile: 'apps/igor-functions/tsconfig.app.json'
+                })
             ]
         },
         /**
