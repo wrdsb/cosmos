b3ddc348bb21d42f821f6418f98a193310624a44
James Schumann <james@schumann.ca>
2021-02-16 13:44:32 -0500

    remove AD auth from query function. will use keys instead.

--- apps/quartermaster-functions/src/device-loan-submissions-query/index.ts
+++ apps/quartermaster-functions/src/device-loan-submissions-query/index.ts
@@ -24,28 +24,6 @@ const deviceLoanSubmissionsQuery: AzureFunction = async function (context: Conte
     const userQuery = request.query.query;
     const queryScope = request.query.scope;
 
-    interface MSALToken {
-        name: string;
-        unique_name: string;
-        roles: string[];
-    };
-    let authenticated = false;
-    let authorized = false;
-    let idToken = '';
-    let userName = '';
-    let userEmail = '';
-    let userRoles = [];
-
-    if (request.headers['x-ms-token-aad-id-token']) {
-        authenticated = true;
-        idToken = request.headers['x-ms-token-aad-id-token'];
-        let decodedToken = jwt_decode(idToken) as MSALToken;
-        userName = decodedToken.name;
-        userEmail = decodedToken.unique_name;
-        userRoles = decodedToken.roles as string[];
-        authorized = userRoles.includes('cosmos-user-its') ? true : false;
-    }
-
     switch (userQuery) {
         // Total form submissions
         case 'total-device-loans':
@@ -72,40 +50,13 @@ const deviceLoanSubmissionsQuery: AzureFunction = async function (context: Conte
             status: 200,
             message: "",
             chatter: "",
-            timestamp: functionInvocation.functionInvocationTimestamp,
-            authenticated: authenticated,
-            authorized: authorized,
-            userName: userName,
-            userEmail: userEmail,
-            userRoles: userRoles
+            timestamp: functionInvocation.functionInvocationTimestamp
         },
+        status: 200,
+        query: userQuery,
+        scope: queryScope,
         payload: {}
     };
-
-    if (!authenticated) {
-        response.header.status = 401;
-        response.header.message = "Unauthorized: Cannot verify your identity.";
-        response.header.chatter = "Unauthorized: Cannot verify your identity.";
-    }
-    else if (authenticated && !authorized) {
-        response.header.status = 403;
-        response.header.message = "Forbidden: You are not permitted to query Viewfinder";
-        response.header.chatter = "Forbidden: You are not permitted to query Viewfinder";
-    }
-    else if (authenticated && authorized) {
-        response.header.status = 200;
-        response.header.message = "";
-        response.header.chatter = "";
-        response.payload = {
-            //count: searchResults.count,
-            //documents: documents
-        }
-    }
-    else {
-        response.header.status = 400;
-        response.header.message = "Bad Request: We're not sure what happend, but we're pretty sure it's you, not us.";
-        response.header.chatter = "Bad Request: We're not sure what happend, but we're pretty sure it's you, not us.";
-    }
 
     context.res = {
         status: response.header.status,
