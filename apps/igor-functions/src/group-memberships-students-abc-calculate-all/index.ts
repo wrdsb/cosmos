fa8adb35e0e4679192aa4136b091f4f57a031555
James Schumann <james@schumann.ca>
2021-02-16 13:47:44 -0500

    now students-abc-calculate-all not students-calculate-all

--- apps/igor-functions/src/group-memberships-students-abc-calculate-all/index.ts
+++ apps/igor-functions/src/group-memberships-students-abc-calculate-all/index.ts
@@ -3,35 +3,35 @@ import { createLogObject } from "@cosmos/azure-functions-shared";
 import { storeLogBlob } from "@cosmos/azure-functions-shared";
 import { createCallbackMessage } from "@cosmos/azure-functions-shared";
 import { createEvent } from "@cosmos/azure-functions-shared";
-import { GroupMembershipsStudentsCalculateAllFunctionRequest, GroupMembershipsStudentsCalculateAllFunctionRequestPayload } from "@cosmos/types";
+import { GoogleGroupsMembershipsStudentsABCCalculateAllFunctionRequest, GoogleGroupsMembershipsStudentsABCCalculateAllFunctionRequestPayload } from "@cosmos/types";
 
-const GroupMembershipsStudentsCalculateAll: AzureFunction = async function (context: Context, triggerMessage: GroupMembershipsStudentsCalculateAllFunctionRequest): Promise<void> {
+const GroupMembershipsStudentsABCCalculateAll: AzureFunction = async function (context: Context, triggerMessage: GoogleGroupsMembershipsStudentsABCCalculateAllFunctionRequest): Promise<void> {
     const functionInvocationID = context.executionContext.invocationId;
     const functionInvocationTime = new Date();
     const functionInvocationTimestamp = functionInvocationTime.toJSON();  // format: 2012-04-23T18:25:43.511Z
 
     const functionName = context.executionContext.functionName;
-    const functionEventType = 'WRDSB.IGOR.Google.Group.Memberships.Students.Calculate.All';
+    const functionEventType = 'WRDSB.IGOR.Google.Group.Memberships.StudentsABC.Calculate.All';
     const functionEventID = `igor-functions-${functionName}-${functionInvocationID}`;
     const functionLogID = `${functionInvocationTime.getTime()}-${functionInvocationID}`;
 
     const logStorageAccount = process.env['storageAccount'];
     const logStorageKey = process.env['storageKey'];
-    const logStorageContainer = 'function-group-memberships-students-calculate-all-logs';
+    const logStorageContainer = 'function-group-memberships-students-abc-calculate-all-logs';
 
     const eventLabel = '';
     const eventTags = [
         "igor", 
     ];
 
-    const triggerObject = triggerMessage as GroupMembershipsStudentsCalculateAllFunctionRequest;
-    const payload = triggerObject.payload as GroupMembershipsStudentsCalculateAllFunctionRequestPayload;
+    const triggerObject = triggerMessage as GoogleGroupsMembershipsStudentsABCCalculateAllFunctionRequest;
+    const payload = triggerObject.payload as GoogleGroupsMembershipsStudentsABCCalculateAllFunctionRequestPayload;
 
     context.log(payload);
 
     const rows = context.bindings.studentsNow;
     
-    let queueMessages = await prepareMessages(rows);
+    const queueMessages = await prepareMessages(rows);
 
     context.bindings.outputQueue = queueMessages;
 
@@ -39,14 +39,6 @@ const GroupMembershipsStudentsCalculateAll: AzureFunction = async function (cont
         queueMessages: queueMessages
     };
     context.log(logPayload);
-
-    const logObject = await createLogObject(functionInvocationID, functionInvocationTime, functionName, logPayload);
-    const logBlob = await storeLogBlob(logStorageAccount, logStorageKey, logStorageContainer, logObject);
-    context.log(logBlob);
-
-    const callbackMessage = await createCallbackMessage(logObject, 200);
-    context.bindings.callbackMessage = JSON.stringify(callbackMessage);
-    context.log(callbackMessage);
 
     const invocationEvent = await createEvent(
         functionInvocationID,
@@ -61,18 +53,16 @@ const GroupMembershipsStudentsCalculateAll: AzureFunction = async function (cont
         eventLabel,
         eventTags
     );
-    context.bindings.flynnEvent = JSON.stringify(invocationEvent);
     context.log(invocationEvent);
 
-    context.done(null, logBlob);
+    context.done(null, invocationEvent);
 
-    
     async function prepareMessages(rows) {
-        let messages = [];
-        let schoolCodes = new Set();
+        const messages = [];
+        const schoolCodes = new Set();
 
         rows.forEach(function(row) {
-            let schoolCode = (row.school_code) ? row.school_code.toLowerCase() : 0;
+            const schoolCode = (row.school_code) ? row.school_code.toLowerCase() : 0;
 
             if (isNaN(schoolCode)) {
                 schoolCodes.add(schoolCode);
@@ -80,7 +70,7 @@ const GroupMembershipsStudentsCalculateAll: AzureFunction = async function (cont
         });
 
         schoolCodes.forEach(schoolCode => {
-            let message = {
+            const message = {
                 payload: {
                     schoolCode: schoolCode
                 }
@@ -89,14 +79,14 @@ const GroupMembershipsStudentsCalculateAll: AzureFunction = async function (cont
             messages.push(message);
         });
 
-        let oyapMessage = {
+        const oyapMessage = {
             payload: {
                 schoolCode: 'oyap'
             }
         };
         messages.push(oyapMessage);
 
-        let shsmMessage = {
+        const shsmMessage = {
             payload: {
                 schoolCode: 'shsm'
             }
@@ -107,4 +97,4 @@ const GroupMembershipsStudentsCalculateAll: AzureFunction = async function (cont
     }
 }
 
-export default GroupMembershipsStudentsCalculateAll;
+export default GroupMembershipsStudentsABCCalculateAll;
