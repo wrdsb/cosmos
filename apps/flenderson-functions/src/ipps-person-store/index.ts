ee271ea0dc70a9f5e843d8c974667dcb787de7c5
James Schumann <james@schumann.ca>
2021-02-17 15:19:29 -0500

    create ipps-person-store based on quartermaster code

---
+++ apps/flenderson-functions/src/ipps-person-store/index.ts
@@ -0,0 +1,298 @@
+import { AzureFunction, Context } from "@azure/functions";
+import { createHash } from "crypto";
+import { FunctionInvocation, IPPSPersonStoreFunctionRequest, IPPSPersonStoreFunctionRequestOperation, IPPSPersonStoreFunctionRequestPayload, IPPSPerson } from "@cosmos/types";
+
+const ippsPersonStore: AzureFunction = async function (context: Context, triggerMessage: IPPSPersonStoreFunctionRequest): Promise<void> {
+    const functionInvocation = {
+        functionInvocationID: context.executionContext.invocationId,
+        functionInvocationTimestamp: new Date().toJSON(),
+        functionApp: 'Flenderson',
+        functionName: context.executionContext.functionName,
+        functionDataType: 'IPPSPerson',
+        functionDataOperation: 'Store',
+        eventLabel: ''
+    } as FunctionInvocation;
+
+    const triggerObject = triggerMessage as IPPSPersonStoreFunctionRequest;
+    const operation = triggerObject.operation as IPPSPersonStoreFunctionRequestOperation;
+    const payload = triggerObject.payload as IPPSPersonStoreFunctionRequestPayload;
+
+    const oldRecord = context.bindings.recordIn;
+
+    const newRecord = {
+        createdAt: '',
+        updatedAt: '',
+        deletedAt: '',
+        deleted: false,
+
+        id: '',
+        email: '',
+        username: '',
+
+        firstName: '',
+        lastName: '',
+        fullName: '',
+        sortableName: '',
+
+        ein: '',
+
+        locationCodes: [],
+        schoolCodes: [],
+        jobCodes: [],
+        homeLocation: '',
+
+        directory: '',
+        phone: '',
+        extension: '',
+        mbxnumber: '',
+
+        assignments: []
+    } as IPPSPerson;
+
+    let result;
+    let statusCode;
+    let statusMessage;
+
+    switch (operation) {
+        case 'delete':
+            result = doDelete(oldRecord, newRecord, payload);
+            statusCode = '200';
+            statusMessage = 'Success: Marked record deleted.';
+            break;
+        case 'patch':
+            result = doPatch(oldRecord, newRecord, payload);
+            statusCode = '200';
+            statusMessage = 'Success: Patched record.';
+            break;
+        case 'replace':
+            result = doReplace(oldRecord, newRecord, payload);
+            statusCode = '200';
+            statusMessage = 'Success: Replaced record.';
+            break;
+        default:
+            break;
+    }
+
+    if (result.changedDetected) {
+        context.bindings.recordOut = result.newRecord;
+
+        const logPayload = result.event;
+        functionInvocation.logPayload = logPayload;
+        context.log(logPayload);
+    } else {
+        const logPayload = {};
+        functionInvocation.logPayload = logPayload;
+        context.log('No change detected.');
+    }
+    
+    context.log(functionInvocation);
+    context.done(null, functionInvocation);
+
+
+    function doDelete(oldRecord, newRecord, payload) {
+        let event = {};
+        const changedDetected = true;
+
+        // check for existing record
+        if (!oldRecord) {
+            newRecord = Object.assign(newRecord, payload);
+            newRecord.created_at = functionInvocation.functionInvocationTimestamp;
+            newRecord.updated_at = functionInvocation.functionInvocationTimestamp;
+
+            // mark the record as deleted
+            newRecord.deleted_at = functionInvocation.functionInvocationTimestamp;
+            newRecord.deleted = true;
+            
+            newRecord.changeDetectionHash = makeHash(newRecord);
+
+            event = craftDeleteEvent(oldRecord);
+
+        } else {
+            newRecord = Object.assign(newRecord, oldRecord);
+
+            // mark the record as deleted
+            newRecord.deleted_at = functionInvocation.functionInvocationTimestamp;
+            newRecord.deleted = true;
+
+            newRecord.changeDetectionHash = makeHash(newRecord);
+
+            event = craftDeleteEvent(oldRecord);
+        }
+
+        return {changedDetected: changedDetected, event: event, newRecord: newRecord};
+    }
+
+    function doPatch(oldRecord, newRecord, payload) {
+        let event = {};
+        let changedDetected = false;
+
+        if (!oldRecord) {
+            newRecord = Object.assign(newRecord, payload);
+            newRecord.created_at = functionInvocation.functionInvocationTimestamp;
+            newRecord.updated_at = functionInvocation.functionInvocationTimestamp;
+    
+            // patching a record implicitly undeletes it
+            newRecord.deleted_at = '';
+            newRecord.deleted = false;
+    
+            newRecord.changeDetectionHash = makeHash(newRecord);
+
+            changedDetected = true;
+            event = craftCreateEvent(newRecord);
+
+        } else {
+            // Merge request object into current record
+            newRecord = Object.assign(newRecord, oldRecord, payload);
+            newRecord.updated_at = functionInvocation.functionInvocationTimestamp;
+    
+            // patching a record implicitly undeletes it
+            newRecord.deleted_at = '';
+            newRecord.deleted = false;
+    
+            newRecord.changeDetectionHash = makeHash(newRecord);
+
+            changedDetected = (oldRecord.changeDetectionHash === newRecord.changeDetectionHash) ? false : true;
+
+            if (changedDetected) {
+                event = craftUpdateEvent(oldRecord, newRecord);
+            } else {
+                newRecord = oldRecord;
+                event = false;
+            }
+        }
+
+        return {changedDetected: changedDetected, event: event, newRecord: newRecord};
+    }
+    
+    function doReplace(oldRecord, newRecord, payload) {
+        let event = {};
+        let changedDetected = false;
+
+        if (!oldRecord) {
+            newRecord = Object.assign(newRecord, payload);
+            newRecord.created_at = functionInvocation.functionInvocationTimestamp;
+            newRecord.updated_at = functionInvocation.functionInvocationTimestamp;
+
+            // replacing a record implicitly undeletes it
+            newRecord.deleted_at = '';
+            newRecord.deleted = false;
+
+            newRecord.changeDetectionHash = makeHash(newRecord);
+
+            changedDetected = true;
+            event = craftCreateEvent(newRecord);
+
+        } else {
+            newRecord = Object.assign(newRecord, payload);
+            newRecord.created_at = oldRecord.created_at;
+            newRecord.updated_at = functionInvocation.functionInvocationTimestamp;
+
+            // replacing a record implicitly undeletes it
+            newRecord.deleted_at = '';
+            newRecord.deleted = false;
+
+            newRecord.changeDetectionHash = makeHash(newRecord);
+
+            changedDetected = (oldRecord.changeDetectionHash === newRecord.changeDetectionHash) ? false : true;
+    
+            if (changedDetected) {
+                event = craftUpdateEvent(oldRecord, newRecord);
+            } else {
+                newRecord = oldRecord;
+                event = false;
+            }
+        }
+
+        return {changedDetected: changedDetected, event: event, newRecord: newRecord};
+    }
+
+    function craftCreateEvent(new_record) {
+        const event_type = 'Flenderson.IPPSPerson.Create';
+        const source = 'create';
+        const schema = 'create';
+        const label = `${new_record.email}'s IPPS record created.`;
+        const payload = {
+            record: new_record
+        };
+
+        const event = craftEvent(new_record.id, source, schema, event_type, label, payload);
+        return event;
+    }
+    
+    function craftUpdateEvent(old_record, new_record) {
+        let event_type = 'Flenderson.IPPSPerson.Update';
+        let source = 'update';
+        let schema = 'update';
+        let label = `${new_record.email}'s IPPS record updated.`;
+        let payload = {
+            old_record: old_record,
+            new_record: new_record,
+        };
+
+        let event = craftEvent(new_record.id, source, schema, event_type, label, payload);
+        return event;
+    }
+
+    function craftDeleteEvent(old_record) {
+        const event_type = 'Flenderson.IPPSPerson.Delete';
+        const source = 'delete';
+        const schema = 'delete';
+        const label = `${old_record.email}'s IPPS record deleted.`;
+        const payload = {
+            record: old_record
+        };
+
+        const event = craftEvent(old_record.id, source, schema, event_type, label, payload);
+        return event;
+    }
+
+    function craftEvent(recordID, source, schema, event_type, label, payload) {
+        const event = {
+            id: `${event_type}-${functionInvocation.functionInvocationID}`,
+            time: functionInvocation.functionInvocationTimestamp,
+
+            type: event_type,
+            source: `/flenderson/ipps-person/${recordID}/${source}`,
+            schemaURL: `ca.wrdsb.flenderson.ipps-person.${schema}.json`,
+
+            label: label,
+            tags: [
+                "flenderson", 
+                "ipps_person_change",
+                "person_change"
+            ], 
+
+            data: {
+                function_name: functionInvocation.functionName,
+                invocation_id: functionInvocation.functionInvocationID,
+                result: {
+                    payload: payload 
+                },
+            },
+
+            eventTypeVersion: "0.1",
+            specversion: "0.2",
+            contentType: "application/json"
+        };
+
+        // TODO: check message length
+        return event;
+    }
+
+    function makeHash(ippsPerson: IPPSPerson): string {
+        const objectForHash = JSON.stringify({
+            email:        ippsPerson.email,
+            username:     ippsPerson.username,
+            ein:          ippsPerson.ein,
+            directory:    ippsPerson.directory,
+            phone:        ippsPerson.phone,
+            extension:    ippsPerson.extension,
+            mbxnumber:    ippsPerson.mbxnumber,
+            assignments:  ippsPerson.assignments
+        });
+        const objectHash = createHash('md5').update(objectForHash).digest('hex');
+        return objectHash;
+    }
+};
+
+export default ippsPersonStore;
