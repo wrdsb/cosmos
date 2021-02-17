1c7c5e42b4fa2c329f238395918d16d6a489ea90
James Schumann <james@schumann.ca>
2021-02-17 11:02:03 -0500

    last version of hris-record

--- libs/types/src/lib/entities/cosmos/hris-record.ts
+++ libs/types/src/lib/entities/cosmos/hris-record.ts
@@ -1,5 +1,12 @@
-interface HRISRecord {
-    id: string;
+import { CosmosCoreFields } from "@cosmos/types";
+
+interface HRISRecord extends CosmosCoreFields {
+    // Fields from CosmosCoreFields
+    // id: string;
+    // createdAt: string;
+    // updatedAt: string;
+    // deletedAt: string;
+    // deleted: boolean
 }
 
 export { HRISRecord };
