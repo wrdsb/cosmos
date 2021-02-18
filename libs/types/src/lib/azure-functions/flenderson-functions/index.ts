e05573c708613c1cd60bdd30c9fb8bc0cbae5386
James Schumann <james@schumann.ca>
2021-02-18 10:14:46 -0500

    update types for flenderson functions

--- libs/types/src/lib/azure-functions/flenderson-functions/index.ts
+++ libs/types/src/lib/azure-functions/flenderson-functions/index.ts
@@ -2,3 +2,8 @@ export * from './ipps-employee-group-store';
 export * from './ipps-job-store';
 export * from './ipps-location-store';
 export * from './ipps-person-store';
+
+export * from './ipps-employee-groups-reconcile';
+export * from './ipps-jobs-reconcile';
+export * from './ipps-locations-reconcile';
+export * from './ipps-people-reconcile';
