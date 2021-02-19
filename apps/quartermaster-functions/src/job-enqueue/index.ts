5570b3870497a390e4184a3a4db24374d58f0837
James Schumann <james@schumann.ca>
2021-02-19 13:07:19 -0500

    make job-enqueu match code from panama

--- apps/quartermaster-functions/src/job-enqueue/index.ts
+++ apps/quartermaster-functions/src/job-enqueue/index.ts
@@ -1,12 +1,41 @@
 import { AzureFunction, Context } from "@azure/functions"
-import { DeviceLoanSubmissionStoreFunctionRequest } from "@cosmos/types";
+import { FunctionInvocation, DeviceLoanSubmissionStoreFunctionRequest } from "@cosmos/types";
 
 const jobEnqueue: AzureFunction = async function (context: Context, triggerMessage): Promise<void> {
-    const execution_timestamp = (new Date()).toJSON();  // format: 2012-04-23T18:25:43.511Z
+    const functionInvocation = {
+        functionInvocationID: context.executionContext.invocationId,
+        functionInvocationTimestamp: new Date().toJSON(),
+        functionApp: 'Panama',
+        functionName: context.executionContext.functionName,
+        functionDataType: 'Event',
+        functionDataOperation: 'Emit',
+        eventLabel: ''
+    } as FunctionInvocation;
+
+    const sgMail = require('@sendgrid/mail');
+    sgMail.setApiKey(process.env['SENDGRID_API_KEY']);
 
     const jobType = triggerMessage.jobType;
     const operation = triggerMessage.operation;
     const payload = triggerMessage.payload;
+    const blobFile = triggerMessage.blobFile;
+
+    let queueTriggered = '';
+    let queueMessage = {};
+    let sentQueueMessage = false;
+
+    const html = '';
+    const sendNotification = false;
+
+    let logPayload = {
+        status: '',
+        message: '',
+        queueMessage: '',
+        queueTriggered: '',
+        error: '',
+        result: ''
+    };
+    let notification = {};
 
     if (jobType) {
         switch (jobType) {
@@ -15,132 +44,130 @@ const jobEnqueue: AzureFunction = async function (context: Context, triggerMessa
                     operation: operation,
                     payload: payload
                 } as DeviceLoanSubmissionStoreFunctionRequest;
-
-                context.bindings.invocationPostProcessor = JSON.stringify({
-                    status: 202,
-                    body: "Accepted. Queued Quartermaster.DeviceLoanSubmission.Store job."
-                });
-
                 break;
 
-            case "Quartermaster.View.ATSAsset.Process":
-                context.bindings.viewATSAssetProcessTrigger = createViewATSAssetProcessJob();
-                
-                context.bindings.invocationPostProcessor = JSON.stringify({
-                    status: 202,
-                    body: "Accepted. Queued Quartermaster.View.ATSAsset.Process job."
-                });
-
+            case 'WRDSB.Quartermaster.View.Asset.Checksum.Process':
+                queueTriggered = 'view-ats-asset-checksum-process';
+                queueMessage = {
+                    jobType: "WRDSB.Quartermaster.View.Asset.Checksum.Process",
+                    incomingBlob: blobFile
+                };
+                context.bindings.viewATSAssetChecksumProcessTrigger = queueMessage;
+                sentQueueMessage = true;
+                break;
+    
+            case "WRDSB.Quartermaster.View.Asset.Process":
+                queueTriggered = 'view-ats-asset-process';
+                queueMessage = {
+                    jobType: 'WRDSB.Quartermaster.View.Asset.Process',
+                    incomingBlob: blobFile
+                };
+                context.bindings.viewATSAssetProcessTrigger = queueMessage;
+                sentQueueMessage = true;
                 break;
 
-            case "Quartermaster.View.ATSAssetClass.Process":
-                context.bindings.viewATSAssetClassProcessTrigger = createViewATSAssetClassProcessJob();
-                
-                context.bindings.invocationPostProcessor = JSON.stringify({
-                    status: 202,
-                    body: "Accepted. Queued Quartermaster.View.ATSAssetClass.Process job."
-                });
-
+            case "WRDSB.Quartermaster.View.AssetClass.Process":
+                queueTriggered = 'view-ats-asset-class-process';
+                queueMessage = {
+                    jobType: 'WRDSB.Quartermaster.View.AssetClass.Process',
+                    incomingBlob: blobFile
+                };
+                context.bindings.viewATSAssetClassProcessTrigger = queueMessage;
+                sentQueueMessage = true;
                 break;
 
-            case "Quartermaster.View.ATSAssetType.Process":
-                context.bindings.viewATSAssetTypeProcessTrigger = createViewATSAssetTypeProcessJob();
-                
-                context.bindings.invocationPostProcessor = JSON.stringify({
-                    status: 202,
-                    body: "Accepted. Queued Quartermaster.View.ATSAssetType.Process job."
-                });
-
+            case "WRDSB.Quartermaster.View.AssetType.Process":
+                queueTriggered = 'view-ats-asset-type-process';
+                queueMessage = {
+                    jobType: 'WRDSB.Quartermaster.View.AssetType.Process',
+                    incomingBlob: blobFile
+                };
+                context.bindings.viewATSAssetTypeProcessTrigger = queueMessage;
+                sentQueueMessage = true;
                 break;
 
-            case "Quartermaster.View.ATSAssetClassType.Process":
-                context.bindings.viewATSAssetClassTypeProcessTrigger = createViewATSAssetClassTypeProcessJob();
-                
-                context.bindings.invocationPostProcessor = JSON.stringify({
-                    status: 202,
-                    body: "Accepted. Queued Quartermaster.View.ATSAssetClassType.Process job."
-                });
-
+            case "WRDSB.Quartermaster.View.AssetClassType.Process":
+                queueTriggered = 'view-ats-asset-class-type-process';
+                queueMessage = {
+                    jobType: 'WRDSB.Quartermaster.View.AssetClassType.Process',
+                    incomingBlob: blobFile
+                };
+                context.bindings.viewATSAssetClassTypeProcessTrigger = queueMessage;
+                sentQueueMessage = true;
                 break;
 
-            case "Quartermaster.View.ATSAsset.Extract.Asset":
-                context.bindings.viewATSAssetExtractAssetsTrigger = createViewATSAssetExtractAssetJob();
-                
-                context.bindings.invocationPostProcessor = JSON.stringify({
-                    status: 202,
-                    body: "Accepted. Queued Quartermaster.View.ATSAsset.Extract.Asset job."
-                });
-
+            case "WRDSB.Quartermaster.View.Asset.Extract.Assets":
+                queueTriggered = 'view-ats-asset-extract-assets';
+                queueMessage = {
+                    jobType: 'WRDSB.Quartermaster.View.Asset.Extract.Assets',
+                    incomingBlob: blobFile
+                };
+                context.bindings.viewATSAssetExtractAssetsTrigger = queueMessage;
+                sentQueueMessage = true;
                 break;
 
-            case "Quartermaster.View.ATSAssetClass.Extract.AssetClass":
-                context.bindings.viewATSAssetClassExtractAssetClassesTrigger = createViewATSAssetClassExtractAssetClassJob();
-                
-                context.bindings.invocationPostProcessor = JSON.stringify({
-                    status: 202,
-                    body: "Accepted. Queued Quartermaster.View.ATSAssetClass.Extract.AssetClass job."
-                });
-
+            case "WRDSB.Quartermaster.View.AssetClass.Extract.AssetClasses":
+                queueTriggered = 'view-ats-asset-class-extract-asset-classes';
+                queueMessage = {
+                    jobType: 'WRDSB.Quartermaster.View.AssetClass.Extract.AssetClasses'
+                };
+                context.bindings.viewATSAssetClassExtractAssetClassesTrigger = queueMessage;
+                sentQueueMessage = true;
                 break;
 
-            case "Quartermaster.View.ATSAssetType.Extract.AssetType":
-                context.bindings.viewATSAssetTypeExtractAssetTypesTrigger = createViewATSAssetTypeExtractAssetTypeJob();
-                
-                context.bindings.invocationPostProcessor = JSON.stringify({
-                    status: 202,
-                    body: "Accepted. Quartermaster.View.ATSAssetType.Extract.AssetType job."
-                });
-
+            case "WRDSB.Quartermaster.View.AssetType.Extract.AssetTypes":
+                queueTriggered = 'view-ats-asset-type-extract-asset-types';
+                queueMessage = {
+                    jobType: 'WRDSB.Quartermaster.View.AssetType.Extract.AssetTypes'
+                };
+                context.bindings.viewATSAssetTypeExtractAssetTypesTrigger = queueMessage;
+                sentQueueMessage = true;
                 break;
 
-            case "Quartermaster.View.ATSAssetClassType.Extract.AssetClassType":
-                context.bindings.viewATSAssetClassTypeExtractAssetClassTypesTrigger = createViewATSAssetClassTypeExtractAssetClassTypeJob();
-                
-                context.bindings.invocationPostProcessor = JSON.stringify({
-                    status: 202,
-                    body: "Accepted. Queued Quartermaster.View.ATSAssetClassType.Extract.AssetClassType job."
-                });
-
+            case "WRDSB.Quartermaster.View.AssetClassType.Extract.AssetClassTypes":
+                queueTriggered = 'view-ats-asset-class-type-extract-asset-class-types';
+                queueMessage = {
+                    jobType: 'WRDSB.Quartermaster.View.AssetClassType.Extract.AssetClassTypes'
+                };
+                context.bindings.viewATSAssetClassTypeExtractAssetClassTypesTrigger = queueMessage;
+                sentQueueMessage = true;
                 break;
 
-            case "Quartermaster.ATS.Assets.Reconcile":
-                context.bindings.atsAssetsReconcileTrigger = createQuartermasterATSAssetsReconcileJob();
-                
-                context.bindings.invocationPostProcessor = JSON.stringify({
-                    status: 202,
-                    body: "Accepted. Queued Quartermaster.ATS.Assets.Reconcile job."
-                });
-
+            case "WRDSB.Quartermaster.ATS.Assets.Reconcile":
+                queueTriggered = 'ats-assets-reconcile';
+                queueMessage = {
+                    jobType: 'WRDSB.Quartermaster.ATS.Assets.Reconcile',
+                    incomingBlob: blobFile
+                };
+                context.bindings.atsAssetsReconcileTrigger = queueMessage;
+                sentQueueMessage = true;
                 break;
 
-            case "Quartermaster.ATS.AssetClasses.Reconcile":
-                context.bindings.atsAssetClassesReconcileTrigger = createQuartermasterATSAssetClassesReconcileJob();
-                
-                context.bindings.invocationPostProcessor = JSON.stringify({
-                    status: 202,
-                    body: "Accepted. Queued Quartermaster.ATS.AssetClasses.Reconcile job."
-                });
-
+            case "WRDSB.Quartermaster.ATS.AssetClasses.Reconcile":
+                queueTriggered = 'ats-asset-classes-reconcile';
+                queueMessage = {
+                    jobType: 'WRDSB.Quartermaster.ATS.AssetClasses.Reconcile'
+                };
+                context.bindings.atsAssetClassesReconcileTrigger = queueMessage;
+                sentQueueMessage = true;
                 break;
 
-            case "Quartermaster.ATS.AssetTypes.Reconcile":
-                context.bindings.atsAssetTypesReconcileTrigger = createQuartermasterATSAssetTypesReconcileJob();
-                
-                context.bindings.invocationPostProcessor = JSON.stringify({
-                    status: 202,
-                    body: "Accepted. Queued Quartermaster.ATS.AssetTypes.Reconcile job."
-                });
-
+            case "WRDSB.Quartermaster.ATS.AssetTypes.Reconcile":
+                queueTriggered = 'ats-asset-types-reconcile';
+                queueMessage = {
+                    jobType: 'WRDSB.Quartermaster.ATS.AssetTypes.Reconcile'
+                };
+                context.bindings.atsAssetTypesReconcileTrigger = queueMessage;
+                sentQueueMessage = true;
                 break;
 
-            case "Quartermaster.ATS.AssetClassTypes.Reconcile":
-                context.bindings.atsAssetClassTypesReconcileTrigger = createQuartermasterATSAssetClassTypesReconcileJob();
-                
-                context.bindings.invocationPostProcessor = JSON.stringify({
-                    status: 202,
-                    body: "Accepted. Queued Quartermaster.ATS.AssetClassTypes.Reconcile job."
-                });
-
+            case "WRDSB.Quartermaster.ATS.AssetClassTypes.Reconcile":
+                queueTriggered = 'ats-asset-class-types-reconcile';
+                queueMessage = {
+                    jobType: 'WRDSB.Quartermaster.ATS.AssetClassTypes.Reconcile'
+                };
+                context.bindings.atsAssetClassTypesReconcileTrigger = queueMessage;
+                sentQueueMessage = true;
                 break;
 
             default:
@@ -148,7 +175,6 @@ const jobEnqueue: AzureFunction = async function (context: Context, triggerMessa
                     status: 422,
                     body: "Unprocessable Entity. Cannot find the specified jobType."
                 });
-
                 break;
         }
     }
@@ -159,70 +185,55 @@ const jobEnqueue: AzureFunction = async function (context: Context, triggerMessa
         });
     }
 
-    function createSkinnerViewSkinnerAssignmentsProcessJob(): string {
-        let triggerMessage = {};
-        return JSON.stringify(triggerMessage);
+    if (sentQueueMessage) {
+        logPayload = {
+            status: "200",
+            message: `Sent ${queueMessage} to ${queueTriggered}`,
+            queueMessage: queueMessage,
+            queueTriggered: queueTriggered,
+            result: '',
+            error: ''
+        };
     }
 
-    function createViewATSAssetProcessJob(): string {
-        let triggerMessage = {};
-        return JSON.stringify(triggerMessage);
+    if (sendNotification) {
+        notification = {
+            subject: 'Poison Message Notification',
+            to: process.env['SENDGRID_TO'],
+            from: {
+                email: 'errors@panama.wrdsb.io',
+                name: 'Panama Errors'
+            },
+            html: html
+        };
+        sgMail.send(notification, (error, result) => {
+            if (error) {
+                logPayload = {
+                    status: "500",
+                    message: 'Failed to send email notification.',
+                    queueMessage: '',
+                    queueTriggered: '',
+                    result: '',
+                    error: error,
+                };
+            } else {
+                logPayload = {
+                    status: "200",
+                    message: 'Sent email notification',
+                    queueMessage: '',
+                    queueTriggered: '',
+                    result: result,
+                    error: error
+                };
+            }
+        });
     }
 
-    function createViewATSAssetClassProcessJob(): string {
-        let triggerMessage = {};
-        return JSON.stringify(triggerMessage);
-    }
+    functionInvocation.logPayload = logPayload;
 
-    function createViewATSAssetTypeProcessJob(): string {
-        let triggerMessage = {};
-        return JSON.stringify(triggerMessage);
-    }
-
-    function createViewATSAssetClassTypeProcessJob(): string {
-        let triggerMessage = {};
-        return JSON.stringify(triggerMessage);
-    }
-
-    function createViewATSAssetExtractAssetJob(): string {
-        let triggerMessage = {};
-        return JSON.stringify(triggerMessage);
-    }
-
-    function createViewATSAssetClassExtractAssetClassJob(): string {
-        let triggerMessage = {};
-        return JSON.stringify(triggerMessage);
-    }
-
-    function createViewATSAssetTypeExtractAssetTypeJob(): string {
-        let triggerMessage = {};
-        return JSON.stringify(triggerMessage);
-    }
-
-    function createViewATSAssetClassTypeExtractAssetClassTypeJob(): string {
-        let triggerMessage = {};
-        return JSON.stringify(triggerMessage);
-    }
-
-    function createQuartermasterATSAssetsReconcileJob(): string {
-        let triggerMessage = {};
-        return JSON.stringify(triggerMessage);
-    }
-
-    function createQuartermasterATSAssetClassesReconcileJob(): string {
-        let triggerMessage = {};
-        return JSON.stringify(triggerMessage);
-    }
-
-    function createQuartermasterATSAssetTypesReconcileJob(): string {
-        let triggerMessage = {};
-        return JSON.stringify(triggerMessage);
-    }
-
-    function createQuartermasterATSAssetClassTypesReconcileJob(): string {
-        let triggerMessage = {};
-        return JSON.stringify(triggerMessage);
-    }
+    context.bindings.invocationPostProcessor = functionInvocation;
+    context.log(functionInvocation);
+    context.done(null, functionInvocation);
 };
 
 export default jobEnqueue;
