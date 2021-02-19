dd30eb36f84d0724154211f0b64aab78769c63ee
James Schumann <james@schumann.ca>
2021-02-19 07:50:57 -0500

    update functions app supporting files

--- apps/panama-functions/webpack.config.js
+++ apps/panama-functions/webpack.config.js
@@ -55,7 +55,9 @@ module.exports = (config, context) => {
         resolve: {
             extensions: ['.tsx', '.ts', '.js'],
             plugins: [
-                new TsconfigPathsPlugin()
+                new TsconfigPathsPlugin({
+                    configFile: 'apps/panama-functions/tsconfig.app.json'
+                })
             ]
         },
         /**
