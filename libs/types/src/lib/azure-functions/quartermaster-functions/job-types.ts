0efccd53cf9efc14a6d10044f8dfee5a512d9ad2
James Schumann <james@schumann.ca>
2021-03-02 13:14:01 -0500

    add type for QuartermasterJobType

---
+++ libs/types/src/lib/azure-functions/quartermaster-functions/job-types.ts
@@ -0,0 +1,24 @@
+export type QuartermasterJobType =
+'Quartermaster.DeviceLoanSubmission.Store' |
+
+'Quartermaster.Asset.Store' |
+'Quartermaster.AssetAssignment.Store' |
+'Quartermaster.AssetAssignmentHistory.Materialize' |
+'Quartermaster.AssetAssignmentHistory.MaterializeAll' |
+'Quartermaster.AssetAssignmentHistory.Store' |
+'Quartermaster.AssetEntitlement.Store' |
+
+'Quartermaster.ViewATSAssetChecksum.Process' |
+'Quartermaster.ViewATSAsset.Process' |
+'Quartermaster.ViewATSAsset.ExtractAssets' |
+'Quartermaster.ViewATSAssetClass.Process' |
+'Quartermaster.ViewATSAssetClass.ExtractAssetClasses' |
+'Quartermaster.ViewATSAssetType.Process' |
+'Quartermaster.ViewATSAssetType.ExtractAssetTypes' |
+'Quartermaster.ViewATSAssetClassType.Process' |
+'Quartermaster.ViewATSAssetClassType.ExtractAssetClassTypes' |
+
+'Quartermaster.ATSAsset.Reconcile' |
+'Quartermaster.ATSAssetClass.Reconcile' |
+'Quartermaster.ATSAssetType.Reconcile' |
+'Quartermaster.ATSAssetClassType.Reconcile';
