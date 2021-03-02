977b98d72a9264eacbe4356a09c97b37b0829c8a
James Schumann <james@schumann.ca>
2021-03-02 14:52:44 -0500

    add all jobTypes available in job-enqueue

--- apps/quartermaster-functions/src/quartermaster-command/index.ts
+++ apps/quartermaster-functions/src/quartermaster-command/index.ts
@@ -1,39 +1,187 @@
 import { AzureFunction, Context } from "@azure/functions"
-import { FunctionInvocation, DeviceLoanSubmissionCommandFunctionRequest } from "@cosmos/types";
+import { FunctionInvocation,
+    QuartermasterCommandFunctionRequest,
+    QuartermasterCommandFunctionRequestBody,
+    QuartermasterCommandJobType,
+    QuartermasterCommandOperation,
+    QuartermasterCommandFunctionRequestPayload } from "@cosmos/types";
 
-const quartermasterCommand: AzureFunction = async function (context: Context, req: DeviceLoanSubmissionCommandFunctionRequest): Promise<void> {
+const quartermasterCommand: AzureFunction = async function (context: Context, req: QuartermasterCommandFunctionRequest): Promise<void> {
     const functionInvocation = {
         functionInvocationID: context.executionContext.invocationId,
         functionInvocationTimestamp: new Date().toJSON(),
         functionApp: 'Quartermaster',
         functionName: context.executionContext.functionName,
-        functionDataType: 'DeviceLoanSubmission',
+        functionDataType: 'Any',
         functionDataOperation: 'Command',
         eventLabel: ''
     } as FunctionInvocation;
 
-    const request = req.body;
-    const jobType = request.jobType;
-    const operation = request.operation;
-    const payload = request.payload;
+    const request = req as QuartermasterCommandFunctionRequest;
+    const requestBody = request.body as QuartermasterCommandFunctionRequestBody;
+    const jobType = requestBody.jobType as QuartermasterCommandJobType;
+    const operation = requestBody.operation as QuartermasterCommandOperation;
+    const payload = requestBody.payload as QuartermasterCommandFunctionRequestPayload;
 
     switch (jobType) {
         case 'Quartermaster.DeviceLoanSubmission.Store':
             context.bindings.jobEnqueue = {
-                jobType: 'Quartermaster.DeviceLoanSubmission.Store',
+                jobType: jobType,
                 operation: operation,
                 payload: payload
             };
-
             break;
 
-        case 'Quartermaster.DeviceLoanSubmission.Return':
+        case 'Quartermaster.Asset.Store':
             context.bindings.jobEnqueue = {
-                jobType: 'Quartermaster.DeviceLoanSubmission.Store',
+                jobType: jobType,
                 operation: operation,
                 payload: payload
             };
+            break;
 
+        case 'Quartermaster.AssetAssignment.Store':
+            context.bindings.jobEnqueue = {
+                jobType: jobType,
+                operation: operation,
+                payload: payload
+            };
+            break;
+
+        case 'Quartermaster.AssetAssignmentHistory.Materialize':
+            context.bindings.jobEnqueue = {
+                jobType: jobType,
+                operation: operation,
+                payload: payload
+            };
+            break;
+
+        case 'Quartermaster.AssetAssignmentHistory.MaterializeAll':
+            context.bindings.jobEnqueue = {
+                jobType: jobType,
+                operation: operation,
+                payload: payload
+            };
+            break;
+
+        case 'Quartermaster.AssetAssignmentHistory.Store':
+            context.bindings.jobEnqueue = {
+                jobType: jobType,
+                operation: operation,
+                payload: payload
+            };
+            break;
+
+        case 'Quartermaster.AssetEntitlement.Store':
+            context.bindings.jobEnqueue = {
+                jobType: jobType,
+                operation: operation,
+                payload: payload
+            };
+            break;
+
+        case 'Quartermaster.ViewATSAssetChecksum.Process':
+            context.bindings.jobEnqueue = {
+                jobType: jobType,
+                operation: operation,
+                payload: payload
+            };
+            break;
+
+        case 'Quartermaster.ViewATSAsset.Process':
+            context.bindings.jobEnqueue = {
+                jobType: jobType,
+                operation: operation,
+                payload: payload
+            };
+            break;
+
+        case 'Quartermaster.ViewATSAsset.ExtractAssets':
+            context.bindings.jobEnqueue = {
+                jobType: jobType,
+                operation: operation,
+                payload: payload
+            };
+            break;
+                
+        case 'Quartermaster.ViewATSAssetClass.Process':
+            context.bindings.jobEnqueue = {
+                jobType: jobType,
+                operation: operation,
+                payload: payload
+            };
+            break;
+                    
+        case 'Quartermaster.ViewATSAssetClass.ExtractAssetClasses':
+            context.bindings.jobEnqueue = {
+                jobType: jobType,
+                operation: operation,
+                payload: payload
+            };
+            break;
+        
+        case 'Quartermaster.ViewATSAssetType.Process':
+            context.bindings.jobEnqueue = {
+                jobType: jobType,
+                operation: operation,
+                payload: payload
+            };
+            break;
+            
+        case 'Quartermaster.ViewATSAssetType.ExtractAssetTypes':
+            context.bindings.jobEnqueue = {
+                jobType: jobType,
+                operation: operation,
+                payload: payload
+            };
+            break;
+
+        case 'Quartermaster.ViewATSAssetClassType.Process':
+            context.bindings.jobEnqueue = {
+                jobType: jobType,
+                operation: operation,
+                payload: payload
+            };
+            break;
+
+        case 'Quartermaster.ViewATSAssetClassType.ExtractAssetClassTypes':
+            context.bindings.jobEnqueue = {
+                jobType: jobType,
+                operation: operation,
+                payload: payload
+            };
+            break;
+        
+        case 'Quartermaster.ATSAsset.Reconcile':
+            context.bindings.jobEnqueue = {
+                jobType: jobType,
+                operation: operation,
+                payload: payload
+            };
+            break;
+
+        case 'Quartermaster.ATSAssetClass.Reconcile':
+            context.bindings.jobEnqueue = {
+                jobType: jobType,
+                operation: operation,
+                payload: payload
+            };
+            break;
+
+        case 'Quartermaster.ATSAssetType.Reconcile':
+            context.bindings.jobEnqueue = {
+                jobType: jobType,
+                operation: operation,
+                payload: payload
+            };
+            break;
+
+        case 'Quartermaster.ATSAssetClassType.Reconcile':
+            context.bindings.jobEnqueue = {
+                jobType: jobType,
+                operation: operation,
+                payload: payload
+            };
             break;
 
         default:
@@ -43,8 +191,8 @@ const quartermasterCommand: AzureFunction = async function (context: Context, re
     const response = {
         header: {
             status: 202,
-            message: "",
-            chatter: "",
+            message: `Accepted. Enqueued jobType ${jobType}.`,
+            chatter: `Wilco. Create and process ${jobType} job.`,
             timestamp: functionInvocation.functionInvocationTimestamp,
         },
         status: 202,
