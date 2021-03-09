ff908a075046c8f30383bb2ad9717e7dca3df147
James Schumann <james@schumann.ca>
2021-03-09 08:41:55 -0500

    standardize radar ping. closes #296

--- apps/radar-functions/ping/index.ts
+++ apps/radar-functions/ping/index.ts
@@ -1,17 +1,90 @@
-import { AzureFunction, Context, HttpRequest } from "@azure/functions"
+import { AzureFunction, Context, HttpRequest } from "@azure/functions";
+import jwt_decode from 'jwt-decode';
+import { FunctionInvocation } from "@cosmos/types";
 
 const ping: AzureFunction = async function (context: Context, req: HttpRequest): Promise<void> {
-    context.log('HTTP trigger function processed a request.');
-    const name = (req.query.name || (req.body && req.body.name));
-    const responseMessage = name
-        ? "Hello, " + name + ". This HTTP triggered function executed successfully."
-        : "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.";
+    const functionInvocation = {
+        functionInvocationID: context.executionContext.invocationId,
+        functionInvocationTimestamp: new Date().toJSON(),
+        functionApp: 'RADAR',
+        functionName: context.executionContext.functionName,
+        functionDataType: 'Ping',
+        functionDataOperation: 'Ping',
+        eventLabel: ''
+    } as FunctionInvocation;
 
-    context.res = {
-        // status: 200, /* Defaults to 200 */
-        body: responseMessage
+    const request = req;
+
+    interface MSALToken {
+        name: string;
+        unique_name: string;
+        roles: string[];
+    };
+    let authenticated = false;
+    let authorized = false;
+    let idToken = '';
+    let userName = '';
+    let userEmail = '';
+    let userRoles = [];
+
+    if (request.headers['x-ms-token-aad-id-token']) {
+        authenticated = true;
+        idToken = request.headers['x-ms-token-aad-id-token'];
+        let decodedToken = jwt_decode(idToken) as MSALToken;
+        userName = decodedToken.name;
+        userEmail = decodedToken.unique_name;
+        userRoles = decodedToken.roles as string[];
+        authorized = userRoles.includes('cosmos-user-its') ? true : false;
+    }
+
+    let response = {
+        header: {
+            status: 200,
+            message: "",
+            chatter: "",
+            timestamp: functionInvocation.functionInvocationTimestamp,
+            authenticated: authenticated,
+            authorized: authorized,
+            userName: userName,
+            userEmail: userEmail,
+            userRoles: userRoles
+        },
+        payload: []
     };
 
+    if (!authenticated) {
+        response.header.status = 401;
+        response.header.message = "Unauthorized: Cannot verify your identity.";
+        response.header.chatter = "Unauthorized: Cannot verify your identity.";
+    }
+    else if (authenticated && !authorized) {
+        response.header.status = 403;
+        response.header.message = "Forbidden: You are not permitted to ping RADAR";
+        response.header.chatter = "Forbidden: You are not permitted to ping RADAR";
+    }
+    else if (authenticated && authorized) {
+        response.header.status = 200;
+        response.header.message = "RADAR is up";
+        response.header.chatter = "RADAR here. What can I do for you?";
+    }
+    else {
+        response.header.status = 400;
+        response.header.message = "Bad Request: We're not sure what happend, but we're pretty sure it's you, not us.";
+        response.header.chatter = "Bad Request: We're not sure what happend, but we're pretty sure it's you, not us.";
+    }
+
+    context.res = {
+        status: response.header.status,
+        body: response
+    }
+
+    const logPayload = response;
+    context.log(logPayload);
+
+    functionInvocation.logPayload = logPayload;
+    context.log(functionInvocation);
+
+    context.done(null, functionInvocation);
 };
 
 export default ping;
