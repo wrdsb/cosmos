50440cac6e8e742dea0a08ed5bbf9dbff05faca9
James Schumann <james@schumann.ca>
2021-03-10 10:39:15 -0500

    auth working with our AAD

--- libs/user-auth2/src/lib/user-auth2.module.ts
+++ libs/user-auth2/src/lib/user-auth2.module.ts
@@ -1,7 +1,94 @@
-import { NgModule } from '@angular/core';
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
+
+import { MsalGuard, MsalInterceptor, MsalBroadcastService, MsalInterceptorConfiguration, MsalModule, MsalService, MSAL_GUARD_CONFIG, MSAL_INSTANCE, MSAL_INTERCEPTOR_CONFIG, MsalGuardConfiguration, MsalRedirectComponent } from '@azure/msal-angular';
+import { IPublicClientApplication, PublicClientApplication, InteractionType, BrowserCacheLocation, LogLevel } from '@azure/msal-browser';
+
+import { EnvironmentService } from "@cosmos/environment";
+
+import { AuthButtonComponent } from './auth-button/auth-button.component';
+
+const isIE = window.navigator.userAgent.indexOf("MSIE ") > -1 || window.navigator.userAgent.indexOf("Trident/") > -1; // Remove this line to use Angular Universal
+
+function loggerCallback(logLevel: LogLevel, message: string) {
+  console.log(message);
+}
+
+function MSALInstanceFactory(env: EnvironmentService): IPublicClientApplication {
+  return new PublicClientApplication({
+    auth: {
+      clientId: env.aadClientId,
+      authority: env.aadAuthority,
+      redirectUri: env.aadRedirectUri,
+      postLogoutRedirectUri: env.aadPostLogoutRedirectUri,
+      navigateToLoginRequestUrl: env.aadNavigateToLoginRequestUrl
+    },
+    cache: {
+      cacheLocation: BrowserCacheLocation.LocalStorage,
+      storeAuthStateInCookie: isIE, // set to true for IE 11. Remove this line to use Angular Universal
+    },
+    system: {
+      loggerOptions: {
+        loggerCallback,
+        logLevel: LogLevel.Info,
+        piiLoggingEnabled: false
+      }
+    }
+  });
+}
+
+function MSALInterceptorConfigFactory(): MsalInterceptorConfiguration {
+  const protectedResourceMap = new Map<string, Array<string>>();
+  protectedResourceMap.set('https://graph.microsoft.com/v1.0/me', ['user.read']);
+
+  return {
+    interactionType: InteractionType.Redirect,
+    protectedResourceMap
+  };
+}
+
+function MSALGuardConfigFactory(): MsalGuardConfiguration {
+  return { 
+    interactionType: InteractionType.Redirect,
+    authRequest: {
+      scopes: ['user.read']
+    },
+    loginFailedRoute: '/login-failed'
+  };
+}
 
 @NgModule({
+  declarations: [
+    AuthButtonComponent
+  ],
   imports: [CommonModule],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
+  providers: [
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: MsalInterceptor,
+      multi: true
+    },
+    MsalService,
+    MsalGuard,
+    MsalBroadcastService,
+    {
+      provide: MSAL_INSTANCE,
+      useFactory: MSALInstanceFactory,
+      deps: [ EnvironmentService ]
+    },
+    {
+      provide: MSAL_GUARD_CONFIG,
+      useFactory: MSALGuardConfigFactory
+    },
+    {
+      provide: MSAL_INTERCEPTOR_CONFIG,
+      useFactory: MSALInterceptorConfigFactory
+    }
+  ],
+  exports: [
+    AuthButtonComponent
+  ]
 })
 export class UserAuth2Module {}
