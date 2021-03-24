32f319bf4914dcf92e9b7dfe841768c8b917d2ca
James Schumann <james@schumann.ca>
2021-03-24 16:38:09 -0400

    define header menu content in app.component.ts

--- apps/quartermaster-console/src/app/app.component.ts
+++ apps/quartermaster-console/src/app/app.component.ts
@@ -5,6 +5,8 @@ import { EnvironmentService } from '@cosmos/environment';
 import { ChassisService } from '@cosmos/chassis';
 import { UserAuthService } from '@cosmos/user-auth';
 
+import { Menu } from "@cosmos/types";
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -14,15 +16,58 @@ export class AppComponent implements OnInit {
   isIframe = false;
   isLoggedIn$: Observable<boolean>;
 
+  headerContent = {
+    items: [
+      {
+        menuID: 'atsMenu',
+        menuTitle: 'ATS',
+        links: [
+          {
+            linkTitle: 'ATS Dashboard',
+            routerLink: '/ats',
+            icon: 'dashboard'
+          },
+          {
+            linkTitle: 'Assets Search',
+            routerLink: '/ats/assets/search',
+            icon: 'search'
+          }
+        ]
+      },
+      {
+        menuID: 'devicesMenu',
+        menuTitle: 'Device Loans',
+        links: [
+          {
+            linkTitle: 'Devices Loans Dashboard',
+            routerLink: '/device-loans',
+            icon: 'dashboard'
+          },
+          {
+            linkTitle: 'Device Loans Search',
+            routerLink: '/device-loans/search',
+            icon: 'search'
+          }
+        ]
+      }
+    ]
+  } as Menu;
+
+  footerContent = {
+    items: [
+    ]
+  } as Menu;
+
   constructor(
     public environmentService: EnvironmentService,
     public chassisService: ChassisService,
     private userAuthService: UserAuthService
-  ) {
-    this.isLoggedIn$ = this.userAuthService.isLoggedIn$;
-  }
+  ) {}
 
   ngOnInit(): void {
+    this.chassisService.setHeaderContent(this.headerContent);
+    this.chassisService.setFooterContent(this.footerContent);
+
     this.isIframe = window !== window.parent && !window.opener; // Remove this line to use Angular Universal
   }
 
--- apps/viewfinder-console/src/app/app.component.ts
+++ apps/viewfinder-console/src/app/app.component.ts
@@ -5,6 +5,8 @@ import { EnvironmentService } from '@cosmos/environment';
 import { ChassisService } from '@cosmos/chassis';
 import { UserAuthService } from '@cosmos/user-auth';
 
+import { Menu } from "@cosmos/types";
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -14,6 +16,151 @@ export class AppComponent implements OnInit {
   isIframe = false;
   isLoggedIn$: Observable<boolean>;
 
+  headerContent = {
+    items: [
+      {
+        menuID: 'aadMenu',
+        menuTitle: 'AAD',
+        links: [
+          {
+            linkTitle: 'AAD Dashboard',
+            routerLink: '/aad',
+            icon: 'dashboard'
+          }
+        ]
+      },
+      {
+        menuID: 'atsMenu',
+        menuTitle: 'ATS',
+        links: [
+          {
+            linkTitle: 'ATS Dashboard',
+            routerLink: '/ats',
+            icon: 'dashboard'
+          },
+          {
+            linkTitle: 'Assets Search',
+            routerLink: '/ats/assets/search',
+            icon: 'search'
+          }
+        ]
+      },
+      {
+        menuID: 'devicesMenu',
+        menuTitle: 'Device Loans',
+        links: [
+          {
+            linkTitle: 'Devices Loans Dashboard',
+            routerLink: '/device-loans',
+            icon: 'dashboard'
+          },
+          {
+            linkTitle: 'Device Loans Search',
+            routerLink: '/device-loans/search',
+            icon: 'search'
+          }
+        ]
+      },
+      {
+        menuID: 'googleMenu',
+        menuTitle: 'Google',
+        links: [
+          {
+            linkTitle: 'Google Dashboard',
+            routerLink: '/google',
+            icon: 'dashboard'
+          },
+          {
+            linkTitle: 'Google Groups',
+            routerLink: '/google/groups',
+            icon: 'group'
+          },
+          {
+            linkTitle: 'Google Calendar',
+            routerLink: '/google/calendar',
+            icon: 'calendar_today'
+          }
+        ]
+      },
+      {
+        menuID: 'ippsMenu',
+        menuTitle: 'IPPS',
+        links: [
+          {
+            linkTitle: 'IPPS Dashboard',
+            routerLink: '/ipps',
+            icon: 'dashboard'
+          }
+        ]
+      },
+      {
+        menuID: 'peopleMenu',
+        menuTitle: 'People',
+        links: [
+          {
+          linkTitle: 'People Dashboard',
+          routerLink: '/people',
+          icon: 'dashboard'
+          }
+        ]
+      },
+      {
+        menuID: 'quartermasterMenu',
+        menuTitle: 'Quartermaster',
+        links: [
+          {
+            linkTitle: 'Quartermaster Dashboard',
+            routerLink: '/quartermaster',
+            icon: 'dashboard'
+          },
+          {
+            linkTitle: 'Assets Search',
+            routerLink: '/quartermaster/assets/search',
+            icon: 'search'
+          }
+        ]
+      },
+      {
+        menuID: 'schoolsMenu',
+        menuTitle: 'Schools',
+        links: [
+          {
+            linkTitle: 'Schools Dashboard',
+            routerLink: '/schools',
+            icon: 'dashboard'
+          }
+        ]
+      },
+      {
+        menuID: 'teamViewerMenu',
+        menuTitle: 'TeamViewer',
+        links: [
+          {
+            linkTitle: 'Teamviewer Dashboard',
+            routerLink: '/teamviewer',
+            icon: 'dashboard'
+          }
+        ]
+      },
+      {
+        menuID: 'trilliumMenu',
+        menuTitle: 'Trillium',
+        links: [
+          {
+            linkTitle: 'Trillium Dashboard',
+            routerLink: '/trillium',
+            icon: 'dashboard'
+          }
+        ]
+      }
+    ]
+  } as Menu;
+
+  footerContent = {
+    items: [
+    ]
+  } as Menu;
+
   constructor(
     public environmentService: EnvironmentService,
     public chassisService: ChassisService,
@@ -21,19 +168,8 @@ export class AppComponent implements OnInit {
   ) {}
 
   ngOnInit(): void {
-    this.chassisService.setHeaderContent(
-      {
-        links: [
-        ]
-      }
-    );
-
-    this.chassisService.setFooterContent(
-      {
-        links: [
-        ]
-      }
-    );
+    this.chassisService.setHeaderContent(this.headerContent);
+    this.chassisService.setFooterContent(this.footerContent);
 
     this.isIframe = window !== window.parent && !window.opener; // Remove this line to use Angular Universal
   };
