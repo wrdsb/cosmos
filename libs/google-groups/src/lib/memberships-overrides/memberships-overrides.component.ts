7654c4eaca3fc667df4e20019b13c8c70019b0d8
James Schumann <james@schumann.ca>
2021-04-06 09:21:54 -0400

    create memberships-overrides component

---
+++ libs/google-groups/src/lib/memberships-overrides/memberships-overrides.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'cosmos-memberships-overrides',
+  templateUrl: './memberships-overrides.component.html',
+  styleUrls: ['./memberships-overrides.component.scss']
+})
+export class MembershipsOverridesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
