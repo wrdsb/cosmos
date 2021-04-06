bb6cd4714cc6e818f326e719984a62bebffc05fd
James Schumann <james@schumann.ca>
2021-04-06 09:22:10 -0400

    create memberships-definitions component

---
+++ libs/google-groups/src/lib/memberships-definitions/memberships-definitions.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'cosmos-memberships-definitions',
+  templateUrl: './memberships-definitions.component.html',
+  styleUrls: ['./memberships-definitions.component.scss']
+})
+export class MembershipsDefinitionsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
