738c19d5ae4da38bd53bb5405e9301bbd42163df
James Schumann <james@schumann.ca>
2021-04-06 11:50:12 -0400

    create job-relay

---
+++ apps/flenderson-functions/src/job-relay/index.ts
@@ -0,0 +1,212 @@
+import { AzureFunction, Context } from "@azure/functions";
+import { FunctionInvocation, FlendersonJobType } from "@cosmos/types";
+
+const jobRelay: AzureFunction = async function (context: Context, triggerMessage: any): Promise<void> {
+    const functionInvocation = {
+        functionInvocationID: context.executionContext.invocationId,
+        functionInvocationTimestamp: new Date().toJSON(),
+        functionApp: 'Flenderson',
+        functionName: context.executionContext.functionName,
+        functionDataType: 'Job',
+        functionDataOperation: 'Relay',
+        eventLabel: ''
+    } as FunctionInvocation;
+
+    //const sgMail = require('@sendgrid/mail');
+    //sgMail.setApiKey(process.env['SENDGRID_API_KEY']);
+
+    const jobType = triggerMessage.jobType as FlendersonJobType;
+    const operation = triggerMessage.operation;
+    const payload = triggerMessage.payload;
+    const incomingBlob = triggerMessage.incomingBlob;
+    const offset = triggerMessage.offset;
+
+    let queueTriggered = '';
+    let jobTriggered = '' as FlendersonJobType;
+    let queueMessage = {};
+    let sentQueueMessage = false;
+
+    const html = '';
+    const sendNotification = false;
+
+    let logPayload = {
+        status: '',
+        message: '',
+        queueMessage: {},
+        queueTriggered: '',
+        error: '',
+        result: ''
+    };
+    const notification = {};
+
+    if (jobType) {
+        switch (jobType) {
+            case 'Flenderson.ViewIAMWP.Process':
+                queueTriggered = 'Flenderson:job-enqueue';
+                queueMessage = [
+                    {
+                        jobType: 'Flenderson.IPPSEmployeeGroup.Reconcile'
+                    },
+                    {
+                        jobType: 'Flenderson.IPPSJob.Reconcile'
+                    },
+                    {
+                        jobType: 'Flenderson.IPPSLocation.Reconcile'
+                    },
+                    {
+                        jobType: 'Flenderson.IPPSPerson.Reconcile'
+                    }
+                ];
+                context.bindings.FlendersonJobEnqueue = queueMessage;
+                sentQueueMessage = true;
+                break;
+
+            case 'Flenderson.ViewStaffDir.Process':
+                queueTriggered = 'Flenderson:job-enqueue';
+                jobTriggered = 'Flenderson.IPPSPerson.Reconcile';
+                queueMessage = {
+                    jobType: jobTriggered
+                };
+                context.bindings.FlendersonJobEnqueue = queueMessage;
+                sentQueueMessage = true;
+                break;
+
+            case 'Flenderson.IPPSEmployeeGroup.Reconcile':
+                //queueTriggered = 'Flenderson:job-enqueue';
+                //jobTriggered = 'Flenderson.IPPSEmployeeGroup.Reconcile';
+                //queueMessage = {
+                    //jobType: jobTriggered,
+                //};
+                //context.bindings.FlendersonJobEnqueue = queueMessage;
+                break;
+
+            case 'Flenderson.IPPSJob.Reconcile':
+                //queueTriggered = 'Flenderson:job-enqueue';
+                //jobTriggered = 'Flenderson.IPPSJob.Reconcile';
+                //queueMessage = {
+                    //jobType: jobTriggered,
+                //};
+                //context.bindings.FlendersonJobEnqueue = queueMessage;
+                break;
+
+            case 'Flenderson.IPPSLocation.Reconcile':
+                //queueTriggered = 'Flenderson:job-enqueue';
+                //jobTriggered = 'Flenderson.IPPSLocation.Reconcile';
+                //queueMessage = {
+                    //jobType: jobTriggered,
+                //};
+                //context.bindings.FlendersonJobEnqueue = queueMessage;
+                break;
+
+            case 'Flenderson.IPPSPerson.Reconcile':
+                //queueTriggered = 'Flenderson:job-enqueue';
+                //jobTriggered = 'Flenderson.IPPSPerson.Reconcile';
+                //queueMessage = {
+                    //jobType: jobTriggered,
+                //};
+                //context.bindings.FlendersonJobEnqueue = queueMessage;
+                break;
+
+            case 'Flenderson.IPPSEmployeeGroup.Store':
+                //queueTriggered = 'Flenderson:job-enqueue';
+                //jobTriggered = 'Flenderson.IPPSEmployeeGroup.Store';
+                //queueMessage = {
+                    //jobType: jobTriggered,
+                //};
+                //context.bindings.FlendersonJobEnqueue = queueMessage;
+                break;
+
+            case 'Flenderson.IPPSJob.Store':
+                //queueTriggered = 'Flenderson:job-enqueue';
+                //jobTriggered = 'Flenderson.IPPSJob.Store';
+                //queueMessage = {
+                    //jobType: jobTriggered,
+                //};
+                //context.bindings.FlendersonJobEnqueue = queueMessage;
+                break;
+
+            case 'Flenderson.IPPSLocation.Store':
+                //queueTriggered = 'Flenderson:job-enqueue';
+                //jobTriggered = 'Flenderson.IPPSLocation.Store';
+                //queueMessage = {
+                    //jobType: jobTriggered,
+                //};
+                //context.bindings.FlendersonJobEnqueue = queueMessage;
+                break;
+
+            case 'Flenderson.IPPSPerson.Store':
+                //queueTriggered = 'Flenderson:job-enqueue';
+                //jobTriggered = 'Flenderson.IPPSPerson.Store';
+                //queueMessage = {
+                    //jobType: jobTriggered,
+                //};
+                //context.bindings.FlendersonJobEnqueue = queueMessage;
+                break;
+
+            default:
+                context.bindings.callbackMessage = JSON.stringify({
+                    status: 422,
+                    body: "Unprocessable Entity. Cannot find the specified jobType."
+                });
+                break;
+        }
+    }
+    else {
+        context.bindings.callbackMessage = JSON.stringify({
+            status: 400,
+            body: "Please pass a valid jobType in the request body."
+        });
+    }
+
+    if (sentQueueMessage) {
+        logPayload = {
+            status: "200",
+            message: `Sent ${queueMessage} to ${queueTriggered}`,
+            queueMessage: queueMessage,
+            queueTriggered: queueTriggered,
+            result: '',
+            error: ''
+        };
+    }
+
+    //if (sendNotification) {
+        //notification = {
+            //subject: 'Poison Message Notification',
+            //to: process.env['SENDGRID_TO'],
+            //from: {
+                //email: 'errors@panama.wrdsb.io',
+                //name: 'Panama Errors'
+            //},
+            //html: html
+        //};
+        //sgMail.send(notification, (error, result) => {
+            //if (error) {
+                //logPayload = {
+                    //status: "500",
+                    //message: 'Failed to send email notification.',
+                    //queueMessage: '',
+                    //queueTriggered: '',
+                    //result: '',
+                    //error: error,
+                //};
+            //} else {
+                //logPayload = {
+                    //status: "200",
+                    //message: 'Sent email notification',
+                    //queueMessage: '',
+                    //queueTriggered: '',
+                    //result: result,
+                    //error: error
+                //};
+            //}
+        //});
+    //}
+
+    functionInvocation.logPayload = logPayload;
+
+    context.bindings.invocationPostProcessor = functionInvocation;
+    context.log(functionInvocation);
+    context.done(null, functionInvocation);
+};
+
+export default jobRelay;
