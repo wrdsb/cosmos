c4b3c06ff3d492fbb0ebc1fea490ce20591d0fc0
James Schumann <james@schumann.ca>
2021-04-09 09:37:10 -0400

    standardize scheduling functions

--- apps/igor-functions/src/schedule-group-memberships-shsm-calculate-all/index.ts
+++ apps/igor-functions/src/schedule-group-memberships-shsm-calculate-all/index.ts
@@ -1,27 +1,16 @@
 import { AzureFunction, Context } from "@azure/functions";
-import { createLogObject } from "@cosmos/azure-functions-shared";
-import { storeLogBlob } from "@cosmos/azure-functions-shared";
-import { createCallbackMessage } from "@cosmos/azure-functions-shared";
-import { createEvent } from "@cosmos/azure-functions-shared";
+import { FunctionInvocation } from "@cosmos/types";
 
 const ScheduleGroupMembershipsSHSMCalculateAll: AzureFunction = async function (context: Context, timer): Promise<void> {
-    const functionInvocationID = context.executionContext.invocationId;
-    const functionInvocationTime = new Date();
-    const functionInvocationTimestamp = functionInvocationTime.toJSON();  // format: 2012-04-23T18:25:43.511Z
-
-    const functionName = context.executionContext.functionName;
-    const functionEventType = 'WRDSB.IGOR.Schedule.Google.Group.Memberships.SHSM.Calculate.All';
-    const functionEventID = `igor-functions-${functionName}-${functionInvocationID}`;
-    const functionLogID = `${functionInvocationTime.getTime()}-${functionInvocationID}`;
-
-    const logStorageAccount = process.env['storageAccount'];
-    const logStorageKey = process.env['storageKey'];
-    const logStorageContainer = 'function-schedule-group-memberships-shsm-calculate-all-logs';
-
-    const eventLabel = '';
-    const eventTags = [
-        "igor", 
-    ];
+    const functionInvocation = {
+        functionInvocationID: context.executionContext.invocationId,
+        functionInvocationTimestamp: new Date().toJSON(),
+        functionApp: 'IGOR',
+        functionName: context.executionContext.functionName,
+        functionDataType: 'GoogleGroupMembershipsSHSM',
+        functionDataOperation: 'ScheduleCalculateAll',
+        eventLabel: ''
+    } as FunctionInvocation;
 
     const queueMessage = {
         payload: {}
@@ -33,33 +22,11 @@ const ScheduleGroupMembershipsSHSMCalculateAll: AzureFunction = async function (
         timer: context.bindings.timer,
         queueMessage: queueMessage
     };
+    functionInvocation.logPayload = logPayload;
     context.log(logPayload);
 
-    const logObject = await createLogObject(functionInvocationID, functionInvocationTime, functionName, logPayload);
-    const logBlob = await storeLogBlob(logStorageAccount, logStorageKey, logStorageContainer, logObject);
-    context.log(logBlob);
-
-    const callbackMessage = await createCallbackMessage(logObject, 200);
-    context.bindings.callbackMessage = JSON.stringify(callbackMessage);
-    context.log(callbackMessage);
-
-    const invocationEvent = await createEvent(
-        functionInvocationID,
-        functionInvocationTime,
-        functionInvocationTimestamp,
-        functionName,
-        functionEventType,
-        functionEventID,
-        functionLogID,
-        logStorageAccount,
-        logStorageContainer,
-        eventLabel,
-        eventTags
-    );
-    context.bindings.flynnEvent = JSON.stringify(invocationEvent);
-    context.log(invocationEvent);
-
-    context.done(null, logBlob);
+    context.log(functionInvocation);
+    context.done(null, functionInvocation);
 }
 
 export default ScheduleGroupMembershipsSHSMCalculateAll;
