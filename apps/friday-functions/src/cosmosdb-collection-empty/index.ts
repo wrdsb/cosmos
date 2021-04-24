4325ad42f4c8feae96e8faa41b63e2638faa3b28
James Schumann <james@schumann.ca>
2021-04-24 14:47:57 -0400

    create cosmosdb-collection-empty

---
+++ apps/friday-functions/src/cosmosdb-collection-empty/index.ts
@@ -0,0 +1,43 @@
+import { AzureFunction, Context } from "@azure/functions"
+import { FunctionInvocation,
+    FridayCosmosDBCollectionEmptyFunctionRequest,
+    FridayCosmosDBCollectionEmptyJobType,
+    FridayCosmosDBCollectionEmptyFunctionRequestPayload } from "@cosmos/types";
+
+const fridayCosmosDBCollectionEmpty: AzureFunction = async function (context: Context, triggerMessage: FridayCosmosDBCollectionEmptyFunctionRequest): Promise<void> {
+    const functionInvocation = {
+        functionInvocationID: context.executionContext.invocationId,
+        functionInvocationTimestamp: new Date().toJSON(),
+        functionApp: 'Friday',
+        functionName: context.executionContext.functionName,
+        functionDataType: 'Any',
+        functionDataOperation: 'Command',
+        eventLabel: ''
+    } as FunctionInvocation;
+
+    const triggerObject = triggerMessage as FridayCosmosDBCollectionEmptyFunctionRequest;
+    const jobType = triggerObject.jobType as FridayCosmosDBCollectionEmptyJobType;
+    const payload = triggerObject.payload as FridayCosmosDBCollectionEmptyFunctionRequestPayload;
+
+    const response = {
+        header: {
+            status: 202,
+            message: `Accepted. Enqueued jobType ${jobType}.`,
+            chatter: `Wilco. Create and process ${jobType} job.`,
+            timestamp: functionInvocation.functionInvocationTimestamp,
+        },
+        status: 202,
+        jobType: jobType,
+        payload: payload
+    };
+
+    const logPayload = response;
+    context.log(logPayload);
+
+    functionInvocation.logPayload = logPayload;
+    context.log(functionInvocation);
+
+    context.done(null, functionInvocation);
+};
+
+export default fridayCosmosDBCollectionEmpty;
