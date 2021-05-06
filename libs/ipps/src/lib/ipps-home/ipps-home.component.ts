2fe2dc833d4a89f3bc851fb8815a4632757f692b
James Schumann <james@schumann.ca>
2021-05-06 16:15:56 -0400

    rename home component for ipps

---
+++ libs/ipps/src/lib/ipps-home/ipps-home.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'cosmos-ipps-home',
+  templateUrl: './ipps-home.component.html',
+  styleUrls: ['./ipps-home.component.scss']
+})
+export class IPPSHomeComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
