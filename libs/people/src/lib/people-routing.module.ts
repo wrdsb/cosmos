0ba513b6ba068e404bf77a5ebcbe2f29d92f8ca3
James Schumann <james@schumann.ca>
2021-05-12 14:22:46 -0400

    routing and menu for people components

--- libs/people/src/lib/people-routing.module.ts
+++ libs/people/src/lib/people-routing.module.ts
@@ -3,9 +3,123 @@ import { Routes, RouterModule } from '@angular/router';
 import { MsalGuard } from '@azure/msal-angular';
 import { RolesGuard } from "@cosmos/guards";
 
-import { HomeComponent } from "./home/home.component";
+import { HomeComponent } from './home/home.component';
+
+import { PeopleBySchoolComponent } from './people-by-school/people-by-school.component';
+
+import { AllPrincipalsComponent } from './all-principals/all-principals.component';
+import { ElementaryPrincipalsComponent } from './elementary-principals/elementary-principals.component';
+import { SecondaryPrincipalsComponent } from './secondary-principals/secondary-principals.component';
+
+import { AllSchoolAdminComponent } from './all-school-admin/all-school-admin.component';
+import { ElementarySchoolAdminComponent } from './elementary-school-admin/elementary-school-admin.component';
+import { SecondarySchoolAdminComponent } from './secondary-school-admin/secondary-school-admin.component';
+
+import { AllFirstSecretariesComponent } from './all-first-secretaries/all-first-secretaries.component';
+import { ElementaryFirstSecretariesComponent } from './elementary-first-secretaries/elementary-first-secretaries.component';
+import { SecondaryFirstSecretariesComponent } from './secondary-first-secretaries/secondary-first-secretaries.component';
 
 const routes: Routes = [
+  { 
+    path: 'people-by-school',
+    component: PeopleBySchoolComponent,
+    data: {
+      roles: ['cosmos-superuser', 'cosmos-user-its']
+    },
+    canActivate: [
+      MsalGuard, RolesGuard
+    ]
+  },
+  { 
+    path: 'all-principals',
+    component: AllPrincipalsComponent,
+    data: {
+      roles: ['cosmos-superuser', 'cosmos-user-its']
+    },
+    canActivate: [
+      MsalGuard, RolesGuard
+    ]
+  },
+  { 
+    path: 'elementary-principals',
+    component: ElementaryPrincipalsComponent,
+    data: {
+      roles: ['cosmos-superuser', 'cosmos-user-its']
+    },
+    canActivate: [
+      MsalGuard, RolesGuard
+    ]
+  },
+  { 
+    path: 'secondary-principals',
+    component: SecondaryPrincipalsComponent,
+    data: {
+      roles: ['cosmos-superuser', 'cosmos-user-its']
+    },
+    canActivate: [
+      MsalGuard, RolesGuard
+    ]
+  },
+  { 
+    path: 'all-school-admin',
+    component: AllSchoolAdminComponent,
+    data: {
+      roles: ['cosmos-superuser', 'cosmos-user-its']
+    },
+    canActivate: [
+      MsalGuard, RolesGuard
+    ]
+  },
+  { 
+    path: 'elementary-school-admin',
+    component: ElementarySchoolAdminComponent,
+    data: {
+      roles: ['cosmos-superuser', 'cosmos-user-its']
+    },
+    canActivate: [
+      MsalGuard, RolesGuard
+    ]
+  },
+  { 
+    path: 'secondary-school-admin',
+    component: SecondarySchoolAdminComponent,
+    data: {
+      roles: ['cosmos-superuser', 'cosmos-user-its']
+    },
+    canActivate: [
+      MsalGuard, RolesGuard
+    ]
+  },
+  { 
+    path: 'all-first-secretaries',
+    component: AllFirstSecretariesComponent,
+    data: {
+      roles: ['cosmos-superuser', 'cosmos-user-its']
+    },
+    canActivate: [
+      MsalGuard, RolesGuard
+    ]
+  },
+  { 
+    path: 'elementary-first-secretaries',
+    component: ElementaryFirstSecretariesComponent,
+    data: {
+      roles: ['cosmos-superuser', 'cosmos-user-its']
+    },
+    canActivate: [
+      MsalGuard, RolesGuard
+    ]
+  },
+  { 
+    path: 'secondary-first-secretaries',
+    component: SecondaryFirstSecretariesComponent,
+    data: {
+      roles: ['cosmos-superuser', 'cosmos-user-its']
+    },
+    canActivate: [
+      MsalGuard, RolesGuard
+    ]
+  },
   { 
     path: '',
     component: HomeComponent,
--- libs/people/src/lib/people.module.ts
+++ libs/people/src/lib/people.module.ts
@@ -5,13 +5,19 @@ import { HTTP_INTERCEPTORS } from '@angular/common/http';
 import { MsalInterceptor } from '@azure/msal-angular';
 
 import { PeopleRoutingModule } from "./people-routing.module";
+
 import { HomeComponent } from './home/home.component';
+
+import { PeopleBySchoolComponent } from './people-by-school/people-by-school.component';
+
 import { AllPrincipalsComponent } from './all-principals/all-principals.component';
 import { ElementaryPrincipalsComponent } from './elementary-principals/elementary-principals.component';
 import { SecondaryPrincipalsComponent } from './secondary-principals/secondary-principals.component';
+
 import { AllSchoolAdminComponent } from './all-school-admin/all-school-admin.component';
 import { ElementarySchoolAdminComponent } from './elementary-school-admin/elementary-school-admin.component';
 import { SecondarySchoolAdminComponent } from './secondary-school-admin/secondary-school-admin.component';
+
 import { AllFirstSecretariesComponent } from './all-first-secretaries/all-first-secretaries.component';
 import { ElementaryFirstSecretariesComponent } from './elementary-first-secretaries/elementary-first-secretaries.component';
 import { SecondaryFirstSecretariesComponent } from './secondary-first-secretaries/secondary-first-secretaries.component';
@@ -19,22 +25,41 @@ import { SecondaryFirstSecretariesComponent } from './secondary-first-secretarie
 @NgModule({
   declarations: [
     HomeComponent,
+
+    PeopleBySchoolComponent,
+
     AllPrincipalsComponent,
     ElementaryPrincipalsComponent,
     SecondaryPrincipalsComponent,
+
     AllSchoolAdminComponent,
     ElementarySchoolAdminComponent,
     SecondarySchoolAdminComponent,
+
     AllFirstSecretariesComponent,
     ElementaryFirstSecretariesComponent,
-    SecondaryFirstSecretariesComponent
+    SecondaryFirstSecretariesComponent,
   ],
   imports: [
     CommonModule,
     PeopleRoutingModule
   ],
   exports: [
-    HomeComponent
+    HomeComponent,
+
+    PeopleBySchoolComponent,
+
+    AllPrincipalsComponent,
+    ElementaryPrincipalsComponent,
+    SecondaryPrincipalsComponent,
+
+    AllSchoolAdminComponent,
+    ElementarySchoolAdminComponent,
+    SecondarySchoolAdminComponent,
+
+    AllFirstSecretariesComponent,
+    ElementaryFirstSecretariesComponent,
+    SecondaryFirstSecretariesComponent
   ],
   providers: [
     { provide: HTTP_INTERCEPTORS, useClass: MsalInterceptor, multi: true }
