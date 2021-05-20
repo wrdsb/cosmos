a2d328e97476023d4ed972c9211836bcecd60a0d
James Schumann <james@schumann.ca>
2021-05-20 10:49:11 -0400

    generate people and schools components inside screens-shared-codex

--- libs/screens/shared/codex/src/lib/screens-shared-codex.module.ts
+++ libs/screens/shared/codex/src/lib/screens-shared-codex.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { PeopleComponent } from './people/people.component';
+import { SchoolsComponent } from './schools/schools.component';
 
 @NgModule({
   imports: [CommonModule],
+  declarations: [PeopleComponent, SchoolsComponent],
 })
 export class ScreensSharedCodexModule {}
