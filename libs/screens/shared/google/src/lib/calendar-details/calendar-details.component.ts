25ecbb1608b3973e3b06b3dc11c339f28398d914
James Schumann <james@schumann.ca>
2021-05-20 11:02:52 -0400

    generate shared google detail components

---
+++ libs/screens/shared/google/src/lib/calendar-details/calendar-details.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'cosmos-calendar-details',
+  templateUrl: './calendar-details.component.html',
+  styleUrls: ['./calendar-details.component.scss']
+})
+export class CalendarDetailsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- libs/screens/shared/google/src/lib/screens-shared-google.module.ts
+++ libs/screens/shared/google/src/lib/screens-shared-google.module.ts
@@ -8,6 +8,8 @@ import { GroupsSearchComponent } from './groups-search/groups-search.component';
 import { CalendarHomeComponent } from './calendar-home/calendar-home.component';
 import { CalendarDashboardComponent } from './calendar-dashboard/calendar-dashboard.component';
 import { CalendarSearchComponent } from './calendar-search/calendar-search.component';
+import { CalendarDetailsComponent } from './calendar-details/calendar-details.component';
+import { GroupDetailsComponent } from './group-details/group-details.component';
 
 @NgModule({
   imports: [CommonModule],
@@ -18,7 +20,9 @@ import { CalendarSearchComponent } from './calendar-search/calendar-search.compo
     
     CalendarHomeComponent,
     CalendarDashboardComponent,
-    CalendarSearchComponent
+    CalendarSearchComponent,
+    CalendarDetailsComponent,
+    GroupDetailsComponent
   ],
 })
 export class ScreensSharedGoogleModule {}
