627ff00c764c355a5477998c314a34619510483d
James Schumann <james@schumann.ca>
2021-05-20 11:04:35 -0400

    generate shared codex detail components

---
+++ libs/screens/shared/codex/src/lib/school-details/school-details.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'cosmos-school-details',
+  templateUrl: './school-details.component.html',
+  styleUrls: ['./school-details.component.scss']
+})
+export class SchoolDetailsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- libs/screens/shared/codex/src/lib/screens-shared-codex.module.ts
+++ libs/screens/shared/codex/src/lib/screens-shared-codex.module.ts
@@ -7,9 +7,11 @@ import { PeopleDashboardComponent } from './people-dashboard/people-dashboard.co
 import { SchoolsDashboardComponent } from './schools-dashboard/schools-dashboard.component';
 import { PeopleSearchComponent } from './people-search/people-search.component';
 import { SchoolsSearchComponent } from './schools-search/schools-search.component';
+import { PersonDetailsComponent } from './person-details/person-details.component';
+import { SchoolDetailsComponent } from './school-details/school-details.component';
 
 @NgModule({
   imports: [CommonModule],
-  declarations: [PeopleHomeComponent, SchoolsHomeComponent, PeopleDashboardComponent, SchoolsDashboardComponent, PeopleSearchComponent, SchoolsSearchComponent],
+  declarations: [PeopleHomeComponent, SchoolsHomeComponent, PeopleDashboardComponent, SchoolsDashboardComponent, PeopleSearchComponent, SchoolsSearchComponent, PersonDetailsComponent, SchoolDetailsComponent],
 })
 export class ScreensSharedCodexModule {}
