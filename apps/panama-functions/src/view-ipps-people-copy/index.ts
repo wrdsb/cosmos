442832940f61222d02cae605ab61b6eb64201528
James Schumann <james@schumann.ca>
2021-06-04 12:08:26 -0400

    create view-ipps-people-copy

---
+++ apps/panama-functions/src/view-ipps-people-copy/index.ts
@@ -0,0 +1,46 @@
+import { AzureFunction, Context, HttpRequest } from "@azure/functions";
+import { FunctionInvocation } from "@cosmos/types";
+
+const viewIAMWPCopy: AzureFunction = async function (context: Context, triggerMessage: any): Promise<void> {
+    const functionInvocation = {
+        functionInvocationID: context.executionContext.invocationId,
+        functionInvocationTimestamp: new Date().toJSON(),
+        functionApp: 'Panama',
+        functionName: context.executionContext.functionName,
+        functionDataType: 'ViewIPPSPeople',
+        functionDataOperation: 'Copy',
+        eventLabel: ''
+    } as FunctionInvocation;
+
+    const jobType = 'WRDSB.Panama.View.IPPSPeople.Copy';
+    let statusCode;
+    let statusMessage;
+
+    const incomingBlob = context.bindings.incomingBlob;
+
+    if (incomingBlob.length < 5000) {
+        statusCode = "404"
+        statusMessage = "Error: Too few source records. Aborting.";
+    } else {
+        statusCode = "200";
+        statusMessage = "Success: Copied ipps-view-ipps-people/incoming.json to ipps-view-ipps-people/now.json.";
+
+        // Copy blob contents
+        context.bindings.outgoingBlob = incomingBlob;
+    }
+
+    const logPayload = {
+        status: statusCode,
+        message: statusMessage,
+        incomingBlob: "ipps-view-ipps-people/incoming.json",
+        outgoingBlob: "ipps-view-ipps-people/now.json"
+    };
+    functionInvocation.logPayload = logPayload;
+
+    context.bindings.jobRelay = {jobType: jobType};
+    context.bindings.invocationPostProcessor = functionInvocation;
+    context.log(functionInvocation);
+    context.done(null, functionInvocation);
+};
+
+export default viewIAMWPCopy;
