205ea71b454f387e6fcd711ce1c6024d889257de
James Schumann <james@schumann.ca>
2021-06-07 14:07:51 -0400

    create ipps-position in place of ipps-assignment

---
+++ libs/types/src/lib/entities/ipps/ipps-position.ts
@@ -0,0 +1,30 @@
+import { CosmosCoreFields } from "@cosmos/types";
+
+interface IPPSPosition extends CosmosCoreFields {
+    // Fields from CosmosCoreFields
+    //createdAt?: DateTime;
+    //updatedAt?: DateTime;
+    //deletedAt?: DateTime;
+    //deleted?: boolean;
+
+    //createdBy?: PersonID;
+    //updatedBy?: PersonID;
+    //deletedBy?: PersonID;
+
+    //id?: string;
+    
+    positionID: string;
+    //employeeID: string;
+
+    employeeGroupCode: string;
+    jobCode: string;
+    locationCode: string;
+
+    establishmentCode: string;
+    isHomeLocation: string;
+
+    positionStartDate: string;
+    positionEndDate: string;
+}
+
+export { IPPSPosition };
