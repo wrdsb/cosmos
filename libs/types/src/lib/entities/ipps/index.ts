ad7c3a64ca10a27b7b98c2f3289bcef65ef0f3e8
James Schumann <james@schumann.ca>
2021-06-07 14:11:44 -0400

    export new ipps types

--- libs/types/src/lib/entities/ipps/index.ts
+++ libs/types/src/lib/entities/ipps/index.ts
@@ -2,6 +2,9 @@ export * from './ipps-employee-group';
 export * from './ipps-job';
 export * from './ipps-location';
 export * from './ipps-person';
+export * from './ipps-position';
 
-export * from './ipps-assignment';
+export * from './ipps-materialized-person';
+export * from './ipps-materialized-position';
+
 export * from './ipps-record';
