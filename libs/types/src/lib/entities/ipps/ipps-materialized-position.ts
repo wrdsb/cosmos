dbc5d286808f9d7a01a4c548c82f7c1332ce754a
James Schumann <james@schumann.ca>
2021-06-07 14:08:42 -0400

    create ipps-materialized-position to hold all position data

---
+++ libs/types/src/lib/entities/ipps/ipps-materialized-position.ts
@@ -0,0 +1,42 @@
+import { CosmosCoreFields } from "@cosmos/types";
+
+interface IPPSMaterializedPosition extends CosmosCoreFields {
+    // Fields from CosmosCoreFields
+    //createdAt?: DateTime;
+    //updatedAt?: DateTime;
+    //deletedAt?: DateTime;
+    //deleted?: boolean;
+
+    //createdBy?: PersonID;
+    //updatedBy?: PersonID;
+    //deletedBy?: PersonID;
+
+    //id?: string;
+    positionID: string;
+    //employeeID: string;
+
+    employeeGroupCode: string;
+    employeeGroupCategory: string;
+    employeeGroupDescription: string;
+    employeeGroupAbbreviation: string;
+
+    jobCode: string;
+    jobDescription: string;
+    jobAbbreviation: string;
+
+    locationCode: string;
+    LocationType: string;
+    locationDescription: string;
+    locationAbbreviation: string;
+    schoolCode: string;
+    panel: string;
+
+    establishmentCode: string;
+
+    isHomeLocation: string;
+
+    positionStartDate: string;
+    positionEndDate: string;
+}
+
+export { IPPSMaterializedPosition };
