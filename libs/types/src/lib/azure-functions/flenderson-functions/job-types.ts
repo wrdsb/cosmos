08ced1f9b1808e8663c29ff0addd7611f228be0a
James Schumann <james@schumann.ca>
2021-06-08 10:55:09 -0400

    fix namespacing of jobTypes

--- libs/types/src/lib/azure-functions/flenderson-functions/job-types.ts
+++ libs/types/src/lib/azure-functions/flenderson-functions/job-types.ts
@@ -1,20 +1,20 @@
 export type FlendersonJobType =
-'WRDSB.Flenderson.ViewIAMWP.Process' |
-'WRDSB.Flenderson.ViewStaffDir.Process' |
+'WRDSB.Flenderson.View.IAMWP.Process' |
+'WRDSB.Flenderson.View.StaffDir.Process' |
 'WRDSB.Flenderson.View.IPPSPositions.Process' |
 'WRDSB.Flenderson.View.IPPSPeople.Process' |
 'WRDSB.Flenderson.View.IPPSLocations.Process' |
 'WRDSB.Flenderson.View.IPPSJobs.Process' |
 'WRDSB.Flenderson.View.IPPSGroups.Process' |
 
-'Flenderson.IPPSEmployeeGroup.Store' |
-'Flenderson.IPPSJob.Store' |
-'Flenderson.IPPSLocation.Store' |
-'Flenderson.IPPSPerson.Store' |
+'WRDSB.Flenderson.IPPSEmployeeGroup.Store' |
+'WRDSB.Flenderson.IPPSJob.Store' |
+'WRDSB.Flenderson.IPPSLocation.Store' |
+'WRDSB.Flenderson.IPPSPerson.Store' |
 
-'Flenderson.IPPSEmployeeGroup.Reconcile' |
-'Flenderson.IPPSJob.Reconcile' |
-'Flenderson.IPPSLocation.Reconcile' |
-'Flenderson.IPPSPerson.Reconcile' |
+'WRDSB.Flenderson.IPPSEmployeeGroup.Reconcile' |
+'WRDSB.Flenderson.IPPSJob.Reconcile' |
+'WRDSB.Flenderson.IPPSLocation.Reconcile' |
+'WRDSB.Flenderson.IPPSPerson.Reconcile' |
 
-'Flenderson.IPPSEmployeeGroup.ChangeParse'
+'WRDSB.Flenderson.IPPSEmployeeGroup.ChangeParse'
