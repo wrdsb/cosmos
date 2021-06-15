0ad522a3ec8d33127b102c25e9dea95f7331aefd
James Schumann <james@schumann.ca>
2021-06-15 15:23:22 -0400

    create flenderson-person-change-trigger

---
+++ apps/flenderson-functions/src/flenderson-person-change-trigger/index.ts
@@ -0,0 +1,27 @@
+import { AzureFunction, Context } from "@azure/functions";
+import { FunctionInvocation, FlendersonJobType, FlendersonPerson } from "@cosmos/types";
+
+const flendersonPersonChangeTrigger: AzureFunction = async function (context: Context, changedRecords: FlendersonPerson[]): Promise<void> {
+    const functionInvocation = {
+        functionInvocationID: context.executionContext.invocationId,
+        functionInvocationTimestamp: new Date().toJSON(),
+        functionApp: 'Flenderson',
+        functionName: context.executionContext.functionName,
+        functionDataType: 'FlendersonPerson',
+        functionDataOperation: 'ChangeTrigger',
+        eventLabel: ''
+    } as FunctionInvocation;
+
+    const jobType: FlendersonJobType = 'WRDSB.Flenderson.FlendersonPerson.ChangeTrigger';
+    functionInvocation.jobType = jobType;
+
+    const logPayload = changedRecords;
+    functionInvocation.logPayload = logPayload;
+
+    context.bindings.jobRelay = {jobType: jobType};
+    context.bindings.invocationPostProcessor = functionInvocation;
+    context.log(functionInvocation);
+    context.done(null, functionInvocation);
+};
+
+export default flendersonPersonChangeTrigger;
