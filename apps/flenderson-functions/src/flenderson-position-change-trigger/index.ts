585adcfffb15da6530a21c298d441374631088d4
James Schumann <james@schumann.ca>
2021-06-15 15:23:51 -0400

    create flenderson-position-change-trigger

---
+++ apps/flenderson-functions/src/flenderson-position-change-trigger/index.ts
@@ -0,0 +1,27 @@
+import { AzureFunction, Context } from "@azure/functions";
+import { FunctionInvocation, FlendersonJobType, FlendersonPosition } from "@cosmos/types";
+
+const flendersonPositionChangeTrigger: AzureFunction = async function (context: Context, changedRecords: FlendersonPosition[]): Promise<void> {
+    const functionInvocation = {
+        functionInvocationID: context.executionContext.invocationId,
+        functionInvocationTimestamp: new Date().toJSON(),
+        functionApp: 'Flenderson',
+        functionName: context.executionContext.functionName,
+        functionDataType: 'FlendersonPosition',
+        functionDataOperation: 'ChangeTrigger',
+        eventLabel: ''
+    } as FunctionInvocation;
+
+    const jobType: FlendersonJobType = 'WRDSB.Flenderson.FlendersonPosition.ChangeTrigger';
+    functionInvocation.jobType = jobType;
+
+    const logPayload = changedRecords;
+    functionInvocation.logPayload = logPayload;
+
+    context.bindings.jobRelay = {jobType: jobType};
+    context.bindings.invocationPostProcessor = functionInvocation;
+    context.log(functionInvocation);
+    context.done(null, functionInvocation);
+};
+
+export default flendersonPositionChangeTrigger;
