a52146bdfdba478f58169bd6e5f20c3be316a374
James Schumann <james@schumann.ca>
2021-06-15 15:23:08 -0400

    create flenderson-person-change-parse

---
+++ apps/flenderson-functions/src/flenderson-person-change-parse/index.ts
@@ -0,0 +1,78 @@
+import { AzureFunction, Context } from "@azure/functions";
+import { FunctionInvocation, FlendersonJobType, FlendersonPerson } from "@cosmos/types";
+
+const flendersonPersonChangeParse: AzureFunction = async function (context: Context, triggerMessage: any): Promise<void> {
+    const functionInvocation = {
+        functionInvocationID: context.executionContext.invocationId,
+        functionInvocationTimestamp: new Date().toJSON(),
+        functionApp: 'Flenderson',
+        functionName: context.executionContext.functionName,
+        functionDataType: 'FlendersonPerson',
+        functionDataOperation: 'ChangeParse',
+        eventLabel: ''
+    } as FunctionInvocation;
+
+    const jobType: FlendersonJobType = 'WRDSB.Flenderson.FlendersonPerson.ChangeParse';
+    functionInvocation.jobType = jobType;
+
+    const triggerObject = triggerMessage;
+    const payload = triggerObject.payload;
+
+    const oldRecord = payload.oldRecord as FlendersonPerson;
+    const newRecord = payload.newRecord as FlendersonPerson;
+
+    const events = [];
+
+    if (!oldRecord) {
+        const eventType = 'WRDSB.Flenderson.FlendersonPerson.Create';
+        const label = `Person ${newRecord.id} created.`;
+
+        const event = craftEvent(eventType, label, newRecord, oldRecord);
+        events.push(event);
+    }
+
+    if (!oldRecord.deleted && newRecord.deleted) {
+        const eventType = 'WRDSB.Flenderson.FlendersonPerson.Delete';
+        const label = `Person ${newRecord.id} deleted.`;
+
+        const event = craftEvent(eventType, label, newRecord, oldRecord);
+        events.push(event);
+    }
+
+    if (!newRecord.deleted && oldRecord.deleted) {
+        const eventType = 'WRDSB.Flenderson.FlendersonPerson.Undelete';
+        const label = `Person ${newRecord.id} undeleted.`;
+
+        const event = craftEvent(eventType, label, newRecord, oldRecord);
+        events.push(event);
+    }
+
+    //if (oldRecord.jobCode !== newRecord.jobCode) {
+        //const eventType = 'WRDSB.Flenderson.FlendersonPerson.JobCode.Change';
+        //const label = `Job ${newRecord.id} code changed.`;
+
+        //const event = craftEvent(eventType, label, newRecord, oldRecord);
+        //events.push(event);
+    //}
+
+    const logPayload = events;
+    functionInvocation.logPayload = logPayload;
+
+    context.bindings.invocationPostProcessor = functionInvocation;
+    context.log(functionInvocation);
+    context.done(null, functionInvocation);
+
+
+    function craftEvent(eventType, label, newRecord, oldRecord) {
+        const event = {
+            eventType: eventType,
+            label: label,
+            newRecord: newRecord,
+            oldRecord: oldRecord
+        }
+
+        return event;
+    }
+};
+
+export default flendersonPersonChangeParse;
