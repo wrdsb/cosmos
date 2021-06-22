9eb693d1ce937eeb887be1e2d4fd18227ee74efe
James Schumann <james@schumann.ca>
2021-06-22 09:04:44 -0400

    stub out some flenderson events

---
+++ libs/types/src/lib/events/flenderson.ts
@@ -0,0 +1,14 @@
+export type WRDSBFlendersonEventSubject =
+'/wrdsb/flenderson/ipps-person/create' |
+'/wrdsb/flenderson/ipps-person/update' |
+'/wrdsb/flenderson/ipps-person/delete';
+
+export type WRDSBFlendersonEventType =
+'Flenderson.IPPSPerson.Create' |
+'Flenderson.IPPSPerson.Update' |
+'Flenderson.IPPSPerson.Delete';
+
+export type WRDSBFlendersonEventTag =
+'flenderson' |
+'ipps_person_change' |
+'person_change';
