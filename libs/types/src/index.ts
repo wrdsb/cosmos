216c3061983f78fd6047a5783655f022ee19db02
James Schumann <james@schumann.ca>
2021-06-22 09:07:55 -0400

    start exporting event types

--- libs/types/src/index.ts
+++ libs/types/src/index.ts
@@ -2,6 +2,7 @@ export * from './lib/types';
 export * from './lib/signalr-connection.model';
 
 export * from './lib/entities';
+export * from './lib/events';
 export * from './lib/azure-functions';
 
 export * from './lib/ui';
