2102c659fe1e9b7b19fcc3caff5a76f7a31be3d6
James Schumann <james@schumann.ca>
2021-06-22 09:04:30 -0400

    create common event types based on Event Grid

--- libs/types/src/lib/events/common.ts
+++ libs/types/src/lib/events/common.ts
@@ -1,34 +1,59 @@
-type DomainEventSpecVersion = '0.3';
-type DomainEventDataContentType = 'application/json';
+import { WRDSBFlendersonEventType, WRDSBFlendersonEventTag } from "@cosmos/types"
+import { FunctionInvocation } from "@cosmos/types";
+import { WRDSBFlendersonEventSubject } from "./flenderson";
 
-type DomainEventType = string;
-type DomainEventTypeURL = string;
-type DomainEventTypeVersion = string;
+export type WRDSBEventSubject =
+    WRDSBFlendersonEventSubject;
 
-type DomainEventID = string;
-type DomainEventTimestamp = string;
-type DomainEventSource = string;
+export type WRDSBEventType =
+    WRDSBFlendersonEventType;
 
-type DomainEventLabel = string;
-type DomainEventTags = string[];
+export type WRDSBEventTag =
+    WRDSBFlendersonEventTag;
 
-interface DomainEvent {
-    specversion: DomainEventSpecVersion;
-    datacontenttype: DomainEventDataContentType;
+// "2017-06-26T18:41:00.9584103Z"
+export type UTCDateTime = string;
 
-    type: DomainEventType,
-    typeURL: DomainEventTypeURL,
-    typeVersion : DomainEventTypeVersion,
+export type WRDSBEventID = string;
+    
+export type WRDSBEventSchemaVersion = "1.0";
+export type EventGridMetadataVersion = "1";
 
-    id : DomainEventID,
-    time : DomainEventTimestamp,
-    source : DomainEventSource,
-
-    label: DomainEventLabel;
-    tags: DomainEventTags;
-
-    payload: DomainEventPayload;
+export interface WRDSBEvent {
+    // topic: string; // provided by Event Grid
+    subject: WRDSBEventSubject;
+    eventType: WRDSBEventType;
+    eventTime: UTCDateTime;
+    id: WRDSBEventID;
+    data: WRDSBEventData;
+    dataVersion: WRDSBEventSchemaVersion;
+    // metadataVersion: EventGridMetadataVersion; // provided by Event Grid
 }
 
-interface DomainEventPayload {
-}    
+export interface WRDSBEventData {
+    // api": "PutBlockList",
+    // clientRequestId": "6d79dbfb-0e37-4fc4-981f-442c9ca65760",
+    // requestId": "831e1650-001e-001b-66ab-eeb76e000000",
+    // eTag": "0x8D4BCC2E4835CD0",
+    // contentType": "application/octet-stream",
+    // contentLength": 524288,
+    // blobType": "BlockBlob",
+    // url": "https://oc2d2817345i60006.blob.core.windows.net/oc2d2817345i200097container/oc2d2817345i20002296blob",
+    // source: `/flenderson/ipps-person/${recordID}/${source}`,
+    // schemaURL?: `ca.wrdsb.flenderson.ipps-person.${schema}.json`,
+    // label?: `${oldRecord.email}'s IPPS record deleted.`;
+
+    functionInvocation?: FunctionInvocation,
+
+    api?: string;
+    clientRequestId?: string;
+    requestId?: string;
+    eTag?: string;
+    contentType: "application/json";
+    contentLength?: string;
+    blobType?: string;
+    url?: string;
+    source?: string;
+    label?: string;
+    tags?: WRDSBEventTag[];
+}
