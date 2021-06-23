b7e70abc338f1c11fa7dd826284c9c9f7e3f5ca6
James Schumann <james@schumann.ca>
2021-06-23 15:32:05 -0400

    helps if we export the shared functions

--- libs/flenderson-functions-shared/src/index.ts
+++ libs/flenderson-functions-shared/src/index.ts
@@ -4,3 +4,6 @@ export * from './lib/craft-store-events';
 export * from './lib/make-hash-ipps-person';
 
 export * from './lib/store-ops-calcs';
+
+export * from './lib/validate-flenderson-command';
+export * from './lib/generate-response-object';
