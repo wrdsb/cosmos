e6b5c85b363d12aca52042e9ba06f2d58dfc66eb
James Schumann <james@schumann.ca>
2021-06-25 11:15:53 -0400

    add remaining ipps- events

--- libs/types/src/lib/events/flenderson.ts
+++ libs/types/src/lib/events/flenderson.ts
@@ -1,4 +1,24 @@
 export type WRDSBFlendersonEventSubject =
+'/wrdsb/flenderson/ipps-directory/create' |
+'/wrdsb/flenderson/ipps-directory/update' |
+'/wrdsb/flenderson/ipps-directory/delete' |
+
+'/wrdsb/flenderson/ipps-employee-group/create' |
+'/wrdsb/flenderson/ipps-employee-group/update' |
+'/wrdsb/flenderson/ipps-employee-group/delete' |
+
+'/wrdsb/flenderson/ipps-job/create' |
+'/wrdsb/flenderson/ipps-job/update' |
+'/wrdsb/flenderson/ipps-job/delete' |
+
+'/wrdsb/flenderson/ipps-location/create' |
+'/wrdsb/flenderson/ipps-location/update' |
+'/wrdsb/flenderson/ipps-location/delete' |
+
+'/wrdsb/flenderson/ipps-pal/create' |
+'/wrdsb/flenderson/ipps-pal/update' |
+'/wrdsb/flenderson/ipps-pal/delete' |
+
 '/wrdsb/flenderson/ipps-person/create' |
 '/wrdsb/flenderson/ipps-person/update' |
 '/wrdsb/flenderson/ipps-person/delete' |
@@ -9,6 +29,26 @@ export type WRDSBFlendersonEventSubject =
 
 
 export type WRDSBFlendersonEventType =
+'Flenderson.IPPSDirectory.Create' |
+'Flenderson.IPPSDirectory.Update' |
+'Flenderson.IPPSDirectory.Delete' |
+
+'Flenderson.IPPSEmployeeGroup.Create' |
+'Flenderson.IPPSEmployeeGroup.Update' |
+'Flenderson.IPPSEmployeeGroup.Delete' |
+
+'Flenderson.IPPSJob.Create' |
+'Flenderson.IPPSJob.Update' |
+'Flenderson.IPPSJob.Delete' |
+
+'Flenderson.IPPSLocation.Create' |
+'Flenderson.IPPSLocation.Update' |
+'Flenderson.IPPSLocation.Delete' |
+
+'Flenderson.IPPSPal.Create' |
+'Flenderson.IPPSPal.Update' |
+'Flenderson.IPPSPal.Delete' |
+
 'Flenderson.IPPSPerson.Create' |
 'Flenderson.IPPSPerson.Update' |
 'Flenderson.IPPSPerson.Delete' |
@@ -20,8 +60,14 @@ export type WRDSBFlendersonEventType =
 export type WRDSBFlendersonEventTag =
 'flenderson' |
 
+'ipps_directory_change' |
+'ipps_employee_group_change' |
+'ipps_job_change' |
+'ipps_location_change' |
+'ipps_pal_change' |
 'ipps_person_change' |
-'person_change' |
-
 'ipps_position_change' |
+
+'pal_change' |
+'person_change' |
 'position_change'
