0a7d3ea17445892a64bff2cf0fabde3cfa8acbb8
James Schumann <james@schumann.ca>
2021-06-25 11:07:06 -0400

    start cataloging flenderson event types

--- libs/types/src/lib/events/flenderson.ts
+++ libs/types/src/lib/events/flenderson.ts
@@ -1,14 +1,27 @@
 export type WRDSBFlendersonEventSubject =
 '/wrdsb/flenderson/ipps-person/create' |
 '/wrdsb/flenderson/ipps-person/update' |
-'/wrdsb/flenderson/ipps-person/delete';
+'/wrdsb/flenderson/ipps-person/delete' |
+
+'/wrdsb/flenderson/ipps-position/create' |
+'/wrdsb/flenderson/ipps-position/update' |
+'/wrdsb/flenderson/ipps-position/delete';
+
 
 export type WRDSBFlendersonEventType =
 'Flenderson.IPPSPerson.Create' |
 'Flenderson.IPPSPerson.Update' |
-'Flenderson.IPPSPerson.Delete';
+'Flenderson.IPPSPerson.Delete' |
+
+'Flenderson.IPPSPosition.Create' |
+'Flenderson.IPPSPosition.Update' |
+'Flenderson.IPPSPosition.Delete';
 
 export type WRDSBFlendersonEventTag =
 'flenderson' |
+
 'ipps_person_change' |
-'person_change';
+'person_change' |
+
+'ipps_position_change' |
+'position_change'
