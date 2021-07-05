e410906ae8608e0ee0d17ae4491f51337ff49818
James Schumann <james@schumann.ca>
2021-07-05 13:31:57 -0400

    refactor for new event crafting functions

--- apps/flenderson-functions/src/flenderson-person-store/index.ts
+++ apps/flenderson-functions/src/flenderson-person-store/index.ts
@@ -1,11 +1,13 @@
 import { AzureFunction, Context } from "@azure/functions";
-import { createHash } from "crypto";
-import { FunctionInvocation, FlendersonJobType, FlendersonPersonStoreFunctionRequest, StoreFunctionOperation, FlendersonPersonStoreFunctionRequestPayload, FlendersonPerson } from "@cosmos/types";
+import { UTCDateTime, FunctionInvocation, FlendersonJobType, FlendersonPersonStoreFunctionRequest, StoreFunctionOperation, FlendersonPersonStoreFunctionRequestPayload, FlendersonPerson } from "@cosmos/types";
+import { CalcArgs, CalcResult } from "@cosmos/flenderson-functions-shared";
+import { calcPatch, calcReplace, calcDelete, makeHashFlendersonPerson } from "@cosmos/flenderson-functions-shared";
+import { craftCreateEvent, craftUpdateEvent, craftDeleteEvent } from "@cosmos/flenderson-functions-shared";
 
 const flendersonPositionStore: AzureFunction = async function (context: Context, triggerMessage: FlendersonPersonStoreFunctionRequest): Promise<void> {
     const functionInvocation = {
         functionInvocationID: context.executionContext.invocationId,
-        functionInvocationTimestamp: new Date().toJSON(),
+        functionInvocationTimestamp: new Date().toJSON() as UTCDateTime,
         functionApp: 'Flenderson',
         functionName: context.executionContext.functionName,
         functionDataType: 'FlendersonPerson',
@@ -18,7 +20,7 @@ const flendersonPositionStore: AzureFunction = async function (context: Context,
     
     const triggerObject = triggerMessage as FlendersonPersonStoreFunctionRequest;
     const operation = triggerObject.operation as StoreFunctionOperation;
-    const payload = triggerObject.payload as FlendersonPersonStoreFunctionRequestPayload;
+    const payload = triggerObject.payload as FlendersonPerson;
 
     const oldRecord = context.bindings.recordIn;
 
@@ -58,23 +60,30 @@ const flendersonPositionStore: AzureFunction = async function (context: Context,
         positions:               []
     } as FlendersonPerson;
 
-    let result;
+    const calcArgs: CalcArgs<FlendersonPerson> = {
+        oldRecord: oldRecord,
+        newRecord: newRecord,
+        payload: payload,
+        functionInvocation: functionInvocation
+    };
+
+    let result: CalcResult<FlendersonPerson>;
     let statusCode;
     let statusMessage;
 
     switch (operation) {
         case 'delete':
-            result = doDelete(oldRecord, newRecord, payload);
+            result = calcDelete<FlendersonPerson>(calcArgs);
             statusCode = '200';
             statusMessage = 'Success: Marked record deleted.';
             break;
         case 'patch':
-            result = doPatch(oldRecord, newRecord, payload);
+            result = calcPatch<FlendersonPerson>(calcArgs);
             statusCode = '200';
             statusMessage = 'Success: Patched record.';
             break;
         case 'replace':
-            result = doReplace(oldRecord, newRecord, payload);
+            result = calcReplace<FlendersonPerson>(calcArgs);
             statusCode = '200';
             statusMessage = 'Success: Replaced record.';
             break;
@@ -82,17 +91,39 @@ const flendersonPositionStore: AzureFunction = async function (context: Context,
             break;
     }
 
-    if (result.changedDetected) {
-        context.bindings.recordOut = result.newRecord;
+    const calcRecord = result.calcRecord;
+    const eventOp = result.eventOp;
 
+    calcRecord.changeDetectionHash = makeHashFlendersonPerson(calcRecord);
+    let changeDetected = false;
+    let event;
+    
+    switch (eventOp) {
+        case 'create':
+            changeDetected = true;
+            event = craftCreateEvent(oldRecord, calcRecord, functionInvocation);
+            break;
+        case 'update':
+            changeDetected = (oldRecord.changeDetectionHash === calcRecord.changeDetectionHash) ? false : true;
+            event = craftUpdateEvent(oldRecord, calcRecord, functionInvocation);
+            break;
+        case 'delete':
+            changeDetected = true;
+            event = craftDeleteEvent(oldRecord, calcRecord, functionInvocation);
+            break;
+    }
+
+    if (changeDetected) {
+        context.bindings.recordOut = calcRecord;
+        context.bindings.eventCascade = event;
         context.bindings.changeParse = {
             "payload": {
                 oldRecord: (oldRecord) ? oldRecord : null,
-                newRecord: result.newRecord
+                newRecord: calcRecord
             }
         };
 
-        const logPayload = result.event;
+        const logPayload = event;
         logPayload['jobType'] = jobType;
         logPayload['statusCode'] = statusCode;
         logPayload['statusMessage'] = statusMessage;
@@ -103,231 +134,15 @@ const flendersonPositionStore: AzureFunction = async function (context: Context,
         logPayload['jobType'] = jobType;
         logPayload['statusCode'] = statusCode;
         logPayload['statusMessage'] = 'No change detected.';
+        logPayload['recordID'] = calcRecord.id;
+        logPayload['newRecordChangeDetectionHash'] = calcRecord.changeDetectionHash;
+        logPayload['oldRecordChangeDetectionHash'] = oldRecord.changeDetectionHash;
         functionInvocation.logPayload = logPayload;
     }
     
     context.bindings.invocationPostProcessor = functionInvocation;
     context.log(functionInvocation);
     context.done(null, functionInvocation);
-
-
-    function doDelete(oldRecord, newRecord, payload) {
-        let event = {};
-        const changedDetected = true;
-
-        // check for existing record
-        if (!oldRecord) {
-            newRecord = Object.assign(newRecord, payload);
-            newRecord.createdAt = functionInvocation.functionInvocationTimestamp;
-            newRecord.updatedAt = functionInvocation.functionInvocationTimestamp;
-
-            // mark the record as deleted
-            newRecord.deletedAt = functionInvocation.functionInvocationTimestamp;
-            newRecord.deleted = true;
-
-            newRecord.changeDetectionHash = makeHash(newRecord);
-
-            event = craftDeleteEvent(oldRecord);
-
-        } else {
-            newRecord = Object.assign(newRecord, oldRecord);
-
-            // mark the record as deleted
-            newRecord.deletedAt = functionInvocation.functionInvocationTimestamp;
-            newRecord.deleted = true;
-
-            newRecord.changeDetectionHash = makeHash(newRecord);
-
-            event = craftDeleteEvent(oldRecord);
-        }
-
-        return {changedDetected: changedDetected, event: event, newRecord: newRecord};
-    }
-
-    function doPatch(oldRecord, newRecord, payload) {
-        let event = {};
-        let changedDetected = false;
-
-        if (!oldRecord) {
-            newRecord = Object.assign(newRecord, payload);
-            newRecord.createdAt = functionInvocation.functionInvocationTimestamp;
-            newRecord.updatedAt = functionInvocation.functionInvocationTimestamp;
-    
-            // patching a record implicitly undeletes it
-            newRecord.deletedAt = '';
-            newRecord.deleted = false;
-    
-            newRecord.changeDetectionHash = makeHash(newRecord);
-
-            changedDetected = true;
-            event = craftCreateEvent(newRecord);
-
-        } else {
-            // Merge request object into current record
-            newRecord = Object.assign(newRecord, oldRecord, payload);
-            newRecord.updatedAt = functionInvocation.functionInvocationTimestamp;
-    
-            // patching a record implicitly undeletes it
-            newRecord.deletedAt = '';
-            newRecord.deleted = false;
-
-            newRecord.changeDetectionHash = makeHash(newRecord);
-
-            changedDetected = (oldRecord.changeDetectionHash === newRecord.changeDetectionHash) ? false : true;
-
-            if (changedDetected) {
-                event = craftUpdateEvent(oldRecord, newRecord);
-            } else {
-                newRecord = oldRecord;
-                event = false;
-            }
-        }
-
-        return {changedDetected: changedDetected, event: event, newRecord: newRecord};
-    }
-    
-    function doReplace(oldRecord, newRecord, payload) {
-        let event = {};
-        let changedDetected = false;
-
-        if (!oldRecord) {
-            newRecord = Object.assign(newRecord, payload);
-            newRecord.createdAt = functionInvocation.functionInvocationTimestamp;
-            newRecord.updatedAt = functionInvocation.functionInvocationTimestamp;
-
-            // replacing a record implicitly undeletes it
-            newRecord.deletedAt = '';
-            newRecord.deleted = false;
-
-            newRecord.changeDetectionHash = makeHash(newRecord);
-
-            changedDetected = true;
-            event = craftCreateEvent(newRecord);
-
-        } else {
-            newRecord = Object.assign(newRecord, payload);
-            newRecord.createdAt = oldRecord.createdAt;
-            newRecord.updatedAt = functionInvocation.functionInvocationTimestamp;
-
-            // replacing a record implicitly undeletes it
-            newRecord.deletedAt = '';
-            newRecord.deleted = false;
-
-            newRecord.changeDetectionHash = makeHash(newRecord);
-
-            changedDetected = (oldRecord.changeDetectionHash === newRecord.changeDetectionHash) ? false : true;
-    
-            if (changedDetected) {
-                event = craftUpdateEvent(oldRecord, newRecord);
-            } else {
-                newRecord = oldRecord;
-                event = false;
-            }
-        }
-
-        return {changedDetected: changedDetected, event: event, newRecord: newRecord};
-    }
-
-    function craftCreateEvent(newRecord) {
-        const eventType = 'Flenderson.FlendersonPerson.Create';
-        const source = 'create';
-        const schema = 'create';
-        const label = `Location ${newRecord.id} created.`;
-        const payload = {
-            record: newRecord
-        };
-
-        const event = craftEvent(newRecord.id, source, schema, eventType, label, payload);
-        return event;
-    }
-    
-    function craftUpdateEvent(oldRecord, newRecord) {
-        const eventType = 'Flenderson.FlendersonPerson.Update';
-        const source = 'update';
-        const schema = 'update';
-        const label = `Location ${newRecord.id} updated.`;
-        const payload = {
-            oldRecord: oldRecord,
-            newRecord: newRecord,
-        };
-
-        const event = craftEvent(newRecord.id, source, schema, eventType, label, payload);
-        return event;
-    }
-
-    function craftDeleteEvent(oldRecord) {
-        const eventType = 'Flenderson.FlendersonPerson.Delete';
-        const source = 'delete';
-        const schema = 'delete';
-        const label = `Location ${oldRecord.id} deleted.`;
-        const payload = {
-            record: oldRecord
-        };
-
-        const event = craftEvent(oldRecord.id, source, schema, eventType, label, payload);
-        return event;
-    }
-
-    function craftEvent(recordID, source, schema, eventType, label, payload) {
-        const event = {
-            id: `${eventType}-${functionInvocation.functionInvocationID}`,
-            time: functionInvocation.functionInvocationTimestamp,
-
-            type: eventType,
-            source: `/flenderson/ipps-location/${recordID}/${source}`,
-            schemaURL: `ca.wrdsb.flenderson.ipps-location.${schema}.json`,
-
-            label: label,
-            tags: [
-                "flenderson", 
-                "ipps_location_change",
-                "location_change"
-            ], 
-
-            data: {
-                functionName: functionInvocation.functionName,
-                invocationID: functionInvocation.functionInvocationID,
-                result: {
-                    payload: payload 
-                },
-            },
-
-            eventTypeVersion: "0.1",
-            specversion: "0.2",
-            contentType: "application/json"
-        };
-
-        // TODO: check message length
-        return event;
-    }
-
-    function makeHash(flendersonPosition: FlendersonPerson): string {
-        const objectForHash = JSON.stringify({
-            email:                    flendersonPosition.email,
-            username:                 flendersonPosition.username,
-            employeeID:               flendersonPosition.employeeID,
-            ein:                      flendersonPosition.ein,
-            activityCode:             flendersonPosition.activityCode,
-            firstName:                flendersonPosition.firstName,
-            lastName:                 flendersonPosition.lastName,
-            fullName:                 flendersonPosition.fullName,
-            sortableName:             flendersonPosition.sortableName,
-            locationCodes:            flendersonPosition.locationCodes,
-            schoolCodes:              flendersonPosition.schoolCodes,
-            jobCodes:                 flendersonPosition.jobCodes,
-            employeeGroupCodes:       flendersonPosition.employeeGroupCodes,
-            homeLocation:             flendersonPosition.homeLocation,
-            directory:                flendersonPosition.directory,
-            phone:                    flendersonPosition.phone,
-            extension:                flendersonPosition.extension,
-            mbxnumber:                flendersonPosition.mbxnumber,
-            numberOfPositions:        flendersonPosition.numberOfPositions,
-            numberOfActivePositions:  flendersonPosition.numberOfActivePositions,
-            positions:                flendersonPosition.positions
-        });
-        const objectHash = createHash('md5').update(objectForHash).digest('hex');
-        return objectHash;
-    }
 };
 
 export default flendersonPositionStore;
