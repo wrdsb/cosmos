d0c6b6c8ca1c202b2cd963233d40c3bc4cd8285e
James Schumann <james@schumann.ca>
2021-07-08 13:32:52 -0400

    add types to support newly refactored code

--- libs/types/src/lib/events/flenderson.ts
+++ libs/types/src/lib/events/flenderson.ts
@@ -1,92 +1,266 @@
 export type WRDSBFlendersonEventSubject =
-'/wrdsb/flenderson/ipps-directory/create' |
-'/wrdsb/flenderson/ipps-directory/update' |
-'/wrdsb/flenderson/ipps-directory/delete' |
+'/wrdsb/flenderson/ipps-directory/store/create'|
+'/wrdsb/flenderson/ipps-directory/store/update'|
+'/wrdsb/flenderson/ipps-directory/store/delete'|
 
-'/wrdsb/flenderson/ipps-employee-group/create' |
-'/wrdsb/flenderson/ipps-employee-group/update' |
-'/wrdsb/flenderson/ipps-employee-group/delete' |
+'/wrdsb/flenderson/ipps-employee-group/store/create'|
+'/wrdsb/flenderson/ipps-employee-group/store/update'|
+'/wrdsb/flenderson/ipps-employee-group/store/delete'|
 
-'/wrdsb/flenderson/ipps-job/create' |
-'/wrdsb/flenderson/ipps-job/update' |
-'/wrdsb/flenderson/ipps-job/delete' |
+'/wrdsb/flenderson/ipps-job/store/create'|
+'/wrdsb/flenderson/ipps-job/store/update'|
+'/wrdsb/flenderson/ipps-job/store/delete'|
 
-'/wrdsb/flenderson/ipps-location/create' |
-'/wrdsb/flenderson/ipps-location/update' |
-'/wrdsb/flenderson/ipps-location/delete' |
+'/wrdsb/flenderson/ipps-location/store/create'|
+'/wrdsb/flenderson/ipps-location/store/update'|
+'/wrdsb/flenderson/ipps-location/store/delete'|
 
-'/wrdsb/flenderson/ipps-pal/create' |
-'/wrdsb/flenderson/ipps-pal/update' |
-'/wrdsb/flenderson/ipps-pal/delete' |
+'/wrdsb/flenderson/ipps-pal/store/create'|
+'/wrdsb/flenderson/ipps-pal/store/update'|
+'/wrdsb/flenderson/ipps-pal/store/delete'|
 
-'/wrdsb/flenderson/ipps-person/create' |
-'/wrdsb/flenderson/ipps-person/update' |
-'/wrdsb/flenderson/ipps-person/delete' |
+'/wrdsb/flenderson/ipps-person/store/create'|
+'/wrdsb/flenderson/ipps-person/store/update'|
+'/wrdsb/flenderson/ipps-person/store/delete'|
 
-'/wrdsb/flenderson/ipps-position/create' |
-'/wrdsb/flenderson/ipps-position/update' |
-'/wrdsb/flenderson/ipps-position/delete' |
+'/wrdsb/flenderson/ipps-position/store/create'|
+'/wrdsb/flenderson/ipps-position/store/update'|
+'/wrdsb/flenderson/ipps-position/store/delete'|
 
-'/wrdsb/flenderson/flenderson-person/create' |
-'/wrdsb/flenderson/flenderson-person/update' |
-'/wrdsb/flenderson/flenderson-person/delete' |
+'/wrdsb/flenderson/flenderson-person/store/create'|
+'/wrdsb/flenderson/flenderson-person/store/update'|
+'/wrdsb/flenderson/flenderson-person/store/delete'|
 
-'/wrdsb/flenderson/flenderson-position/create' |
-'/wrdsb/flenderson/flenderson-position/update' |
-'/wrdsb/flenderson/flenderson-position/delete';
+'/wrdsb/flenderson/flenderson-position/store/create'|
+'/wrdsb/flenderson/flenderson-position/store/update'|
+'/wrdsb/flenderson/flenderson-position/store/delete'|
+
+'/wrdsb/flenderson/ipps-directory/create'|
+'/wrdsb/flenderson/ipps-directory/update'|
+'/wrdsb/flenderson/ipps-directory/delete'|
+'/wrdsb/flenderson/ipps-directory/undelete'|
+'/wrdsb/flenderson/ipps-directory/email/change'|
+'/wrdsb/flenderson/ipps-directory/firstName/change'|
+'/wrdsb/flenderson/ipps-directory/lastName/change'|
+'/wrdsb/flenderson/ipps-directory/fullName/change'|
+'/wrdsb/flenderson/ipps-directory/directory/change'|
+'/wrdsb/flenderson/ipps-directory/phone/change'|
+'/wrdsb/flenderson/ipps-directory/extension/change'|
+'/wrdsb/flenderson/ipps-directory/mbxnumber/change'|
+'/wrdsb/flenderson/ipps-directory/schoolCode/change'|
+'/wrdsb/flenderson/ipps-directory/jobDesc/change'|
+
+'/wrdsb/flenderson/ipps-employee-group/create'|
+'/wrdsb/flenderson/ipps-employee-group/update'|
+'/wrdsb/flenderson/ipps-employee-group/delete'|
+'/wrdsb/flenderson/ipps-employee-group/undelete'|
+'/wrdsb/flenderson/ipps-employee-group/employeeGroupCode/change'|
+'/wrdsb/flenderson/ipps-employee-group/employeeGroupCategory/change'|
+'/wrdsb/flenderson/ipps-employee-group/employeeGroupDescription/change'|
+'/wrdsb/flenderson/ipps-employee-group/employeeGroupAbbreviation/change'|
+
+'/wrdsb/flenderson/ipps-job/create'|
+'/wrdsb/flenderson/ipps-job/update'|
+'/wrdsb/flenderson/ipps-job/delete'|
+'/wrdsb/flenderson/ipps-job/undelete'|
+'/wrdsb/flenderson/ipps-job/jobCode/change'|
+'/wrdsb/flenderson/ipps-job/jobDescription/change'|
+'/wrdsb/flenderson/ipps-job/jobAbbreviation/change'|
+
+'/wrdsb/flenderson/ipps-location/create'|
+'/wrdsb/flenderson/ipps-location/update'|
+'/wrdsb/flenderson/ipps-location/delete'|
+'/wrdsb/flenderson/ipps-location/undelete'|
+'/wrdsb/flenderson/ipps-location/locationCode/change'|
+'/wrdsb/flenderson/ipps-location/locationType/change'|
+'/wrdsb/flenderson/ipps-location/locationDescription/change'|
+'/wrdsb/flenderson/ipps-location/locationAbbreviation/change'|
+'/wrdsb/flenderson/ipps-location/schoolCode/change'|
+'/wrdsb/flenderson/ipps-location/panel/change'|
+
+'/wrdsb/flenderson/ipps-pal/create'|
+'/wrdsb/flenderson/ipps-pal/update'|
+'/wrdsb/flenderson/ipps-pal/delete'|
+'/wrdsb/flenderson/ipps-pal/undelete'|
+'/wrdsb/flenderson/ipps-pal/employeeID/change'|
+'/wrdsb/flenderson/ipps-pal/username/change'|
+
+'/wrdsb/flenderson/ipps-person/create'|
+'/wrdsb/flenderson/ipps-person/update'|
+'/wrdsb/flenderson/ipps-person/delete'|
+'/wrdsb/flenderson/ipps-person/undelete'|
+'/wrdsb/flenderson/ipps-person/employeeID/change'|
+'/wrdsb/flenderson/ipps-person/email/change'|
+'/wrdsb/flenderson/ipps-person/firstName/change'|
+'/wrdsb/flenderson/ipps-person/lastName/change'|
+'/wrdsb/flenderson/ipps-person/status/change'|
+
+'/wrdsb/flenderson/ipps-position/create'|
+'/wrdsb/flenderson/ipps-position/update'|
+'/wrdsb/flenderson/ipps-position/delete'|
+'/wrdsb/flenderson/ipps-position/undelete'|
+'/wrdsb/flenderson/ipps-position/positionID/change'|
+'/wrdsb/flenderson/ipps-position/employeeID/change'|
+'/wrdsb/flenderson/ipps-position/employeeGroupCode/change'|
+'/wrdsb/flenderson/ipps-position/jobCode/change'|
+'/wrdsb/flenderson/ipps-position/locationCode/change'|
+'/wrdsb/flenderson/ipps-position/establishmentCode/change'|
+'/wrdsb/flenderson/ipps-position/isHomeLocation/change'|
+'/wrdsb/flenderson/ipps-position/positionStartDate/change'|
+'/wrdsb/flenderson/ipps-position/positionEndDate/change'|
+
+'/wrdsb/flenderson/flenderson-person/create'|
+'/wrdsb/flenderson/flenderson-person/update'|
+'/wrdsb/flenderson/flenderson-person/delete'|
+'/wrdsb/flenderson/flenderson-person/undelete'|
+
+'/wrdsb/flenderson/flenderson-position/create'|
+'/wrdsb/flenderson/flenderson-position/update'|
+'/wrdsb/flenderson/flenderson-position/delete'|
+'/wrdsb/flenderson/flenderson-position/undelete';
 
 
 export type WRDSBFlendersonEventType =
-'Flenderson.IPPSDirectory.Create' |
-'Flenderson.IPPSDirectory.Update' |
-'Flenderson.IPPSDirectory.Delete' |
+'WRDSB.Flenderson.IPPSDirectory.Store.Create'|
+'WRDSB.Flenderson.IPPSDirectory.Store.Update'|
+'WRDSB.Flenderson.IPPSDirectory.Store.Delete'|
 
-'Flenderson.IPPSEmployeeGroup.Create' |
-'Flenderson.IPPSEmployeeGroup.Update' |
-'Flenderson.IPPSEmployeeGroup.Delete' |
+'WRDSB.Flenderson.IPPSEmployeeGroup.Store.Create'|
+'WRDSB.Flenderson.IPPSEmployeeGroup.Store.Update'|
+'WRDSB.Flenderson.IPPSEmployeeGroup.Store.Delete'|
 
-'Flenderson.IPPSJob.Create' |
-'Flenderson.IPPSJob.Update' |
-'Flenderson.IPPSJob.Delete' |
+'WRDSB.Flenderson.IPPSJob.Store.Create'|
+'WRDSB.Flenderson.IPPSJob.Store.Update'|
+'WRDSB.Flenderson.IPPSJob.Store.Delete'|
 
-'Flenderson.IPPSLocation.Create' |
-'Flenderson.IPPSLocation.Update' |
-'Flenderson.IPPSLocation.Delete' |
+'WRDSB.Flenderson.IPPSLocation.Store.Create'|
+'WRDSB.Flenderson.IPPSLocation.Store.Update'|
+'WRDSB.Flenderson.IPPSLocation.Store.Delete'|
 
-'Flenderson.IPPSPal.Create' |
-'Flenderson.IPPSPal.Update' |
-'Flenderson.IPPSPal.Delete' |
+'WRDSB.Flenderson.IPPSPal.Store.Create'|
+'WRDSB.Flenderson.IPPSPal.Store.Update'|
+'WRDSB.Flenderson.IPPSPal.Store.Delete'|
 
-'Flenderson.IPPSPerson.Create' |
-'Flenderson.IPPSPerson.Update' |
-'Flenderson.IPPSPerson.Delete' |
+'WRDSB.Flenderson.IPPSPerson.Store.Create'|
+'WRDSB.Flenderson.IPPSPerson.Store.Update'|
+'WRDSB.Flenderson.IPPSPerson.Store.Delete'|
 
-'Flenderson.IPPSPosition.Create' |
-'Flenderson.IPPSPosition.Update' |
-'Flenderson.IPPSPosition.Delete' |
+'WRDSB.Flenderson.IPPSPosition.Store.Create'|
+'WRDSB.Flenderson.IPPSPosition.Store.Update'|
+'WRDSB.Flenderson.IPPSPosition.Store.Delete'|
 
-'Flenderson.FlendersonPerson.Create' |
-'Flenderson.FlendersonPerson.Update' |
-'Flenderson.FlendersonPerson.Delete' |
+'WRDSB.Flenderson.FlendersonPerson.Store.Create'|
+'WRDSB.Flenderson.FlendersonPerson.Store.Update'|
+'WRDSB.Flenderson.FlendersonPerson.Store.Delete'|
 
-'Flenderson.FlendersonPosition.Create' |
-'Flenderson.FlendersonPosition.Update' |
-'Flenderson.FlendersonPosition.Delete';
+'WRDSB.Flenderson.FlendersonPosition.Store.Create'|
+'WRDSB.Flenderson.FlendersonPosition.Store.Update'|
+'WRDSB.Flenderson.FlendersonPosition.Store.Delete'|
+
+'WRDSB.Flenderson.IPPSDirectory.Create'|
+'WRDSB.Flenderson.IPPSDirectory.Update'|
+'WRDSB.Flenderson.IPPSDirectory.Delete'|
+'WRDSB.Flenderson.IPPSDirectory.Undelete'|
+'WRDSB.Flenderson.IPPSDirectory.Email.Change'|
+'WRDSB.Flenderson.IPPSDirectory.FirstName.Change'|
+'WRDSB.Flenderson.IPPSDirectory.LastName.Change'|
+'WRDSB.Flenderson.IPPSDirectory.FullName.Change'|
+'WRDSB.Flenderson.IPPSDirectory.Directory.Change'|
+'WRDSB.Flenderson.IPPSDirectory.Phone.Change'|
+'WRDSB.Flenderson.IPPSDirectory.Extension.Change'|
+'WRDSB.Flenderson.IPPSDirectory.MBXNumber.Change'|
+'WRDSB.Flenderson.IPPSDirectory.schoolCode.Change'|
+'WRDSB.Flenderson.IPPSDirectory.jobDesc.Change'|
+
+'WRDSB.Flenderson.IPPSEmployeeGroup.Create'|
+'WRDSB.Flenderson.IPPSEmployeeGroup.Update'|
+'WRDSB.Flenderson.IPPSEmployeeGroup.Delete'|
+'WRDSB.Flenderson.IPPSEmployeeGroup.Undelete'|
+'WRDSB.Flenderson.IPPSEmployeeGroup.EmployeeGroupCode.Change'|
+'WRDSB.Flenderson.IPPSEmployeeGroup.EmployeeGroupCategory.Change'|
+'WRDSB.Flenderson.IPPSEmployeeGroup.EmployeeGroupDescription.Change'|
+'WRDSB.Flenderson.IPPSEmployeeGroup.EmployeeGroupAbbriviation.Change'|
+
+'WRDSB.Flenderson.IPPSJob.Create'|
+'WRDSB.Flenderson.IPPSJob.Update'|
+'WRDSB.Flenderson.IPPSJob.Delete'|
+'WRDSB.Flenderson.IPPSJob.Undelete'|
+'WRDSB.Flenderson.IPPSJob.JobCode.Change'|
+'WRDSB.Flenderson.IPPSJob.JobDescription.Change'|
+'WRDSB.Flenderson.IPPSJob.JobAbbriviation.Change'|
+
+'WRDSB.Flenderson.IPPSLocation.Create'|
+'WRDSB.Flenderson.IPPSLocation.Update'|
+'WRDSB.Flenderson.IPPSLocation.Delete'|
+'WRDSB.Flenderson.IPPSLocation.Undelete'|
+'WRDSB.Flenderson.IPPSLocation.LocationCode.Change'|
+'WRDSB.Flenderson.IPPSLocation.LocationType.Change'|
+'WRDSB.Flenderson.IPPSLocation.LocationDescription.Change'|
+'WRDSB.Flenderson.IPPSLocation.LocationAbbriviation.Change'|
+'WRDSB.Flenderson.IPPSLocation.SchoolCode.Change'|
+'WRDSB.Flenderson.IPPSLocation.Panel.Change'|
+
+'WRDSB.Flenderson.IPPSPal.Create'|
+'WRDSB.Flenderson.IPPSPal.Update'|
+'WRDSB.Flenderson.IPPSPal.Delete'|
+'WRDSB.Flenderson.IPPSPal.Undelete'|
+'WRDSB.Flenderson.IPPSPal.EmployeeID.Change'|
+'WRDSB.Flenderson.IPPSPal.Username.Change'|
+
+'WRDSB.Flenderson.IPPSPerson.Create'|
+'WRDSB.Flenderson.IPPSPerson.Update'|
+'WRDSB.Flenderson.IPPSPerson.Delete'|
+'WRDSB.Flenderson.IPPSPerson.Undelete'|
+'WRDSB.Flenderson.IPPSPerson.EmployeeID.Change'|
+'WRDSB.Flenderson.IPPSPerson.Email.Change'|
+'WRDSB.Flenderson.IPPSPerson.FirstName.Change'|
+'WRDSB.Flenderson.IPPSPerson.LastName.Change'|
+'WRDSB.Flenderson.IPPSPerson.Status.Change'|
+
+'WRDSB.Flenderson.IPPSPosition.Create'|
+'WRDSB.Flenderson.IPPSPosition.Update'|
+'WRDSB.Flenderson.IPPSPosition.Delete'|
+'WRDSB.Flenderson.IPPSPosition.Undelete'|
+'WRDSB.Flenderson.IPPSPosition.PositionID.Change'|
+'WRDSB.Flenderson.IPPSPosition.EmployeeID.Change'|
+'WRDSB.Flenderson.IPPSPosition.EmployeeGroupCode.Change'|
+'WRDSB.Flenderson.IPPSPosition.JobCode.Change'|
+'WRDSB.Flenderson.IPPSPosition.LocationCode.Change'|
+'WRDSB.Flenderson.IPPSPosition.EstablishmentCode.Change'|
+'WRDSB.Flenderson.IPPSPosition.isHomeLocation.Change'|
+'WRDSB.Flenderson.IPPSPosition.PositionStartDate.Change'|
+'WRDSB.Flenderson.IPPSPosition.PositionEndDate.Change'|
+
+'WRDSB.Flenderson.FlendersonPerson.Create'|
+'WRDSB.Flenderson.FlendersonPerson.Update'|
+'WRDSB.Flenderson.FlendersonPerson.Delete'|
+'WRDSB.Flenderson.FlendersonPerson.Undelete'|
+
+'WRDSB.Flenderson.FlendersonPosition.Create'|
+'WRDSB.Flenderson.FlendersonPosition.Update'|
+'WRDSB.Flenderson.FlendersonPosition.Delete'|
+'WRDSB.Flenderson.FlendersonPosition.Undelete';
+
 
 export type WRDSBFlendersonEventTag =
-'flenderson' |
+'flenderson'|
+'store'|
+'flenderson_store'|
+'store_create'|
+'store_update'|
+'store_delete'|
 
-'ipps_directory_change' |
-'ipps_employee_group_change' |
-'ipps_job_change' |
-'ipps_location_change' |
-'ipps_pal_change' |
-'ipps_person_change' |
-'ipps_position_change' |
+'ipps_directory_change'|
+'ipps_employee_group_change'|
+'ipps_job_change'|
+'ipps_location_change'|
+'ipps_pal_change'|
+'ipps_person_change'|
+'ipps_position_change'|
 
-'flenderson_person_change' |
-'flenderson_position_change' |
+'flenderson_person_change'|
+'flenderson_position_change'|
 
-'pal_change' |
-'person_change' |
+'pal_change'|
+'person_change'|
 'position_change'
