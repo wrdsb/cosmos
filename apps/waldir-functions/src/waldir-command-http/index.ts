1826fd921633f38bd9b870da4d98d8cd53561cb9
James Schumann <james@schumann.ca>
2021-07-12 09:06:54 -0400

    stub waldir-command-http

---
+++ apps/waldir-functions/src/waldir-command-http/index.ts
@@ -0,0 +1,46 @@
+import { AzureFunction, Context } from "@azure/functions"
+import { FunctionInvocation, WALDIRCommand, WALDIRCommandFunctionRequest, WALDIRCommandFunctionRequestBody, WALDIRJobType, WALDIRCommandOperation, WALDIRCommandFunctionRequestPayload } from '@cosmos/types';
+import { generateResponseObject, validateWALDIRCommand } from "@cosmos/walder-functions-shared";
+
+const waldirCommandHTTP: AzureFunction = async function (context: Context, req: WALDIRCommandFunctionRequest): Promise<void> {
+    const functionInvocation = {
+        functionInvocationID: context.executionContext.invocationId,
+        functionInvocationTimestamp: new Date().toJSON(),
+        functionApp: 'WALDIR',
+        functionName: context.executionContext.functionName,
+        functionDataType: 'Command',
+        functionDataOperation: 'HTTP',
+        eventLabel: ''
+    } as FunctionInvocation;
+
+    const request = req as WALDIRCommandFunctionRequest;
+    const requestBody = request.body as WALDIRCommandFunctionRequestBody;
+    const command = requestBody.command as WALDIRCommand;
+
+    const jobType = command.jobType as WALDIRJobType;
+    const operation = command.operation as WALDIRCommandOperation;
+    const payload = command.payload as WALDIRCommandFunctionRequestPayload;
+
+    const validatedCommand = validateWALDIRCommand(jobType, operation, payload);
+    const commandIsValid = validatedCommand.isValid;
+    const jobEnqueueMessages = validatedCommand.jobEnqueueMessages;
+
+    if (commandIsValid) {
+        context.bindings.jobEnqueue = jobEnqueueMessages;
+    }
+
+    const response = generateResponseObject(functionInvocation, command, validatedCommand);
+    
+    context.res = {
+        status: response.header.status,
+        body: response
+    }
+
+    const logPayload = response;
+    context.log(logPayload);
+
+    functionInvocation.logPayload = logPayload;
+    context.done(null, functionInvocation);
+};
+
+export default waldirCommandHTTP;
