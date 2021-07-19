d4269c0d25573f84fba91adc12f3233e33e5552d
James Schumann <james@schumann.ca>
2021-07-19 13:41:43 -0400

    add fields to WPUser, including capabilities

--- libs/types/src/lib/entities/wordpress/user.ts
+++ libs/types/src/lib/entities/wordpress/user.ts
@@ -14,17 +14,17 @@ interface WPUser extends CosmosCoreFields {
     //id?: string;
     changeDetectionHash?: string;
 
-    siteURL?: string;
-    siteDomain?: string;
-    siteSlug?: string;
-    siteLink?: string;
+    site_url?: string;
+    site_domain?: string;
+    site_slug?: string;
+    site_link?: string;
 
     username?: string;
     email?: string;
 
     name?: string;
-    firstName?: string;
-    lastName?: string;
+    first_name?: string;
+    last_name?: string;
     nickname?: string;
 
     description?: string;
@@ -33,16 +33,136 @@ interface WPUser extends CosmosCoreFields {
     url?: string;
 
     locale?: string;
-    registeredDate?: string;
-
-    password?: string;
+    registered_date?: string;
 
     roles?: string[];
 
-    capabilities?: any;
-    extraCapabilities?: any;
-    avatarURLs?: any;
-    meta?: any;
+    capabilities?: WPCapabilities;
+    extra_capabilities?: WPExtraCapabilities;
+    avatar_urls?: any;
+    meta?: any[];
+
+    ipps_activity_code?: string[];
+    ipps_employee_group_category?: string[];
+    ipps_employee_group_code?: string[];
+    ipps_employee_group_description?: string[];
+    ipps_extension?: string[];
+    ipps_home_loc?: string[];
+    ipps_job_code?: string[];
+    ipps_job_desc?: string[];
+    ipps_location_code?: string[];
+    ipps_location_desc?: string[];
+    ipps_panel?: string[];
+    ipps_phone_no?: string[];
+    ipps_school_code?: string[];
+    ipps_school_type?: string[];
+    wrdsb_id_number?: string[];
+    wrdsb_school?: string[];
+    wrdsb_baksheesh?: string[];
+    wrdsb_supervisor?: string[];
+    wrdsb_section?: string[];
+    wrdsb_physical_location?: boolean;
+    wrdsb_voicemail?: string;
+    wrdsb_job_title?: string;
+    wrdsb_display_in_staff_list?: string;
+    wrdsb_contact_options?: string;
+    wrdsb_website_url?: string;
+    wrdsb_phone_extension?: false;
+    wrdsb_regular_hours?: false;
+    wrdsb_is_in_today?: false;
+    wrdsb_is_available_now?: false;
 }
 
 export { WPUser };
+
+interface WPCapabilities {
+    switch_themes?: boolean,
+    edit_themes?: boolean,
+    activate_plugins?: boolean,
+    edit_plugins?: boolean,
+    edit_users?: boolean,
+    edit_files?: boolean,
+    manage_options?: boolean,
+    moderate_comments?: boolean,
+    manage_categories?: boolean,
+    manage_links?: boolean,
+    upload_files?: boolean,
+    import?: boolean,
+    unfiltered_html?: boolean,
+    edit_posts?: boolean,
+    edit_others_posts?: boolean,
+    edit_published_posts?: boolean,
+    publish_posts?: boolean,
+    edit_pages?: boolean,
+    read?: boolean,
+    level_10?: boolean,
+    level_9?: boolean,
+    level_8?: boolean,
+    level_7?: boolean,
+    level_6?: boolean,
+    level_5?: boolean,
+    level_4?: boolean,
+    level_3?: boolean,
+    level_2?: boolean,
+    level_1?: boolean,
+    level_0?: boolean,
+    edit_others_pages?: boolean,
+    edit_published_pages?: boolean,
+    publish_pages?: boolean,
+    delete_pages?: boolean,
+    delete_others_pages?: boolean,
+    delete_published_pages?: boolean,
+    delete_posts?: boolean,
+    delete_others_posts?: boolean,
+    delete_published_posts?: boolean,
+    delete_private_posts?: boolean,
+    edit_private_posts?: boolean,
+    read_private_posts?: boolean,
+    delete_private_pages?: boolean,
+    edit_private_pages?: boolean,
+    read_private_pages?: boolean,
+    delete_users?: boolean,
+    create_users?: boolean,
+    unfiltered_upload?: boolean,
+    edit_dashboard?: boolean,
+    update_plugins?: boolean,
+    delete_plugins?: boolean,
+    install_plugins?: boolean,
+    update_themes?: boolean,
+    install_themes?: boolean,
+    update_core?: boolean,
+    list_users?: boolean,
+    remove_users?: boolean,
+    promote_users?: boolean,
+    edit_theme_options?: boolean,
+    delete_themes?: boolean,
+    export?: boolean,
+    frm_view_entries?: boolean,
+    frm_create_entries?: boolean,
+    frm_edit_entries?: boolean,
+    frm_delete_entries?: boolean,
+    frm_view_reports?: boolean,
+    frm_edit_displays?: boolean,
+    read_ai1ec_event?: boolean,
+    edit_ai1ec_event?: boolean,
+    edit_ai1ec_events?: boolean,
+    edit_others_ai1ec_events?: boolean,
+    edit_private_ai1ec_events?: boolean,
+    edit_published_ai1ec_events?: boolean,
+    delete_ai1ec_event?: boolean,
+    delete_ai1ec_events?: boolean,
+    delete_others_ai1ec_events?: boolean,
+    delete_published_ai1ec_events?: boolean,
+    delete_private_ai1ec_events?: boolean,
+    publish_ai1ec_events?: boolean,
+    read_private_ai1ec_events?: boolean,
+    manage_events_categories?: boolean,
+    manage_ai1ec_feeds?: boolean,
+    switch_ai1ec_themes?: boolean,
+    manage_ai1ec_options?: boolean,
+    administrator?: boolean,
+}
+
+interface WPExtraCapabilities {
+    administrator?: boolean
+}
--- libs/waldir-functions-shared/src/lib/make-hash-wp-user.ts
+++ libs/waldir-functions-shared/src/lib/make-hash-wp-user.ts
@@ -3,16 +3,17 @@ import { createHash } from "crypto";
 
 export function makeHashWPUser(objectToHash: WPUser): string {
     const objectForHash = JSON.stringify({
-        siteURL:                  objectToHash.siteURL,
-        siteDomain:               objectToHash.siteDomain,
-        siteSlug:                 objectToHash.siteSlug,
+        site_url:                 objectToHash.site_url,
+        site_domain:              objectToHash.site_domain,
+        site_slug:                objectToHash.site_slug,
+        site_link:                objectToHash.site_link,
 
-        email:                    objectToHash.email,
         username:                 objectToHash.username,
+        email:                    objectToHash.email,
 
         name:                     objectToHash.name,
-        firstName:                objectToHash.firstName,
-        lastName:                 objectToHash.lastName,
+        first_name:               objectToHash.first_name,
+        last_name:                objectToHash.last_name,
         nickname:                 objectToHash.nickname,
 
         description:              objectToHash.description,
@@ -21,16 +22,45 @@ export function makeHashWPUser(objectToHash: WPUser): string {
         url:                      objectToHash.url,
 
         locale:                   objectToHash.locale,
-        registeredDate:           objectToHash.registeredDate,
-
-        // password:              objectToHash.password,
+        registered_date:          objectToHash.registered_date,
 
         roles:                    objectToHash.roles,
 
         capabilities:             objectToHash.capabilities,
-        extraCapabilities:        objectToHash.extraCapabilities,
-        avatarURLs:               objectToHash.avatarURLs,
+        extra_capabilities:       objectToHash.extra_capabilities,
+        avatar_urls:              objectToHash.avatar_urls,
         meta:                     objectToHash.meta,
+
+        ipps_activity_code:                 objectToHash.ipps_activity_code,
+        ipps_employee_group_category:       objectToHash.ipps_employee_group_category,
+        ipps_employee_group_code:           objectToHash.ipps_employee_group_code,
+        ipps_employee_group_description:    objectToHash.ipps_employee_group_description,
+        ipps_extension:                     objectToHash.ipps_extension,
+        ipps_home_loc:                      objectToHash.ipps_home_loc,
+        ipps_job_code:                      objectToHash.ipps_job_code,
+        ipps_job_desc:                      objectToHash.ipps_job_desc,
+        ipps_location_code:                 objectToHash.ipps_location_code,
+        ipps_location_desc:                 objectToHash.ipps_location_desc,
+        ipps_panel:                         objectToHash.ipps_panel,
+        ipps_phone_no:                      objectToHash.ipps_phone_no,
+        ipps_school_code:                   objectToHash.ipps_school_code,
+        ipps_school_type:                   objectToHash.ipps_school_type,
+        
+        wrdsb_id_number:                    objectToHash.wrdsb_id_number,
+        wrdsb_school:                       objectToHash.wrdsb_school,
+        wrdsb_baksheesh:                    objectToHash.wrdsb_baksheesh,
+        wrdsb_supervisor:                   objectToHash.wrdsb_supervisor,
+        wrdsb_section:                      objectToHash.wrdsb_section,
+        wrdsb_physical_location:            objectToHash.wrdsb_physical_location,
+        wrdsb_voicemail:                    objectToHash.wrdsb_voicemail,
+        wrdsb_job_title:                    objectToHash.wrdsb_job_title,
+        wrdsb_display_in_staff_list:        objectToHash.wrdsb_display_in_staff_list,
+        wrdsb_contact_options:              objectToHash.wrdsb_contact_options,
+        wrdsb_website_url:                  objectToHash.wrdsb_website_url,
+        wrdsb_phone_extension:              objectToHash.wrdsb_phone_extension,
+        wrdsb_regular_hours:                objectToHash.wrdsb_regular_hours,
+        wrdsb_is_in_today:                  objectToHash.wrdsb_is_in_today,
+        wrdsb_is_available_now:             objectToHash.wrdsb_is_available_now,
     });
     const objectHash = createHash('md5').update(objectForHash).digest('hex');
     return objectHash;
