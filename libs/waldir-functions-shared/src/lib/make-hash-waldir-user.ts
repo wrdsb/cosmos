4181541facdcdde714e214a702fcfb7b782c18fd
James Schumann <james@schumann.ca>
2021-07-20 13:52:50 -0400

    replace FlendersonPerson with WALDIRUser

--- libs/waldir-functions-shared/src/lib/make-hash-waldir-user.ts
+++ libs/waldir-functions-shared/src/lib/make-hash-waldir-user.ts
@@ -1,29 +1,16 @@
-import { FlendersonPerson } from "@cosmos/types";
+import { WALDIRUser } from "@cosmos/types";
 import { createHash } from "crypto";
 
-export function makeHashFlendersonPerson(objectToHash: FlendersonPerson): string {
+export function makeHashWALDIRUser(objectToHash: WALDIRUser): string {
     const objectForHash = JSON.stringify({
-        email:                    objectToHash.email,
-        username:                 objectToHash.username,
-        employeeID:               objectToHash.employeeID,
-        ein:                      objectToHash.ein,
-        activityCode:             objectToHash.activityCode,
-        firstName:                objectToHash.firstName,
-        lastName:                 objectToHash.lastName,
-        fullName:                 objectToHash.fullName,
-        sortableName:             objectToHash.sortableName,
-        locationCodes:            objectToHash.locationCodes,
-        schoolCodes:              objectToHash.schoolCodes,
-        jobCodes:                 objectToHash.jobCodes,
-        employeeGroupCodes:       objectToHash.employeeGroupCodes,
-        homeLocation:             objectToHash.homeLocation,
-        directory:                objectToHash.directory,
-        phone:                    objectToHash.phone,
-        extension:                objectToHash.extension,
-        mbxnumber:                objectToHash.mbxnumber,
-        numberOfPositions:        objectToHash.numberOfPositions,
-        numberOfActivePositions:  objectToHash.numberOfActivePositions,
-        positions:                objectToHash.positions
+        email:             objectToHash.email,
+        username:          objectToHash.username,
+        firstName:         objectToHash.firstName,
+        lastName:          objectToHash.lastName,
+        fullName:          objectToHash.nickname,
+        siteIDs:           objectToHash.siteIDs,
+        roles:             objectToHash.roles,
+        siteMemberships:   objectToHash.siteMemberships
     });
     const objectHash = createHash('md5').update(objectForHash).digest('hex');
     return objectHash;
